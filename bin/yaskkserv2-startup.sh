ae0a1890f6bcf26d33f3f42c8649a010341c0975
yasunori-kirin0418 <yasunori.kirin0418@gmail.coom>
2021-09-11 23:18:06 +0900

    Update: bin/yaskkserv2-startup.sh

--- bin/yaskkserv2-startup.sh
+++ bin/yaskkserv2-startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 yaskkserv2 \
-    --no-deamonize \
     --google-japanese-input=notfound \
     --google-suggest \
     --google-cache-filename=/tmp/yaskkserv2.cache \
-    /tmp/dictionary.yaskkserv2
-
+    /tmp/dict.yaskkserv2 \
