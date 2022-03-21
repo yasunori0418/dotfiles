550df45ddbf353a15f371786859bfa238be69b13
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-03-21 22:41:41 +0900

    Fix

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require('wezterm')
 return {
     font = wezterm.font("Cica"),
     --use_ime = true, --vimで日本語入力するときは、skkeletonを使っているから問題無い
-    fot_size = 10.0
+    fot_size = 10.0,
     hide_tab_bar_if_only_one_tab = true,
     color_scheme = 'duskfox',
     color_scheme_dirs = { "$HOME/.config/wezterm/colors/" },
