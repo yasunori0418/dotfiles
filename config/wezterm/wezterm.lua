153f263ee0827e6d7294cb3e4ada5ae4cb7749bb
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-03-21 23:49:53 +0900

    Update: wezterm settings.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,12 +1,17 @@
 local wezterm = require('wezterm')
 
 return {
-    font = wezterm.font("Cica"),
     use_ime = false, --vimで日本語入力するときは、skkeletonを使っているから問題無い
+
+    font = wezterm.font("Cica"),
     fot_size = 10.0,
+    adjust_window_size_when_changing_font_size = false,
+
     hide_tab_bar_if_only_one_tab = true,
+
     color_scheme = 'duskfox',
     color_scheme_dirs = { "$HOME/.config/wezterm/colors/" },
+
     window_background_opacity = 0.8,
     window_padding = {
         left = 2,
@@ -14,4 +19,8 @@ return {
         top = 0,
         bottom = 0,
     },
+
+    selection_word_boundary = " \t\n{}[]()\"'`,;:",
+
+    disable_default_key_bindings = true,
 }
