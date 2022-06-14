d3262c1cb8a75863d5abfad0777b9336e86cb75c
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-06-15 00:10:56 +0900

    Add: script of wallpaper download.

---
+++ bin/dl_wallpaper.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# https://store.kde.org/p/1633675
+curl -o ~/Pictures/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
