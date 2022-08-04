692e258a4df184dce07025e21060fc14648387de
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-08-04 23:12:37 +0900

    Update: wall paper downloding place and settings

--- bin/dl_wallpaper.sh
+++ bin/dl_wallpaper.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # https://store.kde.org/p/1633675
-curl -o ~/Pictures/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
+curl --create-dirs -o ~/Pictures/wall_paper/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
