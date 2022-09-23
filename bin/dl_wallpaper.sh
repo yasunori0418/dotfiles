40956f30f68c55b48fa6ca6d32685892770c82d1
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-09-23 22:46:05 +0900

    rename: 壁紙のディレクトリ名を変更

--- bin/dl_wallpaper.sh
+++ bin/dl_wallpaper.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # https://store.kde.org/p/1633675
-curl --create-dirs -o ~/Pictures/wall_paper/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
+curl --create-dirs -o ~/Pictures/wallpaper/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
