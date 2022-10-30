3da9b5e22b22bb595f830a320a48cc72c9d09230
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-10-30 21:50:35 +0900

    Update: dl_wallpaper script

--- bin/dl_wallpaper.sh
+++ bin/dl_wallpaper.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
+DL_PATH=~/Pictures/wallpaper
+FILE_JPG=Nordic-darker.jpg
+FILE_PNG=Nordic-darker.png
+
 # https://store.kde.org/p/1633675
-curl --create-dirs -o ~/Pictures/wallpaper/Nordic-darker.jpg https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
+curl --create-dirs -o $DL_PATH/$FILE_JPG https://vsthemes.org/uploads/posts/2020-04/1586853771_daniel-leone-v7datklzzaw-unsplash-modded.jpg
+
+GEOMETRY=`xrandr | grep -F '*' | awk -F ' ' '{print $1}'`
+
+convert -resize $GEOMETRY! $DL_PATH/$FILE_JPG $DL_PATH/$FILE_PNG
+
+rm $DL_PATH/$FILE_JPG
