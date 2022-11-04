74d3764209fcbcd0dbe83a0af2fdd545a7901bd8
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-11-04 22:16:29 +0900

    Update: 使用してないスクリプトを削除

--- bin/startconky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conky -c ~/.conky/conky-ken/cpu
-sleep 1
-conky -c ~/.conky/conky-ken/mem
-sleep 5
-conky -c ~/.conky/conky-ken/rings
-#sleep 10
-#conky -c ~/.conky/conky-ken/weather
-conky -c /usr/share/conky/conky1.10_shortcuts_maia
