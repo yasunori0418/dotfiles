1cc144ecbd7b358037a6eddf0d663912f71ea9b7
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-11-14 23:39:59 +0900

    Update: 暫定的なインストールスクリプト

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
+cd $HOME
+
+git clone --recursive https://github.com/yasunori-kirin0418/dotfiles.git
+
+ln -svf ~/dotfiles/modules/nord-dircolors/src/dir_colors ~/dotfiles/home/.dir_colors
+ln -svf ~/dotfiles/modules/SKK_Keymap_L2X ~/dotfiles/config/libskk/rules/SKK_Keymap_L2X
+ln -svf ~/dotfiles/modules/friendly-snippets ~/dotfiles/home/.vsnip/friendly-snippets
+
 ln -svf ~/dotfiles/home/.?* ~/
 ln -svf ~/dotfiles/bin ~/
 ln -svf ~/dotfiles/config/* ~/.config/
