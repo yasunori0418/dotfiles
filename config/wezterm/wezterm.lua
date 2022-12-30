439648918c4b569116445b65bc0505c79e5b8e7b
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2022-12-31 00:37:52 +0900

    Update: wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -43,29 +43,6 @@ local my_keybinds = {
   {key = "]", mods = "ALT", action = wezterm.action({ActivatePaneDirection = 'Next'})},
 }
 
--- https://zenn.dev/yutakatay/articles/wezterm-intro#tips
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local user_title = tab.active_pane.user_vars.panetitle
-  if user_title ~= nil and #user_title > 0 then
-    return {
-      { Text = tab.tab_index + 1 .. ":" .. user_title },
-    }
-  end
-
-  local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
-  if title == "" then
-    local uri = utils.convert_home_dir(tab.active_pane.current_working_dir)
-    local basename = utils.basename(uri)
-    if basename == "" then
-      basename = uri
-    end
-    title = wezterm.truncate_right(basename, max_width)
-  end
-  return {
-    { Text = tab.tab_index + 1 .. ":" .. title },
-  }
-end)
-
 return {
   use_ime = false, --vimで日本語入力するときは、skkeletonを使っているから問題無い
 
