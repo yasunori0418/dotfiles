a9a871dd898296a1a40c2a1505a0bb4266ef6110
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-01-02 18:10:12 +0900

    Update:

---
+++ config/nvim/lua/plugins/null_ls.lua
@@ -0,0 +1,26 @@
+local null_ls = require('null-ls')
+
+local code_actions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+--local hover = null_ls.builtins.hover
+
+local sources = {
+  code_actions.gitsigns,
+  completion.vsnip,
+  formatting.stylua,
+  diagnostics.textlint.with({
+    condition = function()
+      return vim.fn.executable('textlint') > 0
+    end,
+    filetypes = { 'markdown' },
+    prefer_local = "node_modules/.bin",
+  }),
+}
+
+null_ls.setup({
+  border = 'single',
+  diagnostics_format = '[#{c}] #{m} (#{s})',
+  sources = sources,
+})
