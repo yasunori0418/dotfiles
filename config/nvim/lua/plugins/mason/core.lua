3d6628a8e11724077c3df048786e8acffc442589
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-01-02 18:23:19 +0900

    Update:

---
+++ config/nvim/lua/plugins/mason/core.lua
@@ -0,0 +1,10 @@
+require('mason').setup({
+  ui = {
+    border = 'single',
+    icons = {
+      package_installed   = '',
+      package_pending     = '',
+      package_uninstalled = '✗ ',
+    },
+  }
+})
