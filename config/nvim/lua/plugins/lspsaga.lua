d275e8af56ed4a09b8901b63d92f832480fdf262
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-01-02 18:06:11 +0900

    Update:

---
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,16 @@
+require('lspsaga').init_lsp_saga({
+  border_style = 'single',
+  finder_action_keys = {
+    open = 'o',
+    vsplit = 'v',
+    split = 's',
+    tabe = 't',
+    quit = { 'q', '<ESC>' },
+  },
+  rename_action_quit = '<ESC>',
+  rename_in_select = false,
+  show_outline = {
+    win_with = 'defx',
+    win_width = 40,
+  }
+})
