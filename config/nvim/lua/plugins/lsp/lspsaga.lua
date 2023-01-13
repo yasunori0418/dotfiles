c904f208b2148675687cfe78162ec5d5d522cfe8
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-01-13 20:49:31 +0900

    Update: lspsagaの設定を最新版に変更

--- config/nvim/lua/plugins/lsp/lspsaga.lua
+++ config/nvim/lua/plugins/lsp/lspsaga.lua
@@ -1,16 +1,62 @@
-require('lspsaga').init_lsp_saga({
-  border_style = 'single',
-  finder_action_keys = {
-    open = 'o',
-    vsplit = 'v',
-    split = 's',
-    tabe = 't',
-    quit = { 'q', '<ESC>' },
+require("lspsaga").setup({
+  ui = {
+    theme = "round",
+    border = "single",
   },
-  rename_action_quit = '<ESC>',
-  rename_in_select = false,
-  show_outline = {
-    win_width = 40,
+  finder = {
+    edit = { "o", "<CR>" },
+    vsplit = "v",
+    split = "s",
+    tabe = "t",
+    quit = { "q", "<ESC>" },
+  },
+  definition = {
+    edit = "<C-c>o",
+    vsplit = "<C-c>v",
+    split = "<C-c>s",
+    tabe = "<C-c>t",
+    quit = "q",
+    close = "<Esc>",
+  },
+  code_action = {
+    num_shortcut = true,
+    keys = {
+      quit = "q",
+      exec = "<CR>",
+    },
+  },
+  lightbulb = {
+    enable = true,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+  rename = {
+    quit = 'q',
+    exec = '<CR>',
+    in_select = false,
+  },
+  outline = {
+    win_position = 'right',
+    win_with = '',
+    win_width = 30,
+    show_detail = true,
     auto_preview = false,
+    auto_refresh = true,
+    auto_close = true,
+    custom_sort = nil,
+    keys = {
+      jump = 'o',
+      expand_collaspe = 'u',
+      quit = 'q',
+    },
+  },
+  symbol_in_winbar = {
+    enable = true,
+    separator = ' ',
+    hide_keyword = true,
+    show_file = false,
+    folder_level = 2,
   },
 })
