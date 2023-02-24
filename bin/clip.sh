614cf9a4e5c23c548c558f414417cc4bfd5fc80e
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-02-25 00:25:04 +0900

    Update: ちょいと小細工

--- bin/clip.sh
+++ bin/clip.sh
@@ -3,8 +3,9 @@
 # パク…参考にさせてもらいます。
 # https://gist.github.com/kuuote/4b2feda31d80fc8266fc562fc7f89c7f
 #
-tmp_file=/tmp/clip
-touch ${tmp_file}
-wezterm start --class Floaterm nvim --listen ~/.cache/nvim/server.pipe ${tmp_file} || exit 1
-head -c -1 ${tmp_file} | xsel -bi
-dunstify --appname=nvim "copied"
+clip_file=/tmp/clip
+[[ -f ${clip_file} ]] && rm ${clip_file}
+touch ${clip_file}
+wezterm start --class Floaterm nvim --listen ~/.cache/nvim/server.pipe ${clip_file} || exit 1
+head -c -1 ${clip_file} | xsel -bi
+dunstify --appname=nv_IME "copied in buffer text."
