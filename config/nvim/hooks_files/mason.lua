69f95441ec07157d626b7cca88c672a534a2bcf5
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-10 23:46:49 +0900

    Update:

---
+++ config/nvim/hooks_files/mason.lua
@@ -0,0 +1,12 @@
+-- lua_source {{{
+require("mason").setup({
+  ui = {
+    border = "single",
+    icons = {
+      package_installed = "",
+      package_pending = "",
+      package_uninstalled = "✗ ",
+    },
+  },
+})
+-- }}}
