3cbcbf861d15b216f0253e7d9080f21b0266d1fc
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-10 23:50:58 +0900

    Update:

---
+++ config/nvim/hooks_files/mason_lspconfig.lua
@@ -0,0 +1,33 @@
+-- lua_source {{{
+local mason_lspconfig = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
+--local keymap = vim.keymap.set
+local lsp = vim.lsp -- nvim lsp api.
+--local diag = vim.diagnostic -- nvim diagnostic api.
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "lua_ls",
+    "intelephense",
+    "pyright",
+    "yamlls",
+    "bashls",
+  },
+  automatic_installation = true,
+})
+
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    local lsp_options = {}
+
+    local capabilities = lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+    if server_name == "lua_ls" then
+      lsp_options.capabilities = capabilities
+    end
+
+    lspconfig[server_name].setup(lsp_options)
+  end,
+})
+-- }}}
