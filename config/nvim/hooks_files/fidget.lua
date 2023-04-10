ea02589c0d9833df9d250b7541c6f8f95b3ee819
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-10 23:56:21 +0900

    Update:

---
+++ config/nvim/hooks_files/fidget.lua
@@ -0,0 +1,16 @@
+-- lua_source {{{
+require("fidget").setup({
+  text = {
+    spinner = "dots",
+  },
+  window = {
+    border = "single",
+    relative = "editor",
+  },
+  sources = {
+    ["null-ls"] = {
+      ignore = true,
+    },
+  },
+})
+-- }}}
