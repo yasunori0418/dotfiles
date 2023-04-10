60eb4ba0c36b33ad9af89ac7ea89ff35ebb6238e
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-10 23:58:11 +0900

    Update:

---
+++ config/nvim/hooks_files/null_ls.lua
@@ -0,0 +1,33 @@
+-- lua_source {{{
+local null_ls = require("null-ls")
+
+local code_actions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+--local hover = null_ls.builtins.hover
+
+local sources = {
+  code_actions.gitsigns,
+  completion.vsnip,
+  formatting.stylua,
+  formatting.taplo,
+  formatting.yamlfmt,
+  formatting.textlint.with({
+    filetypes = { "markdown" },
+    prefer_local = "node_modules/.bin",
+  }),
+  diagnostics.yamllint,
+  diagnostics.hadolint,
+  diagnostics.textlint.with({
+    filetypes = { "markdown" },
+    prefer_local = "node_modules/.bin",
+  }),
+}
+
+null_ls.setup({
+  border = "single",
+  diagnostics_format = "[#{c}] #{m} (#{s})",
+  sources = sources,
+})
+-- }}}
