b97b32692dda18e431751b7401bf7d86192678d9
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-16 00:35:47 +0900

    Update: ノートPCでマルチモニターを使う設定

--- bin/multi_monitor_for_laptop.sh
+++ bin/multi_monitor_for_laptop.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
-wallpaper=~/Pictures/wallpaper/Nordic-darker.png
-feh --bg-fill $wallpaper $wallpaper
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
