0be05836835ee27bef9a495b49d73e0f2c9870aa
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-16 22:40:02 +0900

    Add: もう…終りでいいよね… ぬくもりからの脱却

---
+++ bin/nvim_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+neovim_repo=/home/yasunori/dev/neovim
+
+if [[ ${EUID} -ne 0 ]]; then
+  echo -e 'Parmission denied.\nPlease run as root.'
+  exit 1
+fi
+
+neovim_bin=/usr/local/bin/nvim
+neovim_runtime=/usr/local/share/nvim
+
+if [[ -f ${neovim_bin} ]]; then
+  rm -rf ${neovim_bin} ${neovim_runtime}
+fi
+
+cd ${neovim_repo}
+
+make install
+
+if [[ `command -v nvim` ]]; then
+  nvim --version
+  exit 0
+else
+  echo "Failure install of neovim."
+  exit 1
+fi
