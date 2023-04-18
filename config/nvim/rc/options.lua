d2cc29fb347aca30697198bd6da2f41e7a9881db
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-04-19 00:32:35 +0900

    Update: 実験的なコード

--- config/nvim/rc/options.lua
+++ config/nvim/rc/options.lua
@@ -1,5 +1,130 @@
 
--- Encoding {{{
+local option_tables = {
+
+  encoding_options = {
+    -- Use utf-8 to overall encoding.
+    encoding = 'utf-8',
+
+    -- Use utf-8 when file write.
+    fileencoding = 'utf-8',
+
+    -- Use file encodings when loaded.
+    fileencodings = { 'utf-8', 'sjis', 'iso-2022-jp', 'euc-jp' },
+
+    -- Automatic line feed code recognition.
+    fileformats = { 'unix', 'dos', 'mac' },
+  },
+
+  editor_options = {
+    -- Disable backup.
+    backup = false,
+
+    -- Don't make swapfile.
+    swapfile = false,
+
+    -- UNDOをどうぞ
+    undodir = '/home/yasunori/.cache/undo',--vim.fn.stdpath('cache') .. '/undo',
+    undofile = true,
+
+    -- East asia ambiguous character width problem.
+    ambiwidth = 'single',
+
+    autoread = true,
+
+    -- Use the clipboard on linux systems.
+    clipboard = { append = 'unnamedplus' },
+
+    -- Open diff mode vertically
+    diffopt = { append = 'vertical' },
+
+    -- For pum.vim
+    shortmess = { append = 'c' },
+
+    -- jump momentarily to a matching parenthesis
+    showmatch = true,
+    matchtime = 1,
+  },
+
+  display_options = {
+    -- Display rows number.
+    number = true,
+
+    -- Display relative rows number.
+    relativenumber = true,
+
+    -- Display current row cursorline.
+    cursorline = true,
+  },
+
+  indent_options = {
+    -- Smart indent.
+    smartindent = true,
+    autoindent = true,
+
+    -- Insert tab with half-width space.
+    expandtab = true,
+
+    -- The amount of blank space to insert with each command or smart indent.
+    shiftwidth = 2,
+
+    -- Tab width with 2 spaces.
+    tabstop = 2,
+
+    -- Insert a tab with 2 minutes of half-width space.
+    softtabstop = 2,
+  },
+
+  search_options = {
+    -- Highlight search results
+    hlsearch = true,
+
+    -- Incremental search.
+    -- Search starts when you enter the first character of the search word.
+    incsearch = true,
+
+    -- Search is not case sensitive
+    ignorecase = true,
+
+    -- Searching in lowercase ignores uppercase and lowercase
+    smartcase = true,
+
+    -- When the search progresses to the end of the file, search again from the beginning of the file.
+    wrapscan = true,
+  },
+
+  invisible_chars_option = {
+
+    -- Invisible characters
+    list = true,
+
+    -- Tab/End line Space/End line/No brake space.
+    listchars = {
+      tab = '»-',
+      space = '･',
+      trail = '･',
+      nbsp = '%',
+      eol = '↲',
+      extends = '»',
+      precedes = '«',
+    },
+  },
+
+}
+
+-- local options = {}
+
+for _, option_table in pairs(option_tables) do
+  for option_key, option_value in pairs(option_table) do
+    print('option_key: ' .. option_key)
+    if type(option_value) == 'table' then
+      for key, value in pairs(option_value) do
+        print(key .. ' => ' .. value)
+      end
+    else
+      print(option_value)
+    end
+  end
+end
 
 -- Use utf-8 to overall encoding.
 vim.opt.encoding = 'utf-8'
@@ -12,10 +137,6 @@ vim.opt.fileencodings = { 'utf-8', 'sjis', 'iso-2022-jp', 'euc-jp' }
 
 -- Automatic line feed code recognition.
 vim.opt.fileformats = { 'unix', 'dos', 'mac' }
-
--- }}}
-
--- EditorConfig {{{
 
 -- Disable backup.
 vim.opt.backup = false
@@ -45,10 +166,6 @@ vim.opt.shortmess:append('c')
 vim.opt.showmatch = true
 vim.opt.matchtime = 1
 
--- }}}
-
--- Display {{{
-
 -- Display rows number.
 vim.opt.number = true
 
@@ -57,10 +174,6 @@ vim.opt.relativenumber = true
 
 -- Display current row cursorline.
 vim.opt.cursorline = true
-
--- }}}
-
--- Search {{{
 
 -- Highlight search results
 vim.opt.hlsearch = true
@@ -78,10 +191,6 @@ vim.opt.smartcase = true
 -- When the search progresses to the end of the file, search again from the beginning of the file.
 vim.opt.wrapscan = true
 
--- }}}
-
--- Indent {{{
-
 -- Smart indent.
 vim.opt.smartindent = true
 vim.opt.autoindent = true
@@ -97,9 +206,6 @@ vim.opt.tabstop = 2
 
 -- Insert a tab with 2 minutes of half-width space.
 vim.opt.softtabstop = 2
--- }}}
-
--- Invisible characters{{{
 
 -- Invisible characters
 vim.opt.list = true
@@ -114,9 +220,8 @@ vim.opt.listchars = {
   extends = '»',
   precedes = '«',
 }
--- }}}
 
--- Disable default plugins {{{
+-- Disable default plugins
 -- Fast Startup Settings!!
 
 -- Disable TOhtml.
@@ -157,7 +262,3 @@ vim.g.did_indent_on             = true
 --vim.g.did_load_filetypes        = true
 --vim.g.did_load_ftplugin         = true
 vim.g.loaded_rrhelper           = true
-
--- }}}
-
--- vim:fdm=marker:fen
