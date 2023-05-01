c4f8ed8019fe9f08347b16768ef022de9c7a9b48
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-01 22:36:37 +0900

    Update: emmetの設定もlua化

---
+++ config/nvim/hooks/emmet.lua
@@ -0,0 +1,41 @@
+-- lua_add {{{
+
+vim.g.user_emmet_install_global = false
+-- vim.g.user_emmet_leader_key = "<C-y>"
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'html', 'css', 'scss'},
+  callback = function()
+    vim.cmd[[EmmetInstall]]
+  end,
+})
+
+local html5 = [[
+<!DOCTYPE html>
+<html lang="${lang}">
+  <head>
+    <meta charset="${charset}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    ${child}|
+  </body>
+</html>
+]]
+
+vim.g.user_emmet_settings = {
+  variables = {
+    lang = "ja",
+  },
+  html = {
+    snippets = {
+      html_5 = html5,
+      lrl_s = [[{{ | }}]],
+      lrl_e = [[{!! | !!}]],
+    },
+  },
+}
+-- }}}
