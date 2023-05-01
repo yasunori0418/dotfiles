c6fac1b35a7319c1fdee152ba076d280b1144c14
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-02 00:36:58 +0900

    refactor: autocmd周りのリファクタ…じゃなくて、早よ寝ないと……

--- config/nvim/lua/user/utils.lua
+++ config/nvim/lua/user/utils.lua
@@ -1,18 +1,31 @@
+local autocmd = vim.api.nvim_create_autocmd
 local M = {}
 
 ---全体共通設定のaugroup_id
 ---@return integer
 M.vimrc_augroup = vim.api.nvim_create_augroup('vimrc', {clear = false})
 
+---autocmd単発用
+---自動でvimrcグループにセットする
+---@param events table|string
+---@param pattern table|string
+---@param callback function|string
+M.autocmd_set = function (events, pattern, callback)
+  autocmd(events, {
+    group = M.vimrc_augroup,
+    pattern = pattern,
+    callback = callback,
+  })
+end
+
 ---複数のautocmdを定義する
----@param autocmds { events: string|table, group: integer, callback: function|string }
+---@param autocmds { events: string|table, pattern: table|string, group: integer, callback: function|string }
 M.autocmds_set = function (autocmds)
-  for _, autocmd in pairs(autocmds) do
-    vim.api.nvim_create_autocmd(
-      autocmd.events, {
-        group = autocmd.group,
-        pattern = autocmd.pattern,
-        callback = autocmd.callback,
+  for _, item in pairs(autocmds) do
+    autocmd(item.events, {
+      group = item.group,
+      pattern = item.pattern,
+      callback = item.callback,
     })
   end
 end
