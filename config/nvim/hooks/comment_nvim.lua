60a6d948e1b0ef2726b6591c2acea684682fe3b8
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-03 01:52:25 +0900

    Update:

--- config/nvim/hooks/comment_nvim.lua
+++ config/nvim/hooks/comment_nvim.lua
@@ -1,10 +1,10 @@
 -- lua_add {{{
 local co = require('user.utils').conditional_operator
-local keymaps_set = require('user.utils').keymaps_set
 
 local opt = { silent = true, noremap = false }
 local opt_expr = { silent = true, noremap = false, expr = true }
-keymaps_set{
+
+require('user.utils').keymaps_set{
   { mode = {"o", "n"},  lhs = [[gc]],   rhs = [[<Plug>(comment_toggle_linewise)]],          opts = opt },
   { mode = {"o", "n"},  lhs = [[gb]],   rhs = [[<Plug>(comment_toggle_blockwise)]],         opts = opt },
   { mode = {"x"},       lhs = [[gc]],   rhs = [[<Plug>(comment_toggle_linewise_visual)]],   opts = opt },
