a230b6772075bcc4d75a001bbb3f10f9caea7979
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-03 00:51:27 +0900

    Update: comment.nvimの設定をlua化
    いままでの設定と同じ内容で設定

---
+++ config/nvim/hooks/comment_nvim.lua
@@ -0,0 +1,37 @@
+-- lua_add {{{
+local co = require('user.utils').conditional_operator
+local keymaps_set = require('user.utils').keymaps_set
+
+local opt = { silent = true, noremap = false }
+local opt_expr = { silent = true, noremap = false, expr = true }
+keymaps_set{
+  { mode = {"o", "n"},  lhs = [[gc]],   rhs = [[<Plug>(comment_toggle_linewise)]],          opts = opt },
+  { mode = {"o", "n"},  lhs = [[gb]],   rhs = [[<Plug>(comment_toggle_blockwise)]],         opts = opt },
+  { mode = {"x"},       lhs = [[gc]],   rhs = [[<Plug>(comment_toggle_linewise_visual)]],   opts = opt },
+  { mode = {"x"},       lhs = [[gb]],   rhs = [[<Plug>(comment_toggle_blockwise)]],         opts = opt },
+  {
+    mode = {"n"},
+    lhs = [[gcc]],
+    rhs = function()
+      return co{
+        c = vim.v.count == 0,
+        t = [[<Plug>(comment_toggle_linewise_current)]],
+        f = [[<Plug>(comment_toggle_linewise_count)]]
+      }
+    end,
+    opts = opt_expr,
+  },
+  {
+    mode = {"n"},
+    lhs = [[gbc]],
+    rhs = function()
+      return co{
+        c = vim.v.count == 0,
+        t = [[<Plug>(comment_toggle_blockwise_current)]],
+        f = [[<Plug>(comment_toggle_blockwise_count)]]
+      }
+    end,
+    opts = opt_expr,
+  },
+}
+-- }}}
