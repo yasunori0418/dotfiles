28cc139fedd6eb1fd5c2e319dbe2abec236198bb
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-05 00:07:04 +0900

    Update: ginの設定をlua化

---
+++ config/nvim/hooks/gin.lua
@@ -0,0 +1,26 @@
+-- lua_add {{{
+local opt = {silent = true, noremap = true}
+require('user.utils').keymaps_set{
+  { mode = {"n"}, lhs = [[<Plug>(git)s]], rhs = [[<Cmd>GinStatus<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(git)l]], rhs = [[<Cmd>GinLog ++opener=vsplit --graph --all --abbrev-commit --oneline<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(git)pl]], rhs = [[<Cmd>Gin pull<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(git)ps]], rhs = [[<Cmd>Gin push<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(git)c]], rhs = [[<Cmd>Gin commit<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(git)b]], rhs = [[<Cmd>GinBranch<CR>]], opts = opt },
+}
+-- }}}
+
+-- lua_gin-log {{{
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+-- }}}
+
+-- lua_gin-status {{{
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+-- }}}
+
+-- lua_gin-branch {{{
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+-- }}}
