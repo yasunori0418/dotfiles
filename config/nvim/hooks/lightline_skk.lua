909d6200b3664d3f2dddca17a22beaef949d9597
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-04 14:16:50 +0900

    Update: lightline_skkの設定もlua化

---
+++ config/nvim/hooks/lightline_skk.lua
@@ -0,0 +1,9 @@
+-- lua_source {{{
+vim.fn['lightline_skk#option']('display', {
+  hiragana = 'あぁ﫦',
+  katakana = 'アァ﫦',
+  hankaku_katakana = 'ｱｧ﫦',
+  zenkaku_alphabet = 'Ａａ﫦',
+  alphabet = 'Aa﫦',
+})
+-- }}}
