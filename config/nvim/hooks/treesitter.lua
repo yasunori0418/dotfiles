e5b422db1dc445c714f86bd43a5e51dc68d55b7c
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-07 14:10:33 +0900

    fix: treesitterのfoldexprが動いてなかったので

--- config/nvim/hooks/treesitter.lua
+++ config/nvim/hooks/treesitter.lua
@@ -1,6 +1,6 @@
 -- lua_source {{{
 vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = vim.treesitter.foldexpr()
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldenable = false
 require("nvim-treesitter.configs").setup({
   ensure_installed = "all",
