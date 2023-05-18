0dce543540c7570aedbc96815dbe67840b3d4ea3
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-19 00:08:06 +0900

    docs: コメント付けておく

--- config/nvim/hooks/ddu.lua
+++ config/nvim/hooks/ddu.lua
@@ -6,7 +6,7 @@ require("user.utils").keymaps_set({
   { mode = "n", lhs = [[ f]], rhs = [[<Plug>(ddu-filer)]], opts = opt },
 
   -- ddu-ui-ff starter
-  {
+  { -- current-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)a]],
     rhs = function()
@@ -21,7 +21,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- dorfiles-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)d]],
     rhs = function()
@@ -29,7 +29,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- help-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)h]],
     rhs = function()
@@ -37,7 +37,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- buffer-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)b]],
     rhs = function()
@@ -45,7 +45,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- plugin-list-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)P]],
     rhs = function()
@@ -53,7 +53,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- project-list-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)p]],
     rhs = function()
@@ -61,7 +61,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- home-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)~]],
     rhs = function()
@@ -69,7 +69,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- register-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)r]],
     rhs = function()
@@ -77,7 +77,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- ripgrep-ff live grep
     mode = "n",
     lhs = [[<Plug>(ddu-ff)s]],
     rhs = function()
@@ -85,7 +85,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- mrr-ff most recent repositories
     mode = "n",
     lhs = [[<Plug>(ddu-ff)m]],
     rhs = function()
@@ -93,7 +93,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- mru-ff most recent used files
     mode = "n",
     lhs = [[<Plug>(ddu-ff)n]],
     rhs = function()
@@ -101,7 +101,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- search_line-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)/]],
     rhs = function()
@@ -109,7 +109,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- hightlight-ff
     mode = "n",
     lhs = [[<Plug>(ddu-ff)C]],
     rhs = function()
@@ -119,7 +119,7 @@ require("user.utils").keymaps_set({
   },
 
   -- ddu-ui-filer starter
-  {
+  { -- project_root-filer
     mode = "n",
     lhs = [[<Plug>(ddu-filer)a]],
     rhs = function()
@@ -134,7 +134,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- current-filer open filer at current buffer directory
     mode = "n",
     lhs = [[<Plug>(ddu-filer)f]],
     rhs = function()
@@ -149,7 +149,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- dotfiles-filer
     mode = "n",
     lhs = [[<Plug>(ddu-filer)d]],
     rhs = function()
@@ -157,7 +157,7 @@ require("user.utils").keymaps_set({
     end,
     opts = opt,
   },
-  {
+  { -- home-filer
     mode = "n",
     lhs = [[<Plug>(ddu-filer)~]],
     rhs = function()
