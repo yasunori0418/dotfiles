a3b015c10d9515a1c64d399455a5c71dc38a7fc7
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-21 22:49:23 +0900

    Update: lspのキーマップをlua化

--- config/nvim/hooks/mason_lspconfig.lua
+++ config/nvim/hooks/mason_lspconfig.lua
@@ -1,3 +1,25 @@
+-- lua_add {{{
+local opt = { noremap = true, silent = true }
+require('user.utils').keymaps_set({
+  { mode = {"n"}, lhs = [[ l]], rhs = [[<Plug>(lsp)]], opts = {} },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)k]], rhs = [[<Cmd>Lspsaga hover_doc<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)K]], rhs = [[<Cmd>Lspsaga lsp_finder<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)r]], rhs = [[<Cmd>Lspsaga rename<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)o]], rhs = [[<Cmd>Lspsaga outline<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)c]], rhs = [[<Cmd>Lspsaga code_action<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)h]], rhs = vim.lsp.buf.signature_help, opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)f]], rhs = vim.lsp.buf.format, opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)i]], rhs = vim.lsp.buf.implementation, opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)e]], rhs = [[<Cmd>Lspsaga show_line_diagnostics<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)E]], rhs = [[<Cmd>Lspsaga show_cursor_diagnostics<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[<Plug>(lsp)q]], rhs = [[<Cmd>Lspsaga show_buf_diagnostics<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[gd]], rhs = [[<Cmd>Lspsaga peek_definition<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [[gD]], rhs = vim.lsp.buf.declaration, opts = opt },
+  { mode = {"n"}, lhs = [=[[d]=], rhs = [[<Cmd>Lspsaga diagnostic_jump_prev<CR>]], opts = opt },
+  { mode = {"n"}, lhs = [=[]d]=], rhs = [[<Cmd>Lspsaga diagnostic_jump_next<CR>]], opts = opt },
+})
+-- }}}
+
 -- lua_source {{{
 local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
