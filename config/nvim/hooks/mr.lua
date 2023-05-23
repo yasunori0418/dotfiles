75ba736c4fdb81650a7c245620722ea9398b6e5f
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-23 23:52:50 +0900

    fix: mr.vimのlua設定を修正

--- config/nvim/hooks/mr.lua
+++ config/nvim/hooks/mr.lua
@@ -1,7 +1,7 @@
 -- lua_add {{{
 vim.g['mr#mru#predicates'] = {
   function(filename)
-    return filename ~= 'COMMIT_EDITMSG'
+    return vim.regex([[COMMIT_EDITMSG]]):match_str(filename) == nil
   end,
 }
 -- }}}
