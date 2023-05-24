0f1fbf59354091242ae5f9e7f3d4200532893ab7
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-24 23:27:41 +0900

    Update: いったん雛形を作っておく

---
+++ config/nvim/lua/user/plugins/skkeleton.lua
@@ -0,0 +1,18 @@
+local M = {}
+local config = vim.fn['skkeleton#config']
+local register_kanatable = vim.fn['skkeleton#register_kanatable']
+local register_keymap = vim.fn['skkeleton#register_keymap']
+
+M.init = function()
+end
+
+
+M.pre = function()
+end
+
+
+M.post = function()
+end
+
+
+return M
