48a89c4547b359fd8541e038cbb7427a18e17bf9
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-26 23:43:30 +0900

    Update: L2Xキーマップ設定のlua化

--- config/nvim/lua/user/plugins/skkeleton.lua
+++ config/nvim/lua/user/plugins/skkeleton.lua
@@ -1,13 +1,75 @@
 local M = {}
 local config = vim.fn['skkeleton#config']
 local register_kanatable = vim.fn['skkeleton#register_kanatable']
--- local register_keymap = vim.fn['skkeleton#register_keymap']
+local register_keymap = vim.fn['skkeleton#register_keymap']
 local ddc_custom_get_buffer = vim.fn['ddc#custom#get_buffer']
 local ddc_custom_set_buffer = vim.fn['ddc#custom#set_buffer']
 local ddc_custom_patch_buffer = vim.fn['ddc#custom#patch_buffer']
 
--- local L2X = function()
--- end
+local l2x_rom = function()
+  local rom = {}
+  local disable_l_nexts = vim.fn.split(
+    [=[bcdfghjkmnpqrsvxzBCDFGHJKMNPQRSVXZ,./1234567890-+=`~;:[]{}()<>!@#$%^&*_\"']=],
+    [[\zs]]
+  )
+
+  vim.fn.map(disable_l_nexts, function(_, value)
+      rom['l' .. value] = { '', '' }
+    end
+  )
+
+  local converts = {
+    la = { 'ぁ', '' },
+    li = { 'ぃ', '' },
+    lu = { 'ぅ', '' },
+    le = { 'ぇ', '' },
+    lo = { 'ぉ', '' },
+    ll = { 'っ', 'l' },
+    ltu = { 'っ', '' },
+    ltsu = { 'っ', '' },
+    lwa = { 'ゎ', '' },
+    lwe = { 'ゑ', '' },
+    lwi = { 'ゐ', '' },
+    lya = { 'ゃ', '' },
+    lyo = { 'ょ', '' },
+    lyu = { 'ゅ', '' },
+    xa = { '', '' },
+    xi = { '', '' },
+    xu = { '', '' },
+    xe = { '', '' },
+    xo = { '', '' },
+    xx = { '', '' },
+    xtu = { '', '' },
+    xtsu = { '', '' },
+    xwa = { '', '' },
+    xwe = { '', '' },
+    xwi = { '', '' },
+    xya = { '', '' },
+    xyo = { '', '' },
+    xyu = { '', '' },
+  }
+
+  vim.fn.map(converts, function(key, value)
+      rom[key] = value
+    end
+  )
+
+  return rom
+end
+
+
+local l2x_maps = function()
+  register_kanatable('rom',l2x_rom())
+  register_keymap('input', 'x', 'disable')
+  register_keymap('input', 'X', 'zenkaku')
+  register_kanatable('rom', {
+    ['<s-x>'] = 'zenkaku'
+  })
+  register_kanatable('rom', {
+    ['<s-l>'] =  { 'L', '' }
+    }, true
+  )
+end
 
 
 M.init = function()
@@ -31,6 +93,7 @@ M.init = function()
     [')'] = { '）', '' },
     }
   )
+  l2x_maps()
 end
 
 
@@ -42,6 +105,7 @@ end
 
 M.post = function()
   ddc_custom_set_buffer(vim.b.prev_buffer_config)
+  vim.b.prev_buffer_config = {}
 end
 
 
