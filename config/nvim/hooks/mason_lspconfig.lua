688dad7bdd1d891b4384b1d962e6fa4a688d262b
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-05-29 22:34:55 +0900

    Update: textlintのフォーマットでタイムアウトしちゃうからね…

--- config/nvim/hooks/mason_lspconfig.lua
+++ config/nvim/hooks/mason_lspconfig.lua
@@ -6,13 +6,20 @@ require("user.utils").keymaps_set({
   { mode = { "n" }, lhs = [[mk]],   rhs = lsp_function.buf.hover,          opts = opt },
   { mode = { "n" }, lhs = [[ma]],   rhs = lsp_function.buf.code_action,    opts = opt },
   { mode = { "n" }, lhs = [[mr]],   rhs = lsp_function.buf.rename,         opts = opt },
-  { mode = { "n" }, lhs = [[gq]],   rhs = lsp_function.buf.format,         opts = opt },
   { mode = { "n" }, lhs = [[gi]],   rhs = lsp_function.buf.implementation, opts = opt },
   { mode = { "n" }, lhs = [[gd]],   rhs = lsp_function.buf.definition,     opts = opt },
   { mode = { "n" }, lhs = [[gD]],   rhs = lsp_function.buf.declaration,    opts = opt },
   { mode = { "n" }, lhs = [[ge]],   rhs = diagnostic.open_float,           opts = opt },
   { mode = { "n" }, lhs = [=[[d]=], rhs = diagnostic.goto_prev,            opts = opt },
   { mode = { "n" }, lhs = [=[]d]=], rhs = diagnostic.goto_next,            opts = opt },
+  {
+    mode = { "n" },
+    lhs = [[gq]],
+    rhs = function()
+      lsp_function.buf.format({ timeout_ms = 5000 })
+    end,
+    opts = opt,
+  },
 })
 -- }}}
 
