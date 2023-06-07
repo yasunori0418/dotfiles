b888a85a6e8485414836bf26db2ad2fedf92c420
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-06-08 01:01:58 +0900

    Update: skkで使いたい辞書をマージするやつ
    jawikiをマージする

--- bin/skk_dict.sh
+++ bin/skk_dict.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-skk_dict_dir=${HOME}/.cache/skk_dict
-skk_dir=${HOME}/.skk
-
 if [[ ! $(command -v skkdic-expr2) ]]; then
   skktools.sh install
 fi
 
-if [[ ! -d ${skk_dict_dir} ]]; then
-  git clone https://github.com/skk-dev/dict.git ${skk_dict_dir}
-fi
-cd ${skk_dict_dir}
+# SKK Openlabメンテナンスの辞書
+openlab_dict=${XDG_CACHE_HOME}/skk_dict
+[[ ! -d ${openlab_dict} ]] && git clone https://github.com/skk-dev/dict.git ${openlab_dict}
+cd ${openlab_dict}
+git pull
 
+# tokuhirom/jawiki-kana-kanji-dict 日本語wikipediaの内容をSKKの辞書化した物
+jawiki_dict=${XDG_CACHE_HOME}/jawiki_dict
+[[ ! -d ${jawiki_dict} ]] && git clone https://github.com/tokuhirom/jawiki-kana-kanji-dict.git ${jawiki_dict}
+cd ${jawiki_dict}
+git pull
+
+skk_dir=${HOME}/.skk
 [[ ! -d ${skk_dir} ]] && mkdir ${skk_dir}
 
-skkdic-expr2 -o ${skk_dir}/skk_dict_merged SKK-JISYO.L +  SKK-JISYO.propernoun
-cp ${skk_dict_dir}/SKK-JISYO.emoji ${skk_dir}/SKK-JISYO.emoji
+skkdic-expr2 -o \
+${skk_dir}/skk_dict_merged \
+${openlab_dict}/SKK-JISYO.L \
++ ${openlab_dict}/SKK-JISYO.propernoun \
++ ${jawiki_dict}/SKK-JISYO.jawiki
+
+cp ${openlab_dict}/SKK-JISYO.emoji ${skk_dir}/SKK-JISYO.emoji
