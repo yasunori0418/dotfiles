c521b916fb439948e06f49d3c1b8060a6bc52ce3
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-06-08 22:52:19 +0900

    formated

--- config/nvim/lua/user/plugins/skkeleton.lua
+++ config/nvim/lua/user/plugins/skkeleton.lua
@@ -1,10 +1,10 @@
 local M = {}
-local config = vim.fn['skkeleton#config']
-local register_kanatable = vim.fn['skkeleton#register_kanatable']
-local register_keymap = vim.fn['skkeleton#register_keymap']
-local ddc_custom_get_buffer = vim.fn['ddc#custom#get_buffer']
-local ddc_custom_set_buffer = vim.fn['ddc#custom#set_buffer']
-local ddc_custom_patch_buffer = vim.fn['ddc#custom#patch_buffer']
+local config = vim.fn["skkeleton#config"]
+local register_kanatable = vim.fn["skkeleton#register_kanatable"]
+local register_keymap = vim.fn["skkeleton#register_keymap"]
+local ddc_custom_get_buffer = vim.fn["ddc#custom#get_buffer"]
+local ddc_custom_set_buffer = vim.fn["ddc#custom#set_buffer"]
+local ddc_custom_patch_buffer = vim.fn["ddc#custom#patch_buffer"]
 
 local l2x_rom = function()
   local rom = {}
@@ -14,99 +14,89 @@ local l2x_rom = function()
   )
 
   vim.fn.map(disable_l_nexts, function(_, value)
-      rom['l' .. value] = { '', '' }
-    end
-  )
+    rom["l" .. value] = { "", "" }
+  end)
 
   local converts = {
-    la = { 'ぁ', '' },
-    li = { 'ぃ', '' },
-    lu = { 'ぅ', '' },
-    le = { 'ぇ', '' },
-    lo = { 'ぉ', '' },
-    ll = { 'っ', 'l' },
-    ltu = { 'っ', '' },
-    ltsu = { 'っ', '' },
-    lwa = { 'ゎ', '' },
-    lwe = { 'ゑ', '' },
-    lwi = { 'ゐ', '' },
-    lya = { 'ゃ', '' },
-    lyo = { 'ょ', '' },
-    lyu = { 'ゅ', '' },
-    xa = { '', '' },
-    xi = { '', '' },
-    xu = { '', '' },
-    xe = { '', '' },
-    xo = { '', '' },
-    xx = { '', '' },
-    xtu = { '', '' },
-    xtsu = { '', '' },
-    xwa = { '', '' },
-    xwe = { '', '' },
-    xwi = { '', '' },
-    xya = { '', '' },
-    xyo = { '', '' },
-    xyu = { '', '' },
+    la = { "ぁ", "" },
+    li = { "ぃ", "" },
+    lu = { "ぅ", "" },
+    le = { "ぇ", "" },
+    lo = { "ぉ", "" },
+    ll = { "っ", "l" },
+    ltu = { "っ", "" },
+    ltsu = { "っ", "" },
+    lwa = { "ゎ", "" },
+    lwe = { "ゑ", "" },
+    lwi = { "ゐ", "" },
+    lya = { "ゃ", "" },
+    lyo = { "ょ", "" },
+    lyu = { "ゅ", "" },
+    xa = { "", "" },
+    xi = { "", "" },
+    xu = { "", "" },
+    xe = { "", "" },
+    xo = { "", "" },
+    xx = { "", "" },
+    xtu = { "", "" },
+    xtsu = { "", "" },
+    xwa = { "", "" },
+    xwe = { "", "" },
+    xwi = { "", "" },
+    xya = { "", "" },
+    xyo = { "", "" },
+    xyu = { "", "" },
   }
 
   vim.fn.map(converts, function(key, value)
-      rom[key] = value
-    end
-  )
+    rom[key] = value
+  end)
 
   return rom
 end
 
-
 local l2x_maps = function()
-  register_kanatable('rom',l2x_rom())
-  register_keymap('input', 'x', 'disable')
-  register_keymap('input', 'X', 'zenkaku')
-  register_kanatable('rom', {
-    ['<s-x>'] = 'zenkaku'
+  register_kanatable("rom", l2x_rom())
+  register_keymap("input", "x", "disable")
+  register_keymap("input", "X", "zenkaku")
+  register_kanatable("rom", {
+    ["<s-x>"] = "zenkaku",
   })
-  register_kanatable('rom', {
-    ['<s-l>'] =  { 'L', '' }
-    }, true
-  )
+  register_kanatable("rom", {
+    ["<s-l>"] = { "L", "" },
+  }, true)
 end
-
 
 M.init = function()
   config({
     eggLikeNewline = true,
     globalDictionaries = {
       { [[~/.skk/skk_dict_merged]], [[euc-jp]] },
-      { [[~/.skk/SKK-JISYO.emoji]], [[utf-8]] }
+      { [[~/.skk/SKK-JISYO.emoji]], [[utf-8]] },
     },
     userJisyo = [[~/.skk/skkeleton]],
-    completionRankFile = [[~/.skk/rank.json]]
-    }
-  )
+    completionRankFile = [[~/.skk/rank.json]],
+  })
 
-  register_kanatable('rom', {
-    ['jj'] = 'escape',
-    ['z '] = { '　', '' },
-    ['~'] = { '～', '' },
-    ['z0'] = { '○', '' },
-    ['('] = { '（', '' },
-    [')'] = { '）', '' },
-    }
-  )
+  register_kanatable("rom", {
+    ["jj"] = "escape",
+    ["z "] = { "　", "" },
+    ["~"] = { "～", "" },
+    ["z0"] = { "○", "" },
+    ["("] = { "（", "" },
+    [")"] = { "）", "" },
+  })
   l2x_maps()
 end
 
-
 M.pre = function()
   vim.b.prev_buffer_config = ddc_custom_get_buffer()
-  ddc_custom_patch_buffer('sources', { 'skkeleton' })
+  ddc_custom_patch_buffer("sources", { "skkeleton" })
 end
-
 
 M.post = function()
   ddc_custom_set_buffer(vim.b.prev_buffer_config)
   vim.b.prev_buffer_config = {}
 end
-
 
 return M
