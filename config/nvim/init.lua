9b062de5fff82b7406c51b363b99cf015a944e54
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-06-13 22:19:48 +0900

    Update: Fast startup hack of vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,8 @@
+-- Fast startup hack of vim...
+vim.cmd[[filetype off]]
+vim.cmd[[filetype plugin indent off]]
+vim.cmd[[syntax off]]
+
 -- Enables the experimental Lua module loader
 if vim.loader then
   vim.loader.enable()
