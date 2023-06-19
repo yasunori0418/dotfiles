44b9ee67ca0095ab2ff2d9eb94c65226594dd505
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-06-19 23:03:45 +0900

    Update: 光のプラグイン故、弄れるのはこれだけか…

--- config/nvim/hooks/illuminate.lua
+++ config/nvim/hooks/illuminate.lua
@@ -15,14 +15,24 @@ require("illuminate").configure({
   filetype_overrides = {},
   -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
   filetypes_denylist = {
-    "dirvish",
-    "fugitive",
+    "gin-status",
+    "gin-branch",
+    "gin-log",
+    "ddu-ff-filter",
+    "ddu-ff",
+    "ddu-filer",
+    "deol",
   },
   -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
   filetypes_allowlist = {},
   -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
   -- See `:help mode()` for possible values
-  modes_denylist = {},
+  modes_denylist = {
+    "i",
+    "s",
+    "c",
+    "R",
+  },
   -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
   -- See `:help mode()` for possible values
   modes_allowlist = {},
