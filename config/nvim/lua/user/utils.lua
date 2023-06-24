2da1da523c00506053a75579ccc8b0140e1c29c4
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-06-24 09:38:02 +0900

    fix: 結合するディレクトリのパスをミスってた

--- config/nvim/lua/user/utils.lua
+++ config/nvim/lua/user/utils.lua
@@ -104,7 +104,7 @@ M.gather_files = function(base_dir, config_dir_name)
   local config_dir = base_dir .. "/" .. config_dir_name
   return vim.iter(vim.fn.systemlist("ls " .. config_dir))
       :map(function(file_name)
-        return vim.g.base_dir .. "/" .. file_name
+        return config_dir .. "/" .. file_name
       end)
       :totable()
 end
