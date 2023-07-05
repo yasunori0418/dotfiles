8a07ffc03fd3bc2d41d882961a67ef37f451cc77
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-05 23:10:16 +0900

    Update:

--- config/nvim/hooks/previm/previm.lua
+++ config/nvim/hooks/previm/previm.lua
@@ -10,6 +10,6 @@ require('user.utils').keymaps_set{
 -- lua_source {{{
 vim.g.previm_enable_realtime = true
 vim.g.previm_disable_default_css = true
-vim.g.previm_custom_css_path = vim.fn.expand('~/dotfiles/tmp/previm_markdown.css')
+vim.g.previm_custom_css_path = vim.fs.joinpath(vim.g.hooks_dir, 'previm', 'previm_markdown.css')
 vim.g.previm_plantuml_imageprefix = 'http://localhost:58080/png/'
 -- }}}
