c0dffdae2ba886ae33865f29627e2ddf0a88cf4f
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-08 13:41:48 +0900

    fixed: dial.nvimのdefaultの設定に対しての追加にはならない
    素直にデフォ設定も定義しておく

--- config/nvim/hooks/dial.lua
+++ config/nvim/hooks/dial.lua
@@ -85,6 +85,27 @@ local config = require("dial.config")
 
 config.augends:register_group({
   default = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.date.new {
+			pattern = "%Y/%m/%d",
+			default_kind = "day",
+		},
+		augend.date.new {
+			pattern = "%Y-%m-%d",
+			default_kind = "day",
+		},
+		augend.date.new {
+			pattern = "%m/%d",
+			default_kind = "day",
+			only_valid = true,
+		},
+		augend.date.new {
+			pattern = "%H:%M",
+			default_kind = "day",
+			only_valid = true,
+		},
+		augend.constant.alias.ja_weekday_full,
     augend.constant.alias.bool,
     augend.constant.alias.ja_weekday_full,
     augend.constant.alias.ja_weekday,
