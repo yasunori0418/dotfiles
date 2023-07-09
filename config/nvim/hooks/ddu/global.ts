2e48db4ef1cc6cf0a1d7fab719b0aa7eaedd6838
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-10 00:00:07 +0900

    Update: とりあえずエラーを出さないように書いたレベルの物

--- config/nvim/hooks/ddu/global.ts
+++ config/nvim/hooks/ddu/global.ts
@@ -11,8 +11,39 @@ import * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts"
 // type Params = Record<string, unknown>;
 
 export class Config extends BaseConfig {
-  override config(args: ConfigArguments): Promise<void> {
+  override async config(args: ConfigArguments): Promise<void> {
+    const winWidth = await opt.columns.get(args.denops);
+    const winHeight = await opt.lines.get(args.denops);
     args.contextBuilder.patchGlobal({
+      uiOptions: {
+        filer: {
+          toggle: true,
+        },
+      },
+      uiParams: {
+        ff: {
+          split: "floating",
+          floatingBorder: "single",
+          prompt: "",
+          filterSplitDirection: "floating",
+          filterFloatingPosition: "top",
+          displaySourceName: "long",
+          previewFloating: true,
+          previewFloatingBorder: "double",
+          previewSplit: "horizontal",
+        },
+        filer: {
+          split: "vertical",
+          splitDirection: "topleft",
+          winWidth: Math.floor(winWidth / 6),
+          previewFloating: true,
+          previewFloatingBorder: "single",
+          previewCol: Math.floor(winWidth / 4),
+          previewRow: winHeight,
+          previewWidth: winWidth,
+          previewHeight: 20,
+        },
+      },
     });
     return Promise.resolve();
   }
