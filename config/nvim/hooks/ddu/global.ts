ba4b517ba9558a7acabe6f9ddd4991edc591deaa
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-16 18:15:19 +0900

    chore: 依存パッケージの更新

--- config/nvim/hooks/ddu/global.ts
+++ config/nvim/hooks/ddu/global.ts
@@ -2,11 +2,11 @@ import {
   ActionArguments,
   ActionFlags,
   BaseConfig,
-} from "https://deno.land/x/ddu_vim@v3.2.7/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.2.7/base/config.ts";
+} from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
+import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.4.2/base/config.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.2.7/deps.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.2/file.ts";
-import * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts"
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+import * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
 
 type Params = Record<string, unknown>;
 
