d90e686d4133b3fdcfbc464c9377870e554effd0
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-27 23:16:00 +0900

    Update:

--- config/nvim/hooks/ddu/helper/deps.ts
+++ config/nvim/hooks/ddu/helper/deps.ts
@@ -3,7 +3,7 @@ export {
   BaseConfig,
 } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
 export * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
-export * from "./ui_size.ts";
+export * from "./helper.ts";
 
 // Type definitions.
 export type { ActionArguments } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
