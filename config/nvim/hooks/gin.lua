6e542d1810895f66b36c9f496c7af7d7153b094d
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-29 22:09:02 +0900

    formatted

--- config/nvim/hooks/gin.lua
+++ config/nvim/hooks/gin.lua
@@ -1,19 +1,59 @@
 -- lua_add {{{
-local opt = {silent = true, noremap = true}
-require('user.utils').keymaps_set{
-  { mode = {"n"}, lhs = [[<C-g>]], rhs = [[<Plug>(git)]], opts = {} },
-  { mode = {"n"}, lhs = [[<Plug>(git)s]], rhs = [[<Cmd>GinStatus<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)l]], rhs = [[<Cmd>GinLog ++opener=vsplit --graph --all --abbrev-commit --oneline<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)pl]], rhs = [[<Cmd>Gin pull<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)ps]], rhs = [[<Cmd>Gin push<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)c]], rhs = [[<Cmd>Gin commit<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)b]], rhs = [[<Cmd>GinBranch<CR>]], opts = opt },
-  { mode = {"n"}, lhs = [[<Plug>(git)B]], rhs = [[<Cmd>Gina blame<CR>]], opts = opt },
-}
+local opt = { silent = true, noremap = true }
+require("user.utils").keymaps_set({
+  {
+    mode = { "n" },
+    lhs = [[<C-g>]],
+    rhs = [[<Plug>(git)]],
+    opts = {},
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)s]],
+    rhs = [[<Cmd>GinStatus<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)l]],
+    rhs = [[<Cmd>GinLog ++opener=vsplit --graph --all --abbrev-commit --oneline<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)pl]],
+    rhs = [[<Cmd>Gin pull<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)ps]],
+    rhs = [[<Cmd>Gin push<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)c]],
+    rhs = [[<Cmd>Gin commit<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)b]],
+    rhs = [[<Cmd>GinBranch<CR>]],
+    opts = opt,
+  },
+  {
+    mode = { "n" },
+    lhs = [[<Plug>(git)B]],
+    rhs = [[<Cmd>Gina blame<CR>]],
+    opts = opt,
+  },
+})
 -- }}}
 
 -- lua_source {{{
-vim.fn['gina#custom#command#option']('blame', '--opener', 'tabedit')
+vim.fn["gina#custom#command#option"]("blame", "--opener", "tabedit")
 -- }}}
 
 -- lua_gin-log {{{
