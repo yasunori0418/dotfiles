b1f06d066bb302dd4cb3254fce8fec8b60a12b57
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-30 18:23:51 +0900

    chore: ddu helper deps update

--- config/nvim/hooks/ddu/helper/deps.ts
+++ config/nvim/hooks/ddu/helper/deps.ts
@@ -1,13 +1,13 @@
 export {
   ActionFlags,
   BaseConfig,
-} from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
 export * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
 export * from "./helper.ts";
 
 // Type definitions.
-export type { ActionArguments } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
-export type { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.4.3/base/config.ts";
+export type { ActionArguments } from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
+export type { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.4.4/base/config.ts";
 export type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
 export type {
   ActionData as GitCommitActionData
