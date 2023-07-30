80f722bfef3547fca2fb263fb7336eab8961af76
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-07-30 22:28:13 +0900

    test: Github Action用にバージョンを下げてみる

--- config/nvim/hooks/ddu/helper/deps.ts
+++ config/nvim/hooks/ddu/helper/deps.ts
@@ -1,14 +1,14 @@
 export {
   ActionFlags,
   BaseConfig,
-} from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
-export * as opt from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
+} from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
+export * as opt from "https://deno.land/x/denops_std@v5.0.0/option/mod.ts";
 export * from "./helper.ts";
 
 // Type definitions.
-export type { ActionArguments } from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
-export type { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.4.4/base/config.ts";
-export type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+export type { ActionArguments } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
+export type { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.4.3/base/config.ts";
+export type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.2/file.ts";
 export type {
   ActionData as GitCommitActionData
 } from "https://raw.githubusercontent.com/kyoh86/ddu-source-git_log/main/denops/%40ddu-kinds/git_commit.ts";
