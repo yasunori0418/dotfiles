2d05c18a204e8342a3fe6181bb4e1f488f76597a
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-13 01:21:30 +0900

    Add: pkglistを更新してくれる奴

---
+++ scripts/update_pkglist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+host_type=$(uname -n | awk -F '-' '{print $2}')
+
+yay -Qeqn | rg -v "nvidia|amd" > ./document/pkglist.txt
+yay -Qeqm | rg -v "rtl8812au|epsonscan" > ./document/pkglist-foreign.txt
+
+if [[ ${host_type} == 'laptop' ]]; then
+  yay -Qeq | rg "amd" > ./document/pkglist-laptop.txt
+elif [[ ${host_type} == 'desktop' ]]; then
+  yay -Qeq | rg "nvidia|rtl8812au|epsonscan" > ./document/pkglist-desktop.txt
+fi
