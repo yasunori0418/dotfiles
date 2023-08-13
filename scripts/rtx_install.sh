36f84fa42269515b0c1bf40d3431595186d98df0
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-13 16:28:41 +0900

    refactor: rtx installer

--- scripts/rtx_install.sh
+++ scripts/rtx_install.sh
@@ -4,14 +4,19 @@ set -e -u -o pipefail
 
 dotfiles=${HOME}/dotfiles
 readonly dotfiles
+
 modules=${dotfiles}/modules
 readonly modules
+
 bin=${dotfiles}/bin/
 readonly bin
 
 latest_releases=https://api.github.com/repos/jdxcode/rtx/releases/latest
+readonly latest_releases
+
 dl_file=${modules}/rtx.tar.gz
 readonly dl_file
+
 rtx_bin=${modules}/rtx/bin/rtx
 readonly rtx_bin
 
