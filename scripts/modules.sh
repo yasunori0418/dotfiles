959d34ca7e1c9ee0821f2cd8c44bbb442ce61ba5
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-13 22:09:01 +0900

    Update:

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -10,7 +10,7 @@ declare -r local_bin=${HOME}/.local/bin
 declare -Ar modules=(
   # key => {ユーザー名}/{リポジトリ名}
   # value => リンク対象:symlink先
-  ["yasunori0418/SKK_Keymap_L2X"]="{keymap,rom-kana,metadata.json}:${dotfiles}/config/libskk/rules/L2X"
+  ["yasunori0418/SKK_Keymap_L2X"]=":${dotfiles}/config/libskk/rules/L2X"
   ["rafamadriz/friendly-snippets"]="snippets:${dotfiles}/config/nvim/snippet/vsnip/friendly-snippets"
   ["jluttine/rofi-power-menu"]="rofi-power-menu:${local_bin}/rofi-power-menu"
   ["arcticicestudio/nord-dircolors"]="src/dir_colors:${dotfiles}/home/.dir_colors"
@@ -25,5 +25,5 @@ for module in ${!modules[@]}; do
     | awk -F ':' '{print $1,$2}' \
     | declare targets=$(cat)
 
-  ln -svf ${target} ${link}
+  ln -svf ${targets}
 done
