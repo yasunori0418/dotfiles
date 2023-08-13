5a8cfaee0f6aebb8f69cb7f0a248634f049e5770
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-13 23:25:21 +0900

    Update:

--- scripts/update_pkglist.sh
+++ scripts/update_pkglist.sh
@@ -2,11 +2,11 @@
 
 host_type=$(uname -n | awk -F '-' '{print $2}')
 
-yay -Qeqn | rg -v "nvidia|amd" > ./document/pkglist.txt
+yay -Qeqn | rg -v "nvidia|amd|intel" > ./document/pkglist.txt
 yay -Qeqm | rg -v "rtl8812au|epsonscan" > ./document/pkglist-foreign.txt
 
 if [[ ${host_type} == 'laptop' ]]; then
   yay -Qeq | rg "amd" > ./document/pkglist-laptop.txt
 elif [[ ${host_type} == 'desktop' ]]; then
-  yay -Qeq | rg "nvidia|rtl8812au|epsonscan" > ./document/pkglist-desktop.txt
+  yay -Qeq | rg "nvidia|intel|rtl8812au|epsonscan" > ./document/pkglist-desktop.txt
 fi
