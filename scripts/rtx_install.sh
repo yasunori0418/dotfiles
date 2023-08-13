def2cdeb9dd380e606dd1e5959c5c4d2688ee19e
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-13 23:35:49 +0900

    refactor:

--- scripts/rtx_install.sh
+++ scripts/rtx_install.sh
@@ -2,30 +2,19 @@
 
 set -e -u -o pipefail
 
-dotfiles=${HOME}/dotfiles
-readonly dotfiles
-
-modules=${dotfiles}/modules
-readonly modules
-
-bin=${dotfiles}/bin/
-readonly bin
-
-latest_releases=https://api.github.com/repos/jdxcode/rtx/releases/latest
-readonly latest_releases
-
-dl_file=${modules}/rtx.tar.gz
-readonly dl_file
-
-rtx_bin=${modules}/rtx/bin/rtx
-readonly rtx_bin
+declare -r dotfiles=${HOME}/dotfiles
+declare -r modules=${dotfiles}/modules
+declare -r bin=${dotfiles}/bin/
+declare -r latest_releases=https://api.github.com/repos/jdxcode/rtx/releases/latest
+declare -r dl_file=${modules}/rtx.tar.gz
+declare -r rtx_bin=${modules}/rtx/bin/rtx
 
 curl -s ${latest_releases} \
-| grep browser_download_url \
-| cut -d ":" -f 2,3 \
-| tr -d \" \
-| grep linux-x64.tar.gz \
-| xargs curl -L > ${dl_file}
+  | grep browser_download_url \
+  | cut -d ":" -f 2,3 \
+  | tr -d \" \
+  | grep linux-x64.tar.gz \
+  | xargs curl -L > ${dl_file}
 
 cd ${modules}
 tar zxvf ${dl_file}
