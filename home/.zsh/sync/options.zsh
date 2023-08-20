1f9dd7c7fb67010a1a2c22e388efcbc2d5b6e00f
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-20 23:45:53 +0900

    Update:

--- home/.zsh/sync/options.zsh
+++ home/.zsh/sync/options.zsh
@@ -1,10 +1,23 @@
 # https://zsh.sourceforge.io/Doc/Release/Options.html#Options
+# https://vorfee.hatenablog.jp/entry/2015/03/12/182937
 
 ## Change Directories
 setopt AUTO_CD
 setopt AUTO_PUSHD
 setopt PUSHD_SILENT
 setopt PUSHD_IGNORE_DUPS
+
+## Completion
+setopt ALWAYS_LAST_PROMPT
+setopt AUTO_LIST
+setopt AUTO_MENU
+setopt AUTO_PARAM_KEYS
+setopt AUTO_PARAM_SLASH
+setopt AUTO_REMOVE_SLASH
+setopt HASH_LIST_ALL
+setopt LIST_AMBIGUOUS
+unsetopt LIST_BEEP
+setopt LIST_TYPES
 
 ## Zle
 unsetopt BEEP
