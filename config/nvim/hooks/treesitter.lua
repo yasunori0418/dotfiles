831bf7ab240b16da21679bea717c0a1eb3ed0d17
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-27 01:48:31 +0900

    Update: 自動インストールを有効化

--- config/nvim/hooks/treesitter.lua
+++ config/nvim/hooks/treesitter.lua
@@ -36,6 +36,8 @@ require("nvim-treesitter.configs").setup({
   -- Install parsers synchronously (only applied to `ensure_installed`)
   -- Settings for load reduction.
   sync_install = true,
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
   highlight = {
     enable = true,
     disable = function(lang, buf)
