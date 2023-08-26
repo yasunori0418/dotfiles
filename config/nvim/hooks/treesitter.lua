a3a3db220886ca0354efc5c2a34c11371a6f0101
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-27 01:18:28 +0900

    Update:

--- config/nvim/hooks/treesitter.lua
+++ config/nvim/hooks/treesitter.lua
@@ -25,13 +25,21 @@ require("nvim-treesitter.configs").setup({
   -- Install parsers synchronously (only applied to `ensure_installed`)
   -- Settings for load reduction.
   sync_install = true,
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
   highlight = {
     enable = true,
-    disable = {
-      "vimdoc",
-    },
+    disable = function(lang, buf)
+      local languages = {
+        "vimdoc",
+      }
+      if vim.iter(languages):find(lang) then
+        return true
+      end
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   indent = {
     enable = false,
