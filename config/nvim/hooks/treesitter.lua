33dcd5e03b91a5ec449f4d9646e0dcefb3b395c9
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-26 23:19:50 +0900

    refactor: formatted

--- config/nvim/hooks/treesitter.lua
+++ config/nvim/hooks/treesitter.lua
@@ -1,17 +1,17 @@
 -- lua_source {{{
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldenable = false
 require("nvim-treesitter.configs").setup({
   ensure_installed = "all",
   highlight = {
     enable = true,
     disable = {
-      'vimdoc',
+      "vimdoc",
     },
   },
   indent = {
-    enable = false
+    enable = false,
   },
   yati = {
     enable = true,
