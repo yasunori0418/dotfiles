fe35509bc9fb15bfe13733da30e4a1ac4ff5325e
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-08-29 23:44:51 +0900

    Add: nvim_night.sh

---
+++ scripts/nvim_night.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+declare -r install_prefix=${HOME}/.local/dotfiles
+declare -r tag_name=nightly
+declare -r dl_file=nvim-linux64.tar.gz
+declare -r download_url=https://github.com/neovim/neovim/releases/download/${tag_name}/${dl_file}
+
+curl -Lo ${install_prefix}/${dl_file} ${download_url}
+
+cd ${install_prefix}
+tar zxvf ${dl_file}
+rm ${dl_file}
+
+[[ -d ${install_prefix}/nvim ]] && rm -rf ${install_prefix}/nvim
+mv ${install_prefix}/nvim-linux64 ${install_prefix}/nvim
