7330a89f3e35695fe895efa4424a2dfbbac8ce43
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-02 21:50:40 +0900

    refactor: formatted lua/user/plugins/ddc.lua

--- config/nvim/lua/user/plugins/ddc.lua
+++ config/nvim/lua/user/plugins/ddc.lua
@@ -1,7 +1,7 @@
 local ddc = {}
 
-ddc.cmdline_completion = require('user.plugins.ddc.cmdline_completion').commandline_pre
+ddc.cmdline_completion = require("user.plugins.ddc.cmdline_completion").commandline_pre
 
-ddc.change_filter = require('user.plugins.ddc.other').change_filter
+ddc.change_filter = require("user.plugins.ddc.other").change_filter
 
 return ddc
