a357265aa33cf5d849cecd148b25189b3fdf7038
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-03 20:07:22 +0900

    refactor: formatted

--- config/nvim/ftplugin/json.lua
+++ config/nvim/ftplugin/json.lua
@@ -1,37 +1,17 @@
 local command = vim.api.nvim_create_user_command
 
 command("JY", function(opts)
-  vim.print(opts.buffer)
-  vim.fn["denops#request"](
-    'jsonyaml',
-    'jsonYAML',
-    { opts.line1, opts.line2 }
-  )
-end, { range = '%' })
+  vim.fn["denops#request"]("jsonyaml", "jsonYAML", { opts.line1, opts.line2 })
+end, { range = "%" })
 
 command("YJ", function(opts)
-  vim.print(opts.buffer)
-  vim.fn["denops#request"](
-    'jsonyaml',
-    'yamlJSON',
-    { opts.line1, opts.line2 }
-  )
-end, { range = '%' })
+  vim.fn["denops#request"]("jsonyaml", "yamlJSON", { opts.line1, opts.line2 })
+end, { range = "%" })
 
 command("JT", function(opts)
-  vim.print(opts.buffer)
-  vim.fn["denops#request"](
-    'jsontoml',
-    'jsonTOML',
-    { opts.line1, opts.line2 }
-  )
-end, { range = '%' })
+  vim.fn["denops#request"]("jsontoml", "jsonTOML", { opts.line1, opts.line2 })
+end, { range = "%" })
 
 command("TJ", function(opts)
-  vim.print(opts.buffer)
-  vim.fn["denops#request"](
-    'jsontoml',
-    'tomlJSON',
-    { opts.line1, opts.line2 }
-  )
-end, { range = '%' })
+  vim.fn["denops#request"]("jsontoml", "tomlJSON", { opts.line1, opts.line2 })
+end, { range = "%" })
