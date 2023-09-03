bbcabdb15f7f5a77a9696e88826ad7b88fa90629
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-03 20:16:49 +0900

    Update: on_cmd

--- config/nvim/ftplugin/json.lua
+++ config/nvim/ftplugin/json.lua
@@ -1,17 +1,30 @@
 local command = vim.api.nvim_create_user_command
+local dein = require("dein")
 
 command("JY", function(opts)
+  if not dein.is_sourced("jsonyaml.vim") then
+    dein.source("jsonyaml.vim")
+  end
   vim.fn["denops#request"]("jsonyaml", "jsonYAML", { opts.line1, opts.line2 })
 end, { range = "%" })
 
 command("YJ", function(opts)
+  if not dein.is_sourced("jsonyaml.vim") then
+    dein.source("jsonyaml.vim")
+  end
   vim.fn["denops#request"]("jsonyaml", "yamlJSON", { opts.line1, opts.line2 })
 end, { range = "%" })
 
 command("JT", function(opts)
+  if not dein.is_sourced("jsontoml.vim") then
+    dein.source("jsontoml.vim")
+  end
   vim.fn["denops#request"]("jsontoml", "jsonTOML", { opts.line1, opts.line2 })
 end, { range = "%" })
 
 command("TJ", function(opts)
+  if not dein.is_sourced("jsontoml.vim") then
+    dein.source("jsontoml.vim")
+  end
   vim.fn["denops#request"]("jsontoml", "tomlJSON", { opts.line1, opts.line2 })
 end, { range = "%" })
