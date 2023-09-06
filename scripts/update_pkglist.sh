01908c7db4afe0a74fe9ee0528cedb3a2d94adfb
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-06 23:43:48 +0900

    Update: ちょっと雑運用しているけど:yoshi:

--- scripts/update_pkglist.sh
+++ scripts/update_pkglist.sh
@@ -8,5 +8,5 @@ yay -Qeqm | rg -v "rtl8812au|epsonscan" > ./document/pkglist-foreign.txt
 if [[ ${host_type} == 'laptop' ]]; then
   yay -Qeq | rg "amd" > ./document/pkglist-laptop.txt
 elif [[ ${host_type} == 'desktop' ]]; then
-  yay -Qeq | rg "nvidia|intel|rtl8812au|epsonscan" > ./document/pkglist-desktop.txt
+  yay -Qeq | rg "nvidia|intel|rtl8812au|epsonscan|minecraft" > ./document/pkglist-desktop.txt
 fi
