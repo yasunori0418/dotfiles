038c35bc0ef64b5dde77456ec6619e0368cecb8e
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-09 00:10:04 +0900

    Revert "Update: foldmethodはufoに任せたい"
    This reverts commit d7cdb02ca9d180ca3a4aacd726221a8471537b45.
    markerはufoじゃやってくれないのね…

--- config/nvim/hooks/deinft.lua
+++ config/nvim/hooks/deinft.lua
@@ -45,13 +45,13 @@ vim.opt_local.shiftwidth = 4
 
 -- lua_help {{{
 vim.opt_local.conceallevel = 0
--- vim.opt_local.foldmethod = "marker"
--- vim.opt_local.foldenable = true
+vim.opt_local.foldmethod = "marker"
+vim.opt_local.foldenable = true
 -- }}}
 
 -- lua_toml {{{
--- vim.opt_local.foldmethod = "marker"
--- vim.opt_local.foldenable = true
+vim.opt_local.foldmethod = "marker"
+vim.opt_local.foldenable = true
 -- }}}
 
 -- lua_qf {{{
