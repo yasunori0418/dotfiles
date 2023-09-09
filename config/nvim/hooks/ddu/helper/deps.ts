0a08e8f5d531d567726dfe5de78f270b9b7bf55a
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-10 00:05:17 +0900

    refactor: formatted

--- config/nvim/hooks/ddu/helper/deps.ts
+++ config/nvim/hooks/ddu/helper/deps.ts
@@ -12,8 +12,8 @@ export type { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/co
 export type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.6.0/file.ts";
 
 export type {
-  ActionData as GitCommitActionData
+  ActionData as GitCommitActionData,
 } from "https://raw.githubusercontent.com/kyoh86/ddu-source-git_log/main/denops/%40ddu-kinds/git_commit.ts";
 export type {
-  ActionData as GitBranchActionData
-} from "https://raw.githubusercontent.com/kyoh86/ddu-source-git_branch/main/denops/%40ddu-kinds/git_branch.ts"
+  ActionData as GitBranchActionData,
+} from "https://raw.githubusercontent.com/kyoh86/ddu-source-git_branch/main/denops/%40ddu-kinds/git_branch.ts";
