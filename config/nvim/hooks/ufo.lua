4bc54b8e0c5eba560b74b4c36c7413009cc6afc7
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-09 15:20:21 +0900

    Update: ufoのexample.luaからとりあえずコピーしてくる

--- config/nvim/hooks/ufo.lua
+++ config/nvim/hooks/ufo.lua
@@ -51,6 +51,34 @@ local filetype_foldmethod = {
   help = "", -- foldmethod = marker
 }
 
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, {chunkText, hlGroup})
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, {suffix, 'MoreMsg'})
+  return newVirtText
+end
+
 ufo.setup({
   open_fold_hl_timeout = 150,
   close_fold_kinds = { "imports", "comment" },
@@ -70,6 +98,8 @@ ufo.setup({
       close = "q",
     },
   },
+  enable_get_fold_virt_text = true,
+  fold_virt_text_handler = handler,
   provider_selector = function(_, filetype, _) -- (bufnr, filetype, buftype)
     return filetype_foldmethod[filetype]
   end,
