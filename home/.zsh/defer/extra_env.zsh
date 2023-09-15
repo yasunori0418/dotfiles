d20d3609d8189426587f3c613beadc1e4032acf9
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-15 22:27:24 +0900

    Update: 急ぎでセットされなくても良いenv

---
+++ home/.zsh/defer/extra_env.zsh
@@ -0,0 +1,50 @@
+### extras environment variable ###
+
+# if installed binary by rust-cargo
+cargo_bin=${HOME}/.cargo/bin
+if [[ -d ${cargo_bin} ]]; then
+  export PATH=${PATH}:${cargo_bin}
+fi
+
+# themis environment
+themis_bin=${HOME}/.cache/dein/repos/github.com/thinca/vim-themis/bin
+if [[ -d ${themis_bin} ]]; then
+  export PATH=${PATH}:${themis_bin}
+fi
+
+# go package
+go_bin=${HOME}/go/bin
+if [[ -d ${go_bin} ]]; then
+  export PATH=${PATH}:${go_bin}
+fi
+
+# bat theme
+export BAT_THEME=Nord
+
+# ヒストリの一覧を読みやすい形に変更
+export HISTTIMEFORMAT="[%Y/%M/%D %H:%M:%S] "
+
+# ヒストリーサイズ設定
+export HISTFILE=${HOME}/.zhistory
+export HISTSIZE=1000000
+export SAVEHIST=1000000
+
+# 補完リストが多いときに尋ねない
+export LISTMAX=100
+
+# "|,:"を単語の一部とみなさない
+export WORDCHARS="${WORDCHARS}|:"
+
+# LS_COLORS
+if [[ $(command -v vivid) ]];then
+  export LS_COLORS="$(vivid generate nord)"
+else
+  dir_colors_cache=${XDG_CACHE_HOME}/dir_colors.zsh
+  dir_colors=${HOME}/.dir_colors
+  if [[ ! -r ${dir_colors_cache} || ${dir_colors} -nt ${dir_colors_cache} ]]; then
+    dircolors ${dir_colors} > ${dir_colors_cache}
+  fi
+  source ${dir_colors_cache}
+fi
+
+unset dir_colors dir_colors_cache go_bin cargo_bin themis_bin
