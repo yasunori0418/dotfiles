742d818386ff9616478a7b3271bc819908ca3c84
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-16 13:31:47 +0900

    Update: リファレンス実装から必要そうな物を取得しておく

---
+++ config/nvim/hooks/ddc/deps.ts
@@ -0,0 +1,5 @@
+export { BaseConfig } from "https://deno.land/x/ddc_vim@v4.0.4/types.ts";
+export { fn } from "https://deno.land/x/ddc_vim@v4.0.4/deps.ts";
+
+// Type definitions.
+export type { ConfigArguments } from "https://deno.land/x/ddc_vim@v4.0.4/base/config.ts";
