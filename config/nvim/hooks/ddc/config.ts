833f36dee01cefa70925e775491d9741f035ea6a
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-17 00:10:56 +0900

    Update: sourceOptionsまでをts化

--- config/nvim/hooks/ddc/config.ts
+++ config/nvim/hooks/ddc/config.ts
@@ -32,6 +32,54 @@ export class Config extends BaseConfig {
           sorters: ["sorter_fuzzy"],
           converters: ["converter_fuzzy"],
         },
+        around: { mark: "A" },
+        buffer: { mark: "B" },
+        line: { mark: "line" },
+        rg: {
+          mark: "rg",
+          minAutoCompleteLength: 5,
+          enabledIf: "finddir('.git', ';') != ''",
+        },
+        necovim: { mark: "vim" },
+        vsnip: {
+          mark: "snip",
+          dup: "keep",
+        },
+        "nvim-lsp": {
+          mark: "LSP",
+          forceCompletionPattern: "\\.\\w*|::\\w*|->\\w*",
+          dup: "force",
+        },
+        "nvim-lua": {
+          mark: "lua",
+          forceCompletionPattern: "\\.\\w*",
+        },
+        skkeleton: {
+          mark: "SKK",
+          matchers: ["skkeleton"],
+          sorters: ["sorter_rank"],
+          minAutoCompleteLength: 2,
+          isVolatile: true,
+        },
+
+        cmdline: {
+          mark: "cmd",
+          forceCompletionPattern: "\\S/\\S*|\\.\\w*",
+          dup: "force",
+        },
+        "cmdline-history": {
+          mark: "cmd-history",
+          sorters: [],
+        },
+        input: {
+          mark: "input",
+          forceCompletionPattern: "\\S/\\S*",
+          isVolatile: true,
+          dup: "force",
+        },
+        "shell-history": {
+          mark: "sh-history",
+        },
       },
     });
 
