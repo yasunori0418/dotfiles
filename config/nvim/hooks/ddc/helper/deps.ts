4be0230dcb57b0e7c263101ecbe09c497c730592
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-23 22:53:41 +0900

    refactor: formatted

--- config/nvim/hooks/ddc/helper/deps.ts
+++ config/nvim/hooks/ddc/helper/deps.ts
@@ -3,9 +3,7 @@ export {
   type ConfigArguments,
 } from "https://deno.land/x/ddc_vim@v4.0.5/base/config.ts";
 
-export type {
-  UserSource,
-} from "https://deno.land/x/ddc_vim@v4.0.5/types.ts";
+export type { UserSource } from "https://deno.land/x/ddc_vim@v4.0.5/types.ts";
 
 export {
   fn,
