e99cca951e027822f80a0216747f3bbcd60e94bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-26 23:05:17 +0900

    Update: とりあえず、指定したツールがインストールされた

--- config/nvim/lua/user/plugins/efm.lua
+++ config/nvim/lua/user/plugins/efm.lua
@@ -1,8 +1,13 @@
 local M = {}
 local utils = require('user.utils')
+
+---@type string[]
 M.tools = {}
-M.languages = {}
+
+---@type string[]
 M.filetypes = {}
+
+M.languages = {}
 
 ---@alias kind
 ---| "formatters" # `require('efmls-configs.formatters')`
@@ -31,9 +36,22 @@ local function filetype_config(filetype, tool_configs)
   result[filetype] = {}
   for _, tool_config in ipairs(tool_configs) do
     table.insert(result[filetype], config_require(tool_config))
-    table.insert(M.tools, tool_config)
+    table.insert(M.tools, tool_config.name)
   end
   return result
+end
+
+---mason ensure_installed
+local function ensure_installed()
+  local registry = require("mason-registry")
+  registry.refresh(function()
+    for _, tool in ipairs(M.tools) do
+      local pkg = registry.get_package(tool)
+      if not pkg:is_installed() then
+        pkg:install()
+      end
+    end
+  end)
 end
 
 ---setup of efm-langserver.
@@ -46,6 +64,7 @@ function M.setup(options)
       filetype_config(filetype, options[filetype])
     )
   end
+  ensure_installed()
 end
 
 return M
