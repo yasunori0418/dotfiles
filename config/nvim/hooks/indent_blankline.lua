f0ea3a25d618685379255390104000f805787bbf
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-09-30 19:29:04 +0900

    Update: colorful indent mark highlight

--- config/nvim/hooks/indent_blankline.lua
+++ config/nvim/hooks/indent_blankline.lua
@@ -1,4 +1,25 @@
 -- lua_source {{{
+
+local highlight = {
+  "NordRed",
+  "NordYellow",
+  "NordBlue",
+  "NordOrange",
+  "NordGreen",
+  "NordCyan",
+}
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "NordRed", { fg = "#BF616A" })
+  vim.api.nvim_set_hl(0, "NordYellow", { fg = "#EBCB8B" })
+  vim.api.nvim_set_hl(0, "NordBlue", { fg = "#5E81AC" })
+  vim.api.nvim_set_hl(0, "NordOrange", { fg = "#D08770" })
+  vim.api.nvim_set_hl(0, "NordGreen", { fg = "#A3BE8C" })
+  vim.api.nvim_set_hl(0, "NordCyan", { fg = "#B48EAD" })
+end)
+
 require("ibl").setup({
   enable = true,
   debounce = 100,
