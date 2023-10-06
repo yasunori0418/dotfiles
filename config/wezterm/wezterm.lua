689f3427a96bc888cb6cc3562af5e345c90245a2
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-06 23:57:58 +0900

    Update: weztermの設定をconfig_builderを使うように変更

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,88 +1,42 @@
 local wezterm = require("wezterm")
 
-local my_keybinds = {
+local config = {}
 
-    -- Keybinds of Copy and Paste.
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-    { key = "Insert", mods = "CTRL", action = wezterm.action({ CopyTo = "PrimarySelection" }) },
-    { key = "Insert", mods = "SHIFT", action = wezterm.action({ PasteFrom = "PrimarySelection" }) },
-    --{key = "c", mods = "ALT", action = wezterm.action({CopyTo = "Clipboard"})},
-    --{key = "v", mods = "ALT", action = wezterm.action({PasteFrom = "Clipboard"})},
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
 
-    -- Copy mode like visual mode of vim.
-    { key = "x", mods = "ALT", action = "ActivateCopyMode" },
-    { key = " ", mods = "ALT", action = "QuickSelect" },
+--vimで日本語入力するときは、skkeletonを使っているから問題無い
+config.use_ime = false
 
-    -- Reload Configaration.
-    { key = "r", mods = "ALT", action = "ReloadConfiguration" },
+-- HackGen好き
+config.font = wezterm.font("HackGen35 Console NF")
 
-    -- Keybinds of change font size.
-    { key = "-", mods = "CTRL", action = "DecreaseFontSize" },
-    { key = "=", mods = "CTRL", action = "IncreaseFontSize" },
-    { key = "0", mods = "CTRL", action = "ResetFontSize" },
+-- フォントサイズは偶数でないと変になる。
+config.font_size = 16.0
 
-    -- Keybinds of controlling terminal tab.
-    { key = "t", mods = "ALT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-    { key = "t", mods = "ALT|SHIFT", action = wezterm.action({ SpawnTab = "DefaultDomain" }) },
-    { key = "w", mods = "ALT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-    { key = "w", mods = "ALT|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
-    { key = "[", mods = "ALT|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-    { key = "]", mods = "ALT|SHIFT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+-- If you use a tiling window manager then you may wish to set this to `false`.
+config.adjust_window_size_when_changing_font_size = false
 
-    -- Keybinds of controlling terminal pane.
-    { key = "v", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-    { key = "b", mods = "ALT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-    { key = "w", mods = "ALT", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-    { key = "w", mods = "ALT|SHIFT", action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
-    { key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-    { key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-    { key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-    { key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-    { key = "[", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Prev" }) },
-    { key = "]", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Next" }) },
+-- If set to true, when there is only a single tab, the tab bar is hidden from the display.
+-- If a second tab is created, the tab will be shown.
+config.hide_tab_bar_if_only_one_tab = true
+
+config.color_scheme = "nordfox"
+
+config.window_background_opacity = 0.8
+
+config.window_padding = {
+    left = 5,
+    top = 5,
+    right = 0,
+    bottom = 0,
 }
 
-return {
-    use_ime = false, --vimで日本語入力するときは、skkeletonを使っているから問題無い
+-- Configures the boundaries of a word,
+-- thus what is selected when doing a word selection with the mouse.
+config.selection_word_boundary = [=[ \t\n{}[]()"'`.,;:]=]
 
-    font = wezterm.font("HackGen35 Console NF"),
-    -- font = wezterm.font_with_fallback({
-    --   { family = "HackGen35 Console NF", weight = "Regular", style = "Normal" },
-    --   { family = "HackGen35 Console NF", weight = "Bold", style = "Normal" },
-    --   { family = "Cica", weight = "Regular", style = "Italic" },
-    --   { family = "Cica", weight = "Bold", style = "Italic" },
-    -- }),
-    font_size = 16.0, -- フォントサイズは偶数でないと変になる。
-    -- Must setting when tile window manager.
-    adjust_window_size_when_changing_font_size = false,
+config.scrollback_lines = 5000
 
-    hide_tab_bar_if_only_one_tab = true,
-
-    -- colorscheme settings
-    color_scheme = "nordfox",
-    color_scheme_dirs = { "$HOME/.config/wezterm/colors/" },
-
-    window_background_opacity = 0.8,
-    window_padding = {
-        left = 5,
-        top = 5,
-        right = 0,
-        bottom = 0,
-    },
-
-    selection_word_boundary = " \t\n{}[]()\"'`,;:",
-
-    scrollback_lines = 5000,
-
-    disable_default_key_bindings = true,
-    keys = my_keybinds,
-    mouse_bindings = {
-        -- Ctrl-click will open the link under the mouse cursor
-        {
-            event = { Up = { streak = 1, button = "Left" } },
-            mods = "CTRL",
-            action = "OpenLinkAtMouseCursor",
-        },
-    },
-}
+return config
