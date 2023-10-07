29f2412982dae9f19f34052dd9264c96a757ce8a
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-07 13:44:55 +0900

    Update: weztermの設定を分割してみる

---
+++ config/wezterm/configs/font.lua
@@ -0,0 +1,20 @@
+local M = {}
+local wezterm = require("wezterm")
+
+function M.setup(config)
+    -- HackGen好き
+    config.font = wezterm.font("HackGen35 Console NF")
+
+    -- フォントサイズは偶数でないと変になる。
+    config.font_size = 16.0
+
+    -- If you use a tiling window manager then you may wish to set this to `false`.
+    config.adjust_window_size_when_changing_font_size = false
+
+    -- disable ligatures
+    config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+    return config
+end
+
+return M
