ab27d651dfc90750e283f88c6815d7307e8c6d85
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-07 14:09:07 +0900

    Update: 外観設定も分割

--- config/wezterm/config.lua
+++ config/wezterm/config.lua
@@ -7,24 +7,10 @@ if wezterm.config_builder then
 end
 
 config = require("configs.font").setup(config)
+config = require("configs.appearance").setup(config)
 
 --vimで日本語入力するときは、skkeletonを使っているから問題無い
 config.use_ime = false
-
--- If set to true, when there is only a single tab, the tab bar is hidden from the display.
--- If a second tab is created, the tab will be shown.
-config.hide_tab_bar_if_only_one_tab = true
-
-config.color_scheme = "nordfox"
-
-config.window_background_opacity = 0.8
-
-config.window_padding = {
-    left = 5,
-    top = 5,
-    right = 0,
-    bottom = 0,
-}
 
 -- Configures the boundaries of a word,
 -- thus what is selected when doing a word selection with the mouse.
---
+++ config/wezterm/configs/appearance.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.setup(config)
+    config.color_scheme = "nordfox"
+    config.window_background_opacity = 0.8
+    config.window_padding = {
+        left = 5,
+        top = 5,
+        right = 0,
+        bottom = 0,
+    }
+    -- If set to true, when there is only a single tab, the tab bar is hidden from the display.
+    -- If a second tab is created, the tab will be shown.
+    config.hide_tab_bar_if_only_one_tab = true
+
+    return config
+end
+
+return M
