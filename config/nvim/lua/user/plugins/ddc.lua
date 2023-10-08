d6eb8ea7c9610fc2f5093386f29d4700d5312086
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-08 19:46:58 +0900

    rename: other -> utils

--- config/nvim/lua/user/plugins/ddc.lua
+++
@@ -1,7 +0,0 @@
-local ddc = {}
-
-ddc.cmdline_completion = require("user.plugins.ddc.cmdline_completion").commandline_pre
-
-ddc.change_filter = require("user.plugins.ddc.other").change_filter
-
-return ddc
