982befe099a1b528f708956aa5165e0bb05f3af7
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-11 00:13:02 +0900

    docs:

--- config/nvim/lua/user/plugins/lexima.lua
+++ config/nvim/lua/user/plugins/lexima.lua
@@ -3,7 +3,7 @@ local M = {}
 local add = vim.fn["lexima#add_rule"]
 
 -- simulates altercmd by lexima
--- port of https://scrapbox.io/vim-jp/lexima.vim%E3%81%A7Better_vim-altercmd%E3%82%92%E5%86%8D%E7%8F%BE%E3%81%99%E3%82%8B
+-- https://scrapbox.io/vim-jp/lexima.vim%E3%81%A7Better_vim-altercmd%E3%82%92%E5%86%8D%E7%8F%BE%E3%81%99%E3%82%8B
 function M.altercmd(original, altanative)
   -- <SPACE>
   add({
