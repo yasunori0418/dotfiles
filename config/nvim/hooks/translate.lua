0adbc8a231b8d431be764c7b8c94193a4c3ecba7
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-11 00:08:46 +0900

    Update: Translateコマンドで呼ぶように変更

--- config/nvim/hooks/translate.lua
+++
@@ -1,7 +0,0 @@
--- lua_add {{{
-local opt = { silent = true, noremap = false }
-require("user.utils").keymaps_set({
-    { mode = { "n", "x" }, lhs = [[tx]], rhs = [[<Plug>(Translate)]], opts = opt },
-    { mode = { "n", "x" }, lhs = [[tX]], rhs = [[<Plug>(Translate-reverse)]], opts = opt },
-})
--- }}}
