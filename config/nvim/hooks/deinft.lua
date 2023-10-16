eaab7701780ec3460677a41424c69dbe55fa37a2
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-16 23:06:56 +0900

    Update: markdownlintさんにスペース4つのインデントは怒られる

--- config/nvim/hooks/deinft.lua
+++ config/nvim/hooks/deinft.lua
@@ -26,9 +26,9 @@ vim.opt_local.expandtab = false
 -- }}}
 
 -- lua_markdown {{{
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
 -- }}}
 
 -- lua_php {{{
