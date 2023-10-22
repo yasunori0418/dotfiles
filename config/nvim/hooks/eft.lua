ea15cfb5a013b52d4bba39feffae61b7e308c0bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-22 13:57:55 +0900

    Update: clever-fに戻してみよ

--- config/nvim/hooks/eft.lua
+++ config/nvim/hooks/eft.lua
@@ -1,8 +1,8 @@
 -- lua_add {{{
 local opt = { silent = true, noremap = false }
 require("user.utils").keymaps_set({
-    { mode = { "n", "x", "o" }, lhs = [[f]], rhs = [[<Plug>(eft-f)]], opts = opt },
-    { mode = { "n", "x", "o" }, lhs = [[F]], rhs = [[<Plug>(eft-F)]], opts = opt },
+    { mode = { "n", "x", "o" }, lhs = [[f]], rhs = [[<Plug>(eft-f-repeatable)]], opts = opt },
+    { mode = { "n", "x", "o" }, lhs = [[F]], rhs = [[<Plug>(eft-F-repeatable)]], opts = opt },
     { mode = { "n", "x", "o" }, lhs = [[;]], rhs = [[<Plug>(eft-repeat)]], opts = opt },
 })
 -- }}}
