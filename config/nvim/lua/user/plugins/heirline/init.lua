747c5faf125afc9128d0390a75e0a363b2eb5f34
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-24 23:41:26 +0900

    Update:

--- config/nvim/lua/user/plugins/heirline/init.lua
+++ config/nvim/lua/user/plugins/heirline/init.lua
@@ -1,7 +1,17 @@
 local M = {}
 
+-- local conditions = require("heirline.conditions")
+-- local utils = require("heirline.utils")
+
+local heirline = require("heirline")
+local color = require("user.plugins.heirline.color")
+local Statusline = require("user.plugins.heirline.statusline")
+
 function M.setup()
-    require("heirline").setup({})
+    heirline.load_colors(color)
+    heirline.setup({
+        statusline = Statusline,
+    })
 end
 
 return M
