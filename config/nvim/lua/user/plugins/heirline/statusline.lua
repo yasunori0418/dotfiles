93dacdf92717293d07d7a66326f7aaa7df0dddc7
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-29 14:58:10 +0900

    Update:

--- config/nvim/lua/user/plugins/heirline/statusline.lua
+++ config/nvim/lua/user/plugins/heirline/statusline.lua
@@ -1,13 +1,18 @@
 local Mode = require("user.plugins.heirline.mode")
+local color = require("user.plugins.heirline.color")
 
 return {
     {
-        init = function(_)
+        init = function(self)
+            self.mode_colors = color.mode_colors()
+        end,
+        hl = function(self)
+            return { fg = self.mode_colors.fg, bg = self.mode_colors.bg }
         end,
         {
             Mode,
-            hl = function(_)
-                return { fg = "fg2", bg = "blue_dim" }
+            hl = function(self)
+                return { fg = "fg1", bg = self.mode_colors.bg }
             end,
         },
     },
