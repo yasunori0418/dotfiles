60315293eb4ec232645cb8ccbf45570500cca7e8
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-29 15:48:53 +0900

    Update:

--- config/nvim/lua/user/plugins/heirline/statusline.lua
+++ config/nvim/lua/user/plugins/heirline/statusline.lua
@@ -1,23 +1,49 @@
-local Mode = require("user.plugins.heirline.mode")
-
 return {
     {
         init = function(self)
             self.mode_colors = require("user.plugins.heirline.color").mode_colors()
+            self.separator = {
+                main = {
+                    left = "\u{E0B0}", -- [[]]
+                    right = "\u{E0B2}", -- [[]]
+                },
+                sub = {
+                    left = "\u{E0B1}", -- [[]]
+                    right = "\u{E0B3}", -- [[]]
+                }
+            }
         end,
         hl = function(self)
             return { fg = self.mode_colors.fg, bg = self.mode_colors.bg }
         end,
         {
-            Mode,
+            require("user.plugins.heirline.mode"),
             hl = function(self)
                 return { fg = "fg1", bg = self.mode_colors.bg }
             end,
         },
         {
-            provider = "\u{E0B0}",
+            provider = function(self)
+                return self.separator.main.left
+            end,
             hl = function(self)
-                return { fg = self.mode_colors.bg, bg = "bg0" }
+                return { fg = self.mode_colors.bg, bg = self.mode_colors.fg, }
+            end,
+        },
+        { -- CWD
+            provider = function()
+                return "\u{00A0}" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+            end,
+            hl = function(self)
+                return { fg = "fg3", bg = self.mode_colors.fg }
+            end
+        },
+        {
+            provider = function(self)
+                return self.separator.main.left
+            end,
+            hl = function(self)
+                return { fg = self.mode_colors.fg, bg = "bg0" }
             end,
         },
     },
