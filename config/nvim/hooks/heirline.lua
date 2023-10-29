fea9d2fbf77508ef698457d9c8573c2750a00321
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-29 14:56:39 +0900

    Update: 絵文字のモード表示とシンプルなモード表示を切り替えできるようにしてみる

--- config/nvim/hooks/heirline.lua
+++ config/nvim/hooks/heirline.lua
@@ -6,6 +6,7 @@ vim.opt.laststatus = 0
 -- lua_source {{{
 vim.opt.laststatus = 3
 vim.opt.showtabline = 2
+vim.g.heirline_indicator_type = "simple"
 
 -- $BASE_DIR/lua/user/plugins/heirline
 require("user.plugins.heirline").setup()
