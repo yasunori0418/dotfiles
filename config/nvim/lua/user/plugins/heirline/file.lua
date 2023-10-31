732cedbf61446fade7ea164931ef96316c93a8f5
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-31 23:36:48 +0900

    Update:

--- config/nvim/lua/user/plugins/heirline/file.lua
+++ config/nvim/lua/user/plugins/heirline/file.lua
@@ -1,10 +1,10 @@
 local devicons = require("nvim-web-devicons")
-local conditions = require("heirline.conditions")
+-- local conditions = require("heirline.conditions")
 -- local utils = require("heirline.utils")
 
 local icon = {
     provider = function(self)
-        return self.padding_char .. self.icon .. self.padding_char
+        return " " .. self.icon .. " "
     end,
     hl = function(self)
         return { fg = self.icon_color }
@@ -16,15 +16,15 @@ local name = {
         -- first, trim the pattern relative to the current directory. For other
         -- options, see :h filename-modifers
         local filename = vim.fn.fnamemodify(self.filename, ":.")
-        if filename == "" then
-            return " [No Name]"
+        if filename == "" or filename == nil then
+            return "[No Name]"
         end
         -- now, if the filename would occupy more than 1/4th of the available
         -- space, we trim the file path to its initials
         -- See Flexible Components section below for dynamic truncation
-        if not conditions.width_percent_below(#filename, 0.25) then
-            filename = vim.fn.pathshorten(filename)
-        end
+        -- if not conditions.width_percent_below(#filename, 0.25) then
+        --     filename = vim.fn.pathshorten(filename)
+        -- end
         return filename
     end,
 }
@@ -40,7 +40,7 @@ local flags = {
         condition = function()
             return not vim.bo.modifiable or vim.bo.readonly
         end,
-        provider = "\u{f023}", -- [[]]
+        provider = " \u{f023}", -- [[]]
     },
 }
 
@@ -49,7 +49,6 @@ return {
         self.filename = vim.api.nvim_buf_get_name(0)
         self.extension = vim.fn.fnamemodify(self.filename, ":e")
         self.icon, self.icon_color = devicons.get_icon_color(self.filename, self.extension, { default = true })
-        self.padding_char = "\u{00A0}"
     end,
     icon,
     name,
