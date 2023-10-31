b4f827d9a2c7d6677a913ba483e89c36db85c592
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-31 23:02:40 +0900

    Update: winbarを一部バッファーで無効化する設定

--- config/nvim/lua/user/plugins/heirline/init.lua
+++ config/nvim/lua/user/plugins/heirline/init.lua
@@ -4,12 +4,24 @@ local M = {}
 -- local utils = require("heirline.utils")
 
 local heirline = require("heirline")
+local conditions = require("heirline.conditions")
 
 function M.setup()
     require("user.plugins.heirline.color").set()
     heirline.setup({
         statusline = require("user.plugins.heirline.statusline"),
         winbar = require("user.plugins.heirline.winbar"),
+        opts = {
+            -- if the callback returns true, the winbar will be disabled for that window
+            -- the args parameter corresponds to the table argument passed to autocommand callbacks.
+            -- :h nvim_lua_create_autocmd()
+            disable_winbar_cb = function(args)
+                return conditions.buffer_matches({
+                    buftype = { "nofile", "prompt", "help", "quickfix", "terminal" },
+                    filetype = { "^git.*", "^ddu.*", "^gin.*", "qfreplace" },
+                }, args.buf)
+            end,
+        },
     })
 end
 
