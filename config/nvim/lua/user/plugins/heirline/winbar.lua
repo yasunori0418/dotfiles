869e20a760a8ed94c64e73434049d306e9732f43
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-10-31 22:41:22 +0900

    Update: winbarを設定してみる

---
+++ config/nvim/lua/user/plugins/heirline/winbar.lua
@@ -0,0 +1,11 @@
+return {
+    init = function(self)
+        self.mode_colors = require("user.plugins.heirline.color").mode_colors()
+    end,
+    hl = function()
+        return { fg = "fg1" }
+    end,
+    {
+        require("user.plugins.heirline.file"),
+    },
+}
