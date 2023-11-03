af312db7461fc9f2649be2913e6cf764df38bd39
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-04 00:01:00 +0900

    Update: まずは公式設定を

---
+++ config/nvim/hooks/navic.lua
@@ -0,0 +1,45 @@
+-- lua_source {{{
+local navic = require("nvim-navic")
+
+navic.setup({
+    icons = {
+        File = " ",
+        Module = " ",
+        Namespace = " ",
+        Package = " ",
+        Class = " ",
+        Method = " ",
+        Property = " ",
+        Field = " ",
+        Constructor = " ",
+        Enum = " ",
+        Interface = " ",
+        Function = " ",
+        Variable = " ",
+        Constant = " ",
+        String = " ",
+        Number = " ",
+        Boolean = " ",
+        Array = " ",
+        Object = " ",
+        Key = " ",
+        Null = " ",
+        EnumMember = " ",
+        Struct = " ",
+        Event = " ",
+        Operator = " ",
+        TypeParameter = " ",
+    },
+    lsp = {
+        auto_attach = false,
+        preference = nil,
+    },
+    highlight = false,
+    separator = " > ",
+    depth_limit = 0,
+    depth_limit_indicator = "..",
+    safe_output = true,
+    lazy_update_context = false,
+    click = false,
+})
+-- }}}
