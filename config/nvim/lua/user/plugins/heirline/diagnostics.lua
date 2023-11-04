f88e331499a0f8d9050f411e703fcf40a841f948
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-04 14:10:21 +0900

    Update:

---
+++ config/nvim/lua/user/plugins/heirline/diagnostics.lua
@@ -0,0 +1,52 @@
+local conditions = require("heirline.conditions")
+
+return {
+
+    condition = conditions.has_diagnostics,
+
+    static = {
+        error_icon = "E",
+        warn_icon = "W",
+        info_icon = "I",
+        hint_icon = "H",
+        -- error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        -- warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        -- info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        -- hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+    },
+
+    init = function(self)
+        self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+        self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+        self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+        self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+    end,
+
+    update = { "DiagnosticChanged", "BufEnter" },
+
+    {
+        provider = function(self)
+            -- 0 is just another output, we can decide to print it or not!
+            return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+        end,
+        hl = { fg = "diag_error" },
+    },
+    {
+        provider = function(self)
+            return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+        end,
+        hl = { fg = "diag_warn" },
+    },
+    {
+        provider = function(self)
+            return self.info > 0 and (self.info_icon .. self.info .. " ")
+        end,
+        hl = { fg = "diag_info" },
+    },
+    {
+        provider = function(self)
+            return self.hints > 0 and (self.hint_icon .. self.hints)
+        end,
+        hl = { fg = "diag_hint" },
+    },
+}
