e2714486cc2628d69e4b0bafd2b37792ce973f73
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-05 23:38:33 +0900

    Update:

--- config/nvim/lua/user/plugins/heirline/tabline.lua
+++ config/nvim/lua/user/plugins/heirline/tabline.lua
@@ -1,12 +1,39 @@
 local devicons = require("nvim-web-devicons")
 local utils = require("heirline.utils")
 
-local bufnr = {
+local tabline_bufnr = {
     provider = function(self)
         return tostring(self.bufnr) .. ". "
     end,
     hl = "comment",
 }
+
+-- this is the default function used to retrieve buffers
+local get_bufs = function()
+    return vim.tbl_filter(function(bufnr)
+        return vim.api.nvim_get_option_value("buflisted", {
+            buf = bufnr,
+        })
+    end, vim.api.nvim_list_bufs())
+end
+
+-- initialize the buflist cache
+local buflist_cache = {}
+
+-- setup an autocmd that updates the buflist_cache every time that buffers are added/removed
+vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
+    callback = function()
+        vim.schedule(function()
+            local buffers = get_bufs()
+            for index, value in ipairs(buffers) do
+                buflist_cache[index] = value
+            end
+            for i = #buffers + 1, #buflist_cache do
+                buflist_cache[i] = nil
+            end
+        end)
+    end,
+})
 
 local file_icon = {
     provider = function(self)
@@ -79,7 +106,7 @@ local file_name_block = {
         end,
         name = "heirline_tabline_buffer_callback",
     },
-    bufnr,
+    tabline_bufnr,
     file_icon,
     file_name,
     file_flags,
@@ -97,11 +124,74 @@ end, { file_name_block })
 -- and here we go
 local BufferLine = utils.make_buflist(
     TablineBufferBlock,
-    { provider = "", hl = { fg = "black_dim" } }, -- left truncation, optional (defaults to "<")
-    { provider = "", hl = { fg = "black_dim" } } -- right trunctation, also optional (defaults to ...... yep, ">")
+    { -- left truncation, optional (defaults to "<")
+        provider = "",
+        hl = { fg = "black_bright", bg = "bg0" }
+    },
+    { -- right trunctation, also optional (defaults to ...... yep, ">")
+        provider = "",
+        hl = { fg = "black_bright", bg = "bg0" }
+    },
     -- by the way, open a lot of buffers and try clicking them ;)
+    function()
+        return buflist_cache
+    end,
+    -- no cache, as we're handling everything ourselves
+    false
 )
 
+local Tabpage = {
+    provider = function(self)
+        return "%" .. self.tabnr .. "T " .. self.tabpage .. " %T"
+    end,
+    hl = function(self)
+        if self.is_active then
+            return "TabLineSel"
+        else
+            return "TabLine"
+        end
+    end,
+}
+
+local Tabpages = {
+    -- only show this component if there's 2 or more tabpages
+    condition = function()
+        return #vim.api.nvim_list_tabpages() >= 2
+    end,
+    { provider = "%=" },
+    utils.make_tablist(Tabpage),
+}
+
+local TabLineOffset = {
+    condition = function(self)
+        local win = vim.api.nvim_tabpage_list_wins(0)[1]
+        local bufnr = vim.api.nvim_win_get_buf(win)
+        self.winid = win
+
+        if vim.bo[bufnr].filetype == "NvimTree" then
+            self.title = "NvimTree"
+            return true
+        end
+    end,
+
+    provider = function(self)
+        local title = self.title
+        local width = vim.api.nvim_win_get_width(self.winid)
+        local pad = math.ceil((width - #title) / 2)
+        return string.rep(" ", pad) .. title .. string.rep(" ", pad)
+    end,
+
+    hl = function(self)
+        if vim.api.nvim_get_current_win() == self.winid then
+            return "TablineSel"
+        else
+            return "Tabline"
+        end
+    end,
+}
+
 return {
+    TabLineOffset,
     BufferLine,
+    Tabpages,
 }
