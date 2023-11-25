5ebbfa514d0d1f6350e6d281a563c325132f24c1
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-25 13:20:13 +0900

    Update(#36): henkanモードの設定忘れていた

--- config/nvim/hooks/skkeleton_state_popup.lua
+++ config/nvim/hooks/skkeleton_state_popup.lua
@@ -3,6 +3,7 @@ vim.fn["skkeleton_state_popup#config"]({
     labels = {
         ["input:okurinasi"] = { hira = "▽", kata = "▽", hankata = "▽", abbrev = "▽" },
         ["input:okuriari"] = { hira = "▽", kata = "▽", hankata = "▽" },
+        henkan = { hira = "▼", kata = "▼", hankata = "▼", abbrev = "▼" },
     },
     opts = { relative = "cursor", col = 2, row = 0, anchor = "NW", style = "minimal" },
 })
