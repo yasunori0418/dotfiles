aba6f730f90f45e906fe2cd2dfb7f0c4ca5e17ec
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-25 23:09:23 +0900

    Update: 関数ジャンプ良さげ

--- config/nvim/hooks/ts_textobjects.lua
+++ config/nvim/hooks/ts_textobjects.lua
@@ -23,6 +23,22 @@ require("nvim-treesitter.configs").setup({
                 ["[a"] = "@parameter.inner",
             },
         },
+        move = {
+            enable = true,
+            set_jumps = false,
+            goto_next_start = {
+                ["]f"]  = "@function.outer",
+            },
+            goto_next_end = {
+                ["]F"]  = "@function.outer",
+            },
+            goto_previous_start = {
+                ["[f"]  = "@function.outer",
+            },
+            goto_previous_end = {
+                ["[F"]  = "@function.outer",
+            },
+        },
     },
 })
 -- }}}
