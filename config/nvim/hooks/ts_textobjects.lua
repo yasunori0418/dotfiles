a0288e85f8e48165ad87cfdc8950090ff9dc7827
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-11-25 22:43:17 +0900

    Update: treesitterで関数を選択できるようにしてみた

--- config/nvim/hooks/ts_textobjects.lua
+++ config/nvim/hooks/ts_textobjects.lua
@@ -1,3 +1,19 @@
 -- lua_source {{{
-
+require("nvim-treesitter.configs").setup({
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+            },
+            selection_modes = {
+                ["@function.outer"] = "V",
+                ["@function.inner"] = "V",
+            },
+            include_surrounding_whitespace = false,
+        },
+    },
+})
 -- }}}
