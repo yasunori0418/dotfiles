350c13544127f113ceb331db420ecd4932bbc90e
GitHub <noreply@github.com>
2023-12-07 01:09:49 +0000

    chore(deps): update deno dependencies

--- config/nvim/hooks/ddc/helper/deps.ts
+++ config/nvim/hooks/ddc/helper/deps.ts
@@ -1,15 +1,15 @@
 export {
   BaseConfig,
   type ConfigArguments,
-} from "https://deno.land/x/ddc_vim@v4.1.0/base/config.ts";
+} from "https://deno.land/x/ddc_vim@v4.3.1/base/config.ts";
 
-export type { UserSource } from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
+export type { UserSource } from "https://deno.land/x/ddc_vim@v4.3.1/types.ts";
 
 export {
   autocmd,
   fn,
   op,
   vars,
-} from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v4.3.1/deps.ts";
 
 export * as lambda from "https://deno.land/x/denops_std@v5.1.0/lambda/mod.ts";
