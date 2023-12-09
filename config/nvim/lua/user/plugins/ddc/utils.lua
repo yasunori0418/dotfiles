ff33a4bc552138ac7c9dd69101f3362b81f95649
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-12-09 23:54:30 +0900

    Update:

--- config/nvim/lua/user/plugins/ddc/utils.lua
+++ config/nvim/lua/user/plugins/ddc/utils.lua
@@ -1,9 +1,7 @@
 local M = {}
-local vimx = require("artemis")
-local ddc_custom = vimx.fn.ddc.custom
 
-local patch_global = ddc_custom.patch_global
-local get_global = ddc_custom.get_global
+local patch_global = vim.fn['ddc#custom#patch_global']
+local get_global = vim.fn['ddc#custom#get_global']
 
 function M.change_filter(bang, filter_name)
     if filter_name == "normal" then
