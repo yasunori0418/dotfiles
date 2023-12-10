9a0d4e72d0cc40adff48579146f653f3dbda8b4b
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-12-10 22:59:57 +0900

    Update(#40): deol#start()で起動できるようにする

--- config/nvim/hooks/deol.lua
+++ config/nvim/hooks/deol.lua
@@ -1,36 +1,45 @@
 -- lua_add {{{
 local utils = require("user.utils")
 
----@param path? string default: vim.fn.getcwd()
----@param split? string default: 'floating'
----@return table
-local deol_create_option = function(path, split)
+---@alias deol_split_kinds
+---| "" # No split
+---| "floating" # Use neovim floating window feature
+---| "vertical" # Split buffer vertically
+---| "farleft" # Split buffer far left, like |CTRL-W_H|
+---| "farright" # Split buffer far right, like |CTRL-W_L|
+---| "horizontal" # Split buffer horizontally
+
+---@param path? string default: require("user.utils").search_repo_root()
+---@param split? deol_split_kinds default: 'floating'
+---@param magnification? integer default: 1
+local function deol_open(path, split, magnification)
     path = path or utils.search_repo_root()
     split = split or "floating"
+    magnification = magnification or 1
 
     local winheight = ""
     if vim.regex([[floating\|horizontal]]):match_str(split) then
-        winheight = "-winheight=" .. vim.fn.float2nr(vim.opt.lines:get() / 1.5)
+        winheight = vim.fn.float2nr(vim.opt.lines:get() / magnification)
     end
 
     local winwidth = ""
-    if vim.regex([[floating\|vertical]]):match_str(split) then
-        winwidth = "-winwidth=" .. vim.fn.float2nr(vim.opt.columns:get() / 1.5)
+    if vim.regex([[floating\|vertical\|farleft\|farright]]):match_str(split) then
+        winwidth = vim.fn.float2nr(vim.opt.columns:get() / magnification)
     end
 
-    local deol_opt = {
-        "Deol",
-        "-no-auto-cd",
-        "-no-start-insert",
-        "-cwd=" .. path,
-        "-split=" .. split,
-        winwidth,
-        winheight,
-        "-toggle",
+    local options = {
+        auto_cd = false,
+        cwd = path,
+        dir_changed = false,
+        edit = false,
+        split = split,
+        start_insert = false,
+        toggle = true,
+        winheight = winheight,
+        winwidth = winwidth,
     }
-    return vim.fn.map(deol_opt, function(_, value)
-        return '"' .. value .. '"'
-    end)
+
+    vim.fn["deol#start"](options)
 end
 
 local opt = { silent = true, noremap = true }
@@ -51,21 +60,7 @@ utils.keymaps_set({
         mode = "n",
         lhs = [[<Plug>(term)a]],
         rhs = function()
-            vim.cmd({
-                cmd = "execute",
-                args = deol_create_option(utils.search_repo_root()),
-            })
-        end,
-        opts = opt,
-    },
-    { -- project root term
-        mode = "n",
-        lhs = [[<Plug>(term)s]],
-        rhs = function()
-            vim.cmd({
-                cmd = "execute",
-                args = deol_create_option(utils.search_repo_root(), ""),
-            })
+            deol_open()
         end,
         opts = opt,
     },
@@ -73,10 +68,7 @@ utils.keymaps_set({
         mode = "n",
         lhs = [[<Plug>(term)c]],
         rhs = function()
-            vim.cmd({
-                cmd = "execute",
-                args = deol_create_option(vim.fn.fnamemodify(tostring(vim.fn.expand("%")), ":h")),
-            })
+            deol_open(vim.fn.fnamemodify(tostring(vim.fn.expand("%")), ":h"))
         end,
         opts = opt,
     },
@@ -84,10 +76,7 @@ utils.keymaps_set({
         mode = "n",
         lhs = [[<Plug>(term)~]],
         rhs = function()
-            vim.cmd({
-                cmd = "execute",
-                args = deol_create_option("~"),
-            })
+            deol_open("~")
         end,
         opts = opt,
     },
@@ -96,10 +85,7 @@ utils.keymaps_set({
         lhs = [[<Plug>(term)t]],
         rhs = function()
             vim.cmd.tabnew()
-            vim.cmd({
-                cmd = "execute",
-                args = deol_create_option(utils.search_repo_root(), ""),
-            })
+            deol_open(nil, "")
         end,
         opts = opt,
     },
