98a5df65c5560d97de983e3d380f27cac2d5653f
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-12-18 22:53:07 +0900

    Update: Neovimをビルドするときにtree-sitterのパーサを含めないようにするやつ

--- scripts/nvim_build.sh
+++ scripts/nvim_build.sh
@@ -17,7 +17,11 @@ fi
 
 [[ -d ${install_prefix} ]] && rm -rf ${install_prefix}
 
-make CMAKE_INSTALL_PREFIX=${install_prefix} CMAKE_BUILD_TYPE=Release install
+make \
+  CMAKE_INSTALL_PREFIX=${install_prefix} \
+  CMAKE_BUILD_TYPE=Release \
+  BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF' \
+  install
 
 rm -rf ${install_prefix}/lib
 
