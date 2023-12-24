b68c757fdb7064c83a16f67d2ca9ab0c9f7fdd70
yasunori0418 <yasunori.kirin0418@gmail.com>
2023-12-24 23:23:40 +0900

    Update: dpp.load_stateはbooleanではないので、ちゃんと比較しよう

--- config/dpp/lua/user/rc.lua
+++ config/dpp/lua/user/rc.lua
@@ -28,7 +28,7 @@ end
 
 local function dpp_setup()
     local dpp = require("dpp")
-    if dpp.load_state(M.dpp_dir) then
+    if dpp.load_state(M.dpp_dir) > 0 then
         vim.api.nvim_create_autocmd("User", {
             pattern = "DenopsReady",
             callback = function()
