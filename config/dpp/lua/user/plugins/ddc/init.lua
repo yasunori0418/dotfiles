5ef99b8d27a248bb25e09b93dab8dd0461aaebfb
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-04 15:33:13 +0900

    Delete: 移行完了につき、作業用のディレクトリを削除

--- config/dpp/lua/user/plugins/ddc/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.cmdline_completion = require("user.plugins.ddc.cmdline_completion").commandline_pre
-
-M.change_filter = require("user.plugins.ddc.utils").change_filter
-
-return M
