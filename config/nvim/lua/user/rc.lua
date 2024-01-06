9a41ea5e8eeabc102921201490573277f7fd5e8c
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-06 14:28:42 +0900

    refactor: formatted

--- config/nvim/lua/user/rc.lua
+++ config/nvim/lua/user/rc.lua
@@ -31,11 +31,7 @@ local function dpp_setup()
     local dpp = require("dpp")
     local rc_autocmds = vim.api.nvim_create_augroup("RcAutocmds", { clear = true })
     if dpp.load_state(M.dpp_dir) > 0 then
-        dpp.make_state(
-            M.dpp_dir,
-            joinpath(vim.g.base_dir, "dpp", "config.ts"),
-            M.nvim_appname
-        )
+        dpp.make_state(M.dpp_dir, joinpath(vim.g.base_dir, "dpp", "config.ts"), M.nvim_appname)
     else
         vim.api.nvim_create_autocmd("BufWritePost", {
             pattern = {
