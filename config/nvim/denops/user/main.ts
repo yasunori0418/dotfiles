cac39196691b6600cfa80af406772e285b4a36bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-08 18:41:47 +0900

    Add: いつか、ここにdenops側で設定するvimrcを書くんだ…

---
+++ config/nvim/denops/user/main.ts
@@ -0,0 +1,8 @@
+import { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
+
+export function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
+  };
+
+  return Promise.resolve();
+}
