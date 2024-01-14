05f07c63779ec8e1c7a89112976ff6f58c6353f1
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-14 16:30:24 +0900

    Update: codeiumの設定はこのくらいで大丈夫かな

--- config/nvim/hooks/codeium.lua
+++ config/nvim/hooks/codeium.lua
@@ -1,9 +1,5 @@
--- lua_add {{{
-
--- }}}
-
 -- lua_source {{{
-vim.g.codeium_disable_bindings = 1
-vim.g.codeium_render = false
+-- vim.g.codeium_disable_bindings = 1
+vim.g.codeium_render = true
 vim.g.codeium_manual = true
 -- }}}
