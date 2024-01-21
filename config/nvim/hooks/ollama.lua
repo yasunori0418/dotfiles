832b51bfd8f90484b0b560c55009ae564b542fc4
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-21 23:36:08 +0900

    Update: ローカルLLM楽しい

--- config/nvim/hooks/ollama.lua
+++ config/nvim/hooks/ollama.lua
@@ -1,5 +1,11 @@
 -- lua_add {{{
 
+vim.keymap.set("n", [[ ac]], function()
+    vim.fn["ollama#start_chat"]("codellama:13b", {
+        openner = "tabnew",
+    })
+end, {})
+
 -- }}}
 
 -- lua_source {{{
