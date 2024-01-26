63489011ff694c0f2ec7b9de8f5b2242291140a3
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-26 20:35:53 +0900

    Update: コマンドの定義のコールバックの中でrequireするように変更

--- config/nvim/rc/commands.lua
+++ config/nvim/rc/commands.lua
@@ -1,29 +1,27 @@
 local command = vim.api.nvim_create_user_command
-local ddc_change_filter = require("user.plugins.ddc").change_filter
-local dpp = require("dpp")
 
 command("DppInstall", function()
-    dpp.sync_ext_action("installer", "install")
+    require("dpp").sync_ext_action("installer", "install")
 end, {})
 
 command("DppUpdate", function()
-    dpp.async_ext_action("installer", "update")
+    require("dpp").async_ext_action("installer", "update")
 end, {})
 
 command("DppClear", function()
-    dpp.clear_state()
+    require("dpp").clear_state()
     vim.loader.reset()
     vim.cmd.quit()
 end, {})
 
 command("DDCFuzzyFilter", function(opts)
-    ddc_change_filter(opts.bang, "fuzzy")
+    require("user.plugins.ddc").change_filter(opts.bang, "fuzzy")
 end, { bang = true })
 
 command("DDCNormalFilter", function(opts)
-    ddc_change_filter(opts.bang, "normal")
+    require("user.plugins.ddc").change_filter(opts.bang, "normal")
 end, { bang = true })
 
 command("DDCEchoFilter", function()
-    ddc_change_filter(1, "")
+    require("user.plugins.ddc").change_filter(1, "")
 end, {})
