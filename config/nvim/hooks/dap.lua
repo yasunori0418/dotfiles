54e86fa38a50506acda3d646b828d47aaa55f045
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-27 17:34:32 +0900

    Update: 雑にキーマップを設定してみる

--- config/nvim/hooks/dap.lua
+++ config/nvim/hooks/dap.lua
@@ -1,5 +1,61 @@
 -- lua_add {{{
-
+local opt = { noremap = false }
+require("user.utils").keymaps_set({
+    -- {
+    --     mode = "n",
+    --     lhs = [[ r]],
+    --     rhs = [[<Plug>(dap)]],
+    --     opts = opt,
+    -- },
+    -- {
+    --     mode = "n",
+    --     lhs = [[<Plug>(dap)]],
+    --     rhs = function()
+    --         -- various
+    --     end,
+    --     opts = opt,
+    -- },
+    {
+        mode = "n",
+        lhs = [[<F5>]],
+        rhs = function()
+            require("dap").continue()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<F10>]],
+        rhs = function()
+            require("dap").step_over()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<F11>]],
+        rhs = function()
+            require("dap").step_into()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<F12>]],
+        rhs = function()
+            require("dap").step_out()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<F3>]],
+        rhs = function()
+            require("dapui").toggle()
+        end,
+        opts = opt,
+    },
+})
 -- }}}
 
 -- lua_source {{{
