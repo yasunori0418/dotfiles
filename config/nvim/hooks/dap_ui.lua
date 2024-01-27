5ccb99a7b767546989e47dad517f8db5c385f4cb
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-27 17:33:53 +0900

    Update: コピペでとりあえずdapUiを動かしてみる

--- config/nvim/hooks/dap_ui.lua
+++ config/nvim/hooks/dap_ui.lua
@@ -3,5 +3,27 @@
 -- }}}
 
 -- lua_source {{{
-
+require("dapui").setup({
+    icons = { expanded = "", collapsed = "" },
+    layouts = {
+        {
+            elements = {
+                { id = "watches", size = 0.20 },
+                { id = "stacks", size = 0.20 },
+                { id = "breakpoints", size = 0.20 },
+                { id = "scopes", size = 0.40 },
+            },
+            size = 64,
+            position = "right",
+        },
+        {
+            elements = {
+                "repl",
+                "console",
+            },
+            size = 0.20,
+            position = "bottom",
+        },
+    },
+})
 -- }}}
