acb02eb4f3618ebbfd51cc7e7d44bbc0c04eb838
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-28 23:48:34 +0900

    Update: lua_lsでruntimeを読み込めていない問題を解決

--- config/nvim/lua/user/lsp/lua_ls.lua
+++ config/nvim/lua/user/lsp/lua_ls.lua
@@ -1,12 +1,33 @@
+local function library()
+    local dpp = require("dpp")
+    local paths = {}
+    for _, plugin in pairs(dpp.get()) do
+        table.insert(paths, plugin.path .. "/lua")
+    end
+    table.insert(paths, vim.fn.stdpath("config") .. "/lua")
+    table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
+    table.insert(paths, "${3rd}/luv/library")
+    table.insert(paths, "${3rd}/busted/library")
+    table.insert(paths, "${3rd}/luassert/library")
+    return paths
+end
+
 return function()
     require("lspconfig").lua_ls.setup({
         capabilities = require("user.lsp.utils").capabilities,
         settings = {
             Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using
+                    -- (most likely LuaJIT in the case of Neovim)
+                    pathStrict = true,
+                    version = "LuaJIT",
+                    path = { "?.lua", "?/init.lua" },
+                },
                 workspace = {
                     checkThirdParty = false,
-                    library = vim.api.nvim_get_runtime_file("lua", true),
-                    maxPreload = 1000,
+                    library = library(),
+                    -- maxPreload = 1000,
                 },
                 completion = {
                     callSnippet = "Both",
