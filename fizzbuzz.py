eb1a5209bf721dd07d443a5c153dc23009719300
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-28 23:07:33 +0900

    Update:

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,15 @@
-# fizzbazz
-def fizzbazz(loop_index: int):
+# fizzbuzz
+def fizzbuzz(loop_index: int):
     for i in range(1, loop_index):
         if i % 15 == 0:
-            print("FizzBazz")
+            print("FizzBuzz")
         elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
-            print("Bazz")
+            print("Buzz")
         else:
             print(i)
 
 
 if __name__ == "__main__":
-    fizzbazz(100)
+    fizzbuzz(100)
