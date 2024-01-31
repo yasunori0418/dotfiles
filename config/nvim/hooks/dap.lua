57e38ab83fb169dc66969cf02e54560b48dd1a1b
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-01-31 23:41:00 +0900

    Update: dapで使いそうなキーマップをとりあえず設定しておく

--- config/nvim/hooks/dap.lua
+++ config/nvim/hooks/dap.lua
@@ -1,20 +1,68 @@
 -- lua_add {{{
 local opt = { noremap = false }
 require("user.utils").keymaps_set({
-    -- {
-    --     mode = "n",
-    --     lhs = [[ r]],
-    --     rhs = [[<Plug>(dap)]],
-    --     opts = opt,
-    -- },
-    -- {
-    --     mode = "n",
-    --     lhs = [[<Plug>(dap)]],
-    --     rhs = function()
-    --         -- various
-    --     end,
-    --     opts = opt,
-    -- },
+    {
+        mode = "n",
+        lhs = [[ r]],
+        rhs = [[<Plug>(dap)]],
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)b]],
+        rhs = function()
+            require("dap").toggle_breakpoint()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)B]],
+        rhs = function()
+            require("dap").set_breakpoint()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)lp]],
+        rhs = function()
+            require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)C]],
+        rhs = function()
+            require("dap").clear_breakpoints()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)L]],
+        rhs = function()
+            require("dap").list_breakpoints()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)dr]],
+        rhs = function()
+            require("dap").repl.toggle()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(dap)dl]],
+        rhs = function()
+            require("dap").run_last()
+        end,
+        opts = opt,
+    },
     {
         mode = "n",
         lhs = [[<F5>]],
