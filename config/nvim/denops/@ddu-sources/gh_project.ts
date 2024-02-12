32e06b21a3f742adac67649740e5617d79f2636d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-02-12 23:25:30 +0900

    Update: ghコマンドから取得できるjsonは取れるようになった

--- config/nvim/denops/@ddu-sources/gh_project.ts
+++ config/nvim/denops/@ddu-sources/gh_project.ts
@@ -13,6 +13,7 @@ import {
 } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { join } from "https://deno.land/std@0.215.0/path/mod.ts";
 import { ActionData } from "../@ddu-kinds/gh_project.ts";
+import { JSONLinesParseStream } from "https://deno.land/x/jsonlines@v1.2.2/mod.ts";
 
 type Params = {
   cmd?: string;
@@ -32,7 +33,32 @@ export class Source extends BaseSource<Params> {
 
         const tree = async (root: string) => {
           const items: Item<ActionData>[] = [];
+
+          const { stdout } = new Deno.Command("gh", {
+            args: [
+              "project",
+              "list",
+              "--owner",
+              "@me",
+              "--limit",
+              "0",
+              "--format",
+              "json",
+              "--jq",
+              ".projects"
+            ],
+            stdin: "null",
+            stderr: "null",
+            stdout: "piped",
+          }).spawn();
+          const readable = stdout
+            .pipeThrough(new TextDecoderStream())
+            .pipeThrough(new JSONLinesParseStream());
+
           try {
+            for await (const chunk of readable) {
+              console.log(chunk);
+            }
             for await (const entry of Deno.readDir(root)) {
               const path = join(root, entry.name);
 
