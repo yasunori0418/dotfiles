f65e803d5614b219baa0c5ed9dc64a72a3f3b07e
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-02-15 22:27:15 +0900

    Update: gh_projectのソース内で型を定義しておく

--- config/nvim/denops/@ddu-sources/gh_project.ts
+++ config/nvim/denops/@ddu-sources/gh_project.ts
@@ -57,6 +57,27 @@ type Params = {
   limit: number;
 };
 
+type GHProject = {
+  closed: boolean,
+  fields: {
+    totalCount: number
+  },
+  id: string,
+  items: {
+    totalCount: number,
+  },
+  number: number,
+  owner: {
+    login: string,
+    type: string
+  },
+  public: boolean,
+  readme: string,
+  shortDescription: string,
+  title: string,
+  url: string
+};
+
 export class Source extends BaseSource<Params> {
   override kind = "gh_project";
 
