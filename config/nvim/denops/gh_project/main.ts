fba010f7927a878fda86ccb3dcebd739586f8aaa
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-04 18:22:56 +0900

    add: denops/gh_project

---
+++ config/nvim/denops/gh_project/main.ts
@@ -0,0 +1,8 @@
+import { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
+
+export function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
+  };
+
+  return Promise.resolve();
+}
