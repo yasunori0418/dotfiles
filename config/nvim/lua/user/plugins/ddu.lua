7663d490360ce5f087d7508a8ec63233751f9a89
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-12 23:10:45 +0900

    feat: dduをinputでstartFilterするヘルパーを追加

--- config/nvim/lua/user/plugins/ddu.lua
+++ config/nvim/lua/user/plugins/ddu.lua
@@ -20,4 +20,63 @@ function M.move_ignore_dummy(dir)
     end
 end
 
+---ddu-ui-ff startFilter using input function.
+---@param options table # :h ddu-options
+---@param after_filter_flag? boolean # Open filter window after `Ddu:uiReady`. default: `false`
+---@param completion? # :h command-completion | default: nil
+---| "arglist"
+---| "augroup"
+---| "buffer"
+---| "behave"
+---| "color"
+---| "command"
+---| "compiler"
+---| "cscope"
+---| "dir"
+---| "environment"
+---| "event"
+---| "expression"
+---| "file"
+---| "file_in_path"
+---| "filetype"
+---| "function"
+---| "help"
+---| "highlight"
+---| "history"
+---| "locale"
+---| "mapclear"
+---| "mapping"
+---| "menu"
+---| "messages"
+---| "option"
+---| "packadd"
+---| "shellcmd"
+---| "sign"
+---| "syntax"
+---| "syntime"
+---| "tag"
+---| "tag_listfiles"
+---| "user"
+---| "var"
+---| nil
+function M.start_input_filter(options, after_filter_flag, completion)
+    after_filter_flag = after_filter_flag or false
+    completion = completion or nil
+    if completion then
+        options.input = vim.fn.input("Pattern: ", "", completion)
+    else
+        options.input = vim.fn.input("Pattern: ", "")
+    end
+    if after_filter_flag then
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "Ddu:uiReady",
+            callback = function()
+                vim.fn.timer_start(5, vim.fn["ddu#ui#do_action"]("openFilterWindow"))
+            end,
+            once = true,
+        })
+    end
+    vim.fn["ddu#start"](options)
+end
+
 return M
