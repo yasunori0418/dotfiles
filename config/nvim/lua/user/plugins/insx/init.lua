035ea93d87226e0b0ddda9d9c5ae56ea535939af
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-31 02:25:30 +0900

    wip: このファイルでいろんなヘルパーを呼びたいなって

---
+++ config/nvim/lua/user/plugins/insx/init.lua
@@ -0,0 +1,36 @@
+-- local insx = require("insx")
+-- local helper = require("insx.helper")
+-- local esc = require("insx.helper.regex").esc
+-- local jump_next = require("insx.recipe.jump_next")
+-- local builtin_auto_pairs = require("insx.recipe.auto_pair")
+-- local builtin_delete_pair = require("insx.recipe.delete_pair")
+
+local M = {}
+
+
+-- local rules = {
+--     {
+--         open = [[(]],
+--         close = [[)]],
+--         options = {},
+--     },
+--     {
+--         open = [[{]],
+--         close = [[}]],
+--         options = {},
+--     },
+--     {
+--         open = [=[[]=],
+--         close = [=[]]=],
+--         options = {},
+--     },
+-- }
+
+
+-- local rules = {
+--     { quote = [[']] },
+--     { quote = [["]] },
+--     { quote = [[`]] },
+-- }
+
+return M
