4e0cc314a1ec0aefa08cb635b85c9e5ba7147698
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-31 02:19:28 +0900

    update: とりあえずプリセットを全て有効にしてみる

--- config/nvim/hooks/insx.lua
+++ config/nvim/hooks/insx.lua
@@ -4,4 +4,19 @@
 
 -- lua_source {{{
 
+require("insx.preset.standard").setup({
+    cmdline = {
+        enabled = true,
+    },
+    fast_break = {
+        enabled = true,
+    },
+    fast_wrap = {
+        enabled = true,
+    },
+    spacing = {
+        enabled = true,
+    },
+})
+
 -- }}}
