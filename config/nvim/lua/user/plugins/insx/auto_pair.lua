080574a80f69d8dacd448273528c76ea1a3e447c
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-31 02:21:25 +0900

    wip: auto_pair override

---
+++ config/nvim/lua/user/plugins/insx/auto_pair.lua
@@ -0,0 +1,23 @@
+local with_options = require("user.plugins.insx.with_options")
+
+---@class AutoPairRule
+---@diagnostic disable:duplicate-doc-field
+---@field public open string
+---@field public close string
+---@field public options InsxWithOptions
+---@field public new fun(rule: { open: string, close: string, options?: table<string, string> }): AutoPairRule
+local AutoPairRule = {}
+
+---AutoPairRule initializer
+---@param rule { open: string, close: string, options?: table<string, string> }
+---@return AutoPairRule
+function AutoPairRule.new(rule)
+    local obj = {
+        open = rule.open,
+        close = rule.close,
+        options = with_options.new(rule.options or {}),
+    }
+    return setmetatable(obj, { __index = { AutoPairRule } })
+end
+
+return AutoPairRule
