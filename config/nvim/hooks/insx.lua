126019fd7cc1999409535e582ec6627e0a78803a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-01 00:47:15 +0900

    wip

--- config/nvim/hooks/insx.lua
+++ config/nvim/hooks/insx.lua
@@ -1,22 +1,68 @@
--- lua_add {{{
-
--- }}}
-
 -- lua_source {{{
 
-require("insx.preset.standard").setup({
-    cmdline = {
-        enabled = true,
+-- require("insx.preset.standard").setup({
+--     cmdline = {
+--         enabled = true,
+--     },
+--     fast_break = {
+--         enabled = true,
+--     },
+--     fast_wrap = {
+--         enabled = true,
+--     },
+--     spacing = {
+--         enabled = true,
+--     },
+-- })
+
+local insx_helper = require("user.plugins.insx")
+
+---@type { open: string, close: string, options?: WithOptions, fast_break?: FastBreakOption }[]
+local auto_pairs = {
+    {
+        open = [[(]],
+        close = [[)]],
+        options = {
+            undopoint = true,
+            priority = 10,
+        },
+        fast_break = {
+            arguments = true,
+            html_attrs = true,
+            html_tags = true,
+        }
     },
-    fast_break = {
-        enabled = true,
+    {
+        open = [[{]],
+        close = [[}]],
+        options = {
+            undopoint = true,
+            nomatch = [[\%#\w]],
+        },
+        fast_break = {
+            arguments = true,
+            html_attrs = true,
+            html_tags = true,
+        }
     },
-    fast_wrap = {
-        enabled = true,
+    {
+        open = [=[[]=],
+        close = [=[]]=],
+        options = {
+            undopoint = true,
+            nomatch = [[\%#\w]],
+        },
+        fast_break = {
+            arguments = true,
+            html_attrs = true,
+            html_tags = true,
+        }
     },
-    spacing = {
-        enabled = true,
-    },
-})
+}
+
+for _, auto_pair in ipairs(auto_pairs) do
+    local rule = insx_helper.auto_pair.new(auto_pair)
+    insx_helper.auto_pair.apply(rule)
+end
 
 -- }}}
