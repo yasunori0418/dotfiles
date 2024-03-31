6866884db3eb5f52639f7a8117d8d34c8df0099d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-31 22:58:30 +0900

    update: use alias

--- config/nvim/lua/user/plugins/insx/auto_pair.lua
+++ config/nvim/lua/user/plugins/insx/auto_pair.lua
@@ -5,11 +5,11 @@ local with_options = require("user.plugins.insx.with_options")
 ---@field public open string
 ---@field public close string
 ---@field public options InsxWithOptions
----@field public new fun(rule: { open: string, close: string, options?: table<string, string> }): AutoPairRule
+---@field public new fun(rule: { open: string, close: string, options?: WithOptions }): AutoPairRule
 local AutoPairRule = {}
 
 ---AutoPairRule initializer
----@param rule { open: string, close: string, options?: table<string, string> }
+---@param rule { open: string, close: string, options?: WithOptions }
 ---@return AutoPairRule
 function AutoPairRule.new(rule)
     local obj = {
