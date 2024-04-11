5adf8cb1b45f8b749bbbe53489c553fb0a927920
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-11 23:17:31 +0900

    update:

--- scripts/nvim_build.sh
+++ scripts/nvim_build.sh
@@ -2,28 +2,29 @@
 
 set -e -u -o pipefail
 
-declare -r ghq_root=$(ghq root)
+ghq_root=$(ghq root)
+readonly ghq_root
 declare -r host=github.com
 declare -r repo=neovim/neovim
 declare -r install_prefix=${HOME}/.local/dotfiles/nvim
 
 ghq get -u https://${host}/${repo}.git
-cd ${ghq_root}/${host}/${repo}
+cd "${ghq_root}/${host}/${repo}"
 
 if [[ -d ./.deps ]]; then
   make clean
   git restore .
 fi
 
-[[ -d ${install_prefix} ]] && rm -rf ${install_prefix}
+[[ -d ${install_prefix} ]] && rm -rf "${install_prefix}"
 
 make \
-  CMAKE_INSTALL_PREFIX=${install_prefix} \
+  CMAKE_INSTALL_PREFIX="${install_prefix}" \
   CMAKE_BUILD_TYPE=Release \
   BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF' \
   install
 
-rm -rf ${install_prefix}/lib
+rm -rf "${install_prefix:?}/lib"
 
 if [[ $(command -v nvim) ]]; then
   nvim -V1 -v
