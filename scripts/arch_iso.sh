98ec6b9405e3b99a7cbf0f02ee8c74af5ba5296a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-11 22:55:43 +0900

    update:

--- scripts/arch_iso.sh
+++ scripts/arch_iso.sh
@@ -4,26 +4,26 @@
 
 set -e -u -o pipefail
 
-declare -r date_ym1="$(date +'%Y-%m-%d')"
+date_ym1="$(date +'%Y-%m-%d')"
+readonly date_ym1
 declare -r img_file="archlinux-x86_64.iso"
-declare -r sig_file="${img_file}.sig"
 declare -r iso_url="http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/iso/latest/${img_file}"
 declare -r sig_url="${iso_url}.sig"
 declare -r download_dir="${HOME}/Downloads/arch_iso_${date_ym1}"
 
-[[ ! -d ${download_dir} ]] && mkdir -p ${download_dir}
-cd ${download_dir}
+[[ ! -d ${download_dir} ]] && mkdir -p "${download_dir}"
+cd "${download_dir}"
 
 curl -OL ${iso_url} \
      -OL ${sig_url}
 
 if [[ $(command -v pacman-key) ]]; then
-  pacman-key -v ${img_file}.sig
+  pacman-key -v "${img_file}.sig"
   exit 0
 fi
 
 if [[ $(command -v gpg) ]]; then
-  gpg --keyserver-options auto-key-retrieve --verify ${img_file}.sig
+  gpg --keyserver-options auto-key-retrieve --verify "${img_file}.sig"
   exit 0
 fi
 
