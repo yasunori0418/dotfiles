999306db08530e1b3e06f243394d07be481b18e7
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-12 00:24:50 +0900

    update:

--- scripts/nvim_night.sh
+++ scripts/nvim_night.sh
@@ -7,14 +7,14 @@ declare -r tag_name=nightly
 declare -r dl_file=nvim-linux64.tar.gz
 declare -r download_url=https://github.com/neovim/neovim/releases/download/${tag_name}/${dl_file}
 
-curl -Lo ${install_prefix}/${dl_file} ${download_url}
+curl -Lo "${install_prefix}/${dl_file}" "${download_url}"
 
-cd ${install_prefix}
-tar zxvf ${dl_file}
-rm ${dl_file}
+cd "${install_prefix}"
+tar zxvf "${dl_file}"
+rm "${dl_file}"
 
-[[ -d ${install_prefix}/nvim ]] && rm -rf ${install_prefix}/nvim
-mv ${install_prefix}/nvim-linux64 ${install_prefix}/nvim
+[[ -d ${install_prefix}/nvim ]] && rm -rf "${install_prefix}/nvim"
+mv "${install_prefix}/nvim-linux64" "${install_prefix}/nvim"
 rm -rf nvim/lib
 
 echo -e '\n\n\n'
