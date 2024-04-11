cca8890a1b077759404572b079fe01d01c1a43a1
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-12 00:23:44 +0900

    Update:

--- scripts/work_repolist.sh
+++ scripts/work_repolist.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 export GHQ_ROOT=${HOME}/work
-declare -r gist_id=$(\
+gist_id=$(\
   gh gist list \
   | rg work_repolist \
   | awk -F ' ' '{print $1}' \
 )
+readonly gist_id
 
 if [[ ${1} == 'view' ]]; then
-  gh gist view ${gist_id}
+  gh gist view "${gist_id}"
 else
-  gh gist view ${gist_id} | ghq get -p -u --parallel
+  gh gist view "${gist_id}" | ghq get -p -u --parallel
 fi
