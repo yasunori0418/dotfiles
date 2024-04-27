b157d7a189c9e4df7b7eb0e71faecce3671f5bac
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-27 13:12:54 +0900

    update: previm config

--- config/nvim/hooks/previm/previm.lua
+++ config/nvim/hooks/previm/previm.lua
@@ -1,9 +1,24 @@
 -- lua_add {{{
 local opt = { noremap = true }
+vim.g.previm_open_cmd = "google-chrome-stable"
 require("user.utils").keymaps_set({
     { mode = "n", lhs = [[ p]], rhs = [[<Plug>(previm)]], opts = {} },
-    { mode = "n", lhs = [[<Plug>(previm)o]], rhs = [[<Cmd>PrevimOpen<CR>]], opts = opt },
-    { mode = "n", lhs = [[<Plug>(previm)r]], rhs = [[<Cmd>call previm#refresh()<CR>]], opts = opt },
+    {
+        mode = "n",
+        lhs = [[<Plug>(previm)o]],
+        rhs = function()
+            vim.cmd.PrevimOpen()
+        end,
+        opts = opt,
+    },
+    {
+        mode = "n",
+        lhs = [[<Plug>(previm)r]],
+        rhs = function()
+            vim.fn["previm#refresh"]()
+        end,
+        opts = opt,
+    },
 })
 -- }}}
 
