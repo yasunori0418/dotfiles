faf1ce08a3fd89884333e269a58e73ef948b81c5
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-04-30 20:09:26 +0900

    update:

--- scripts/update_pkglist.sh
+++ scripts/update_pkglist.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 host_type=$(uname -n | awk -F '-' '{print $2}')
 
-yay -Qeqn | rg -v "nvidia|amd|intel" > ./document/pkglist.txt
+yay -Qeqn | rg -v "nvidia|ollama-cuda" > ./document/pkglist.txt
 yay -Qeqm | rg -v "rtl8812au|epsonscan" > ./document/pkglist-foreign.txt
 
 if [[ ${host_type} == 'laptop' ]]; then
-  yay -Qeq | rg "amd" > ./document/pkglist-laptop.txt
+  # yay -Qeq | rg "amd" > ./document/pkglist-laptop.txt
 elif [[ ${host_type} == 'desktop' ]]; then
-  yay -Qeq | rg "nvidia|intel|rtl8812au|epsonscan|minecraft" > ./document/pkglist-desktop.txt
+  yay -Qeq | rg "nvidia|epsonscan|minecraft|ollama-cuda" > ./document/pkglist-desktop.txt
 fi
