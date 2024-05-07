d1d2d86830f69374c8e67e2739eec8dd10fc6b65
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-08 00:14:14 +0900

    update: wezterm search mode

--- config/wezterm/configs/keybinds.lua
+++ config/wezterm/configs/keybinds.lua
@@ -103,10 +103,9 @@ M.key_tables = {
     },
 
     search_mode = {
-        { key = "Enter", mods = "NONE", action = act.CopyMode("PriorMatch") },
+        { key = "Enter", mods = "NONE", action = act.CopyMode("NextMatch") },
+        { key = "Enter", mods = "SHIFT", action = act.CopyMode("PriorMatch") },
         { key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
-        { key = "n", mods = "CTRL", action = act.CopyMode("NextMatch") },
-        { key = "p", mods = "CTRL", action = act.CopyMode("PriorMatch") },
         { key = "r", mods = "CTRL", action = act.CopyMode("CycleMatchType") },
         { key = "u", mods = "CTRL", action = act.CopyMode("ClearPattern") },
         { key = "PageUp", mods = "NONE", action = act.CopyMode("PriorMatchPage") },
