3a5058739921be8f7964f12c196335ccd3394a9d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-09 23:54:31 +0900

    update:

--- config/nvim/hooks/hlchunk.lua
+++ config/nvim/hooks/hlchunk.lua
@@ -1,27 +1,13 @@
 -- lua_source {{{
+local utils = require("user.utils")
 
----get color code from hlGroup
----@param hl_name string
----@param type? "fg" | "bg"
----@param mode? "gui" | "cterm" | "term"
----@return string
-local function get_color_code(hl_name, type, mode)
-    type = type or "fg"
-    mode = mode or "gui"
-    return vim.fn.synIDattr(
-        vim.fn.synIDtrans(vim.fn.hlID(hl_name)),
-        type,
-        mode
-    )
-end
-
-local red = get_color_code("Error")
--- local yellow = get_color_code("Type")
-local blue = get_color_code("Title")
--- local orange = get_color_code("Boolean")
--- local green = get_color_code("String")
-local cyan = get_color_code("Keyword")
-local light_gray = get_color_code("Whitespace")
+local red = utils.get_color_code("Error")
+-- local yellow = utils.get_color_code("Type")
+local blue = utils.get_color_code("Title")
+-- local orange = utils.get_color_code("Boolean")
+-- local green = utils.get_color_code("String")
+local cyan = utils.get_color_code("Keyword")
+local light_gray = utils.get_color_code("Whitespace")
 
 local ft = require("hlchunk.utils.filetype")
 require("hlchunk").setup({
@@ -36,9 +22,9 @@ require("hlchunk").setup({
         chars = {
             horizontal_line = "─",
             vertical_line = "│",
-            left_top = "╭",
-            left_bottom = "╰",
-            right_arrow = ">",
+            left_top = "┌",
+            left_bottom = "└",
+            right_arrow = "─",
         },
         style = {
             { fg = cyan },
@@ -54,12 +40,15 @@ require("hlchunk").setup({
         use_treesitter = true,
         chars = {
             "│",
-            "¦",
-            "┆",
-            "┊",
         },
         style = {
-            { fg = get_color_code("Whitespace") },
+            "#FF0000",
+            "#FF7F00",
+            "#FFFF00",
+            "#00FF00",
+            "#00FFFF",
+            "#0000FF",
+            "#8B00FF",
         },
     },
 
@@ -73,7 +62,7 @@ require("hlchunk").setup({
         enable = true,
         use_treesitter = true,
         chars = {
-            " ",
+            ".",
         },
         style = {
             { fg = light_gray },
