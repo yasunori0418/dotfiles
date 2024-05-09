b47ab5860b96189a86e440df76bd5cda800d895d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-09 22:08:47 +0900

    update: 'Moralerspace \w+ NF'で遊んでみる

--- config/wezterm/configs/font.lua
+++ config/wezterm/configs/font.lua
@@ -4,6 +4,11 @@ local wezterm = require("wezterm")
 function M.setup(config)
     -- HackGen好き
     config.font = wezterm.font("HackGen35 Console NF")
+    -- config.font = wezterm.font("Moralerspace Neon NF")
+    -- config.font = wezterm.font("Moralerspace Argon NF")
+    -- config.font = wezterm.font("Moralerspace Xenon NF")
+    -- config.font = wezterm.font("Moralerspace Radon NF")
+    -- config.font = wezterm.font("Moralerspace Krypton NF")
 
     -- フォントサイズは偶数でないと変になる。
     config.font_size = 16.0
