0143eff0e560cbfc83d769b7f894b878ead11ca9
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-12 00:02:57 +0900

    add: devbox activetor

---
+++ home/.zsh/sync/devbox.zsh
@@ -0,0 +1,11 @@
+# devbox
+devbox_cache="${XDG_CACHE_HOME}/devbox.zsh"
+devbox_json="${XDG_DATA_HOME}/devbox/global/default/devbox.json"
+if [[ -d /nix && $(command -v devbox) ]]; then
+  if [[ ! -r ${devbox_cache} || ${devbox_json} -nt ${devbox_cache} ]]; then
+    devbox global shellenv --init-hook > ${devbox_cache}
+  fi
+  source ${devbox_cache}
+fi
+
+unset devbox_cache devbox_json
