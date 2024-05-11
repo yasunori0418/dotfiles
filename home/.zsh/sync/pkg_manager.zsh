256c20c545deba303df2f111750c67ac92c7cff4
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-12 00:51:09 +0900

    update: merged package manager setting.

--- home/.zsh/sync/pkg_manager.zsh
+++ home/.zsh/sync/pkg_manager.zsh
@@ -1,3 +1,10 @@
+# mise
+mise_cache=${XDG_CACHE_HOME}/mise.zsh
+if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
+  mise activate zsh > ${mise_cache}
+fi
+source ${mise_cache}
+
 # devbox
 devbox_cache="${XDG_CACHE_HOME}/devbox.zsh"
 devbox_json="${XDG_DATA_HOME}/devbox/global/default/devbox.json"
@@ -8,4 +15,4 @@ if [[ -d /nix && $(command -v devbox) ]]; then
   source ${devbox_cache}
 fi
 
-unset devbox_cache devbox_json
+unset devbox_cache devbox_json mise_cache
