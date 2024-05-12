fd5868fe15d7b130e67a1481ce691258acbf71f7
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-13 00:39:56 +0900

    update: all in one setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,23 @@
 
 set -euo pipefail
 
+mkdir -p "${HOME}/.local/bin"
+mkdir -p "${HOME}/.local/dotfiles"
+mkdir -p "${HOME}/.config"
+mkdir -p "${HOME}/.cache"
+
+# aqua install
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer | bash
+
 cd "${HOME}/dotfiles"
+
+"${HOME}/dotfiles/scripts/expand_symlink.sh"
 
 # shellcheck disable=SC1091
 source "${HOME}/dotfiles/home/.zshenv"
 aqua install --only-link --all
 mise install --yes
 
-make nvim-night
+"${HOME}/dotfiles/scripts/nvim_night.sh"
 
 zsh
