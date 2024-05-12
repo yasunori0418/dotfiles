81512dbd0365120107d3d979df381d7f2a97022a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-12 21:04:13 +0900

    disable: devbox global

--- home/.zsh/sync/pkg_manager.zsh
+++ home/.zsh/sync/pkg_manager.zsh
@@ -5,14 +5,4 @@ if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
 fi
 source ${mise_cache}
 
-# devbox
-devbox_cache="${XDG_CACHE_HOME}/devbox.zsh"
-devbox_json="${XDG_DATA_HOME}/devbox/global/default/devbox.json"
-if [[ -d /nix && $(command -v devbox) ]]; then
-  if [[ ! -r ${devbox_cache} || ${devbox_json} -nt ${devbox_cache} ]]; then
-    devbox global shellenv --init-hook > ${devbox_cache}
-  fi
-  source ${devbox_cache}
-fi
-
-unset devbox_cache devbox_json mise_cache
+unset mise_cache
