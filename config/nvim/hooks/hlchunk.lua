c61db6f83a7ebe3edafe2747317b4a574b724b9f
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-05-25 21:16:04 +0900

    update: hlchunk.nvim

--- config/nvim/hooks/hlchunk.lua
+++ config/nvim/hooks/hlchunk.lua
@@ -1,96 +1,34 @@
 -- lua_source {{{
-local utils = require("user.utils")
+-- local utils = require("user.utils")
 
-local red = utils.get_color_code("Error")
+-- local red = utils.get_color_code("Error")
 -- local yellow = utils.get_color_code("Type")
-local blue = utils.get_color_code("Title")
+-- local blue = utils.get_color_code("Title")
 -- local orange = utils.get_color_code("Boolean")
 -- local green = utils.get_color_code("String")
-local cyan = utils.get_color_code("Keyword")
-local light_gray = utils.get_color_code("Whitespace")
-
-local ft = require("hlchunk.utils.filetype")
-ft.exclude_filetypes["ddu-ff"] = true
-ft.exclude_filetypes["ddu-filer"] = true
-ft.exclude_filetypes["deol"] = true
+-- local cyan = utils.get_color_code("Keyword")
+-- local light_gray = utils.get_color_code("Whitespace")
 
 require("hlchunk").setup({
     chunk = {
         enable = true,
         notify = true,
         use_treesitter = true,
-        -- details about support_filetypes and exclude_filetypes in
-        -- https://github.com/shellRaining/hlchunk.nvim/blob/main/lua/hlchunk/utils/filetype.lua
-        support_filetypes = ft.support_filetypes,
-        exclude_filetypes = ft.exclude_filetypes,
-        chars = {
-            horizontal_line = "─",
-            vertical_line = "│",
-            left_top = "┌",
-            left_bottom = "└",
-            right_arrow = "─",
+        exclude_filetypes = {
+            ["ddu-ff"] = true,
+            ["ddu-filer"] = true,
         },
-        style = {
-            { fg = cyan },
-            { fg = red }, -- this fg is used to highlight wrong chunk
-        },
-        textobject = "",
-        max_file_size = 1024 * 1024,
-        error_sign = true,
     },
-
     indent = {
         enable = true,
         use_treesitter = true,
-        notify = true,
-        support_filetypes = ft.support_filetypes,
-        exclude_filetypes = ft.exclude_filetypes,
-        chars = {
-            "│",
-        },
-        style = {
-            "#FF0000",
-            "#FF7F00",
-            "#FFFF00",
-            "#00FF00",
-            "#00FFFF",
-            "#0000FF",
-            "#8B00FF",
-        },
     },
-
     line_num = {
         enable = true,
         use_treesitter = true,
-        notify = true,
-        support_filetypes = ft.support_filetypes,
-        exclude_filetypes = ft.exclude_filetypes,
-        style = cyan,
     },
-
     blank = {
         enable = false,
-        use_treesitter = true,
-        chars = {
-            ".",
-        },
-        style = {
-            { fg = light_gray },
-        },
-    },
-
-    context = {
-        enable = true,
-        notify = true,
-        use_treesitter = false,
-        support_filetypes = ft.support_filetypes,
-        exclude_filetypes = ft.exclude_filetypes,
-        chars = {
-            "┃", -- Box Drawings Heavy Vertical
-        },
-        style = {
-            blue,
-        },
     },
 })
 -- }}}
