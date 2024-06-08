e77f775804963074a440814643fc2286f2e06341
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-06-08 23:37:34 +0900

    delete comment

--- config/nvim/rc/keymaps.lua
+++ config/nvim/rc/keymaps.lua
@@ -367,13 +367,6 @@ require("user.utils").keymaps_set({
         end,
         opts = noremap_silent,
     },
-    -- { -- 読む用・書く用
-        -- いまこの機能メンテナンスできていない
-    --     mode = { "n" },
-    --     lhs = [[ n]],
-    --     rhs = require("user.utils").toggle_view,
-    --     opts = noremap_silent,
-    -- },
     { -- qって結構誤爆するんだよね…
         mode = { "n", "x" },
         lhs = [[<C-Q>]],
