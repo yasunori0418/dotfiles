45d2b33c0ce68d00a0f68291b5b7e8b3c5388f5d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-06-16 02:00:54 +0900

    update: deolの設定させていただきありがとうございます

--- config/nvim/hooks/deol.lua
+++ config/nvim/hooks/deol.lua
@@ -42,19 +42,6 @@ vim.fn["deol#set_option"]({
     toggle = true,
     external_history_path = vim.fn.expand("~/.zhistory"),
     nvim_server = vim.fs.joinpath(vim.fn.stdpath("cache") --[[@as string]], "server.pipe"),
-    custom_maps = {
-        edit = "",
-        quit = "q",
-        bg = "<C-z>",
-        paste_prompt = "<C-y>",
-        next_prompt = "<C-n>",
-        previous_prompt = "<C-p>",
-        execute_line = "<CR>",
-        start_append_last = "A",
-        start_append = "a",
-        start_insert_first = "I",
-        start_insert = "i",
-    },
     floating_border = "single",
     prompt_pattern = "❯ ",
 })
@@ -66,4 +53,64 @@ vim.fn["deol#set_local_option"]("current", {
     winwidth = vim.fn.float2nr(vim.opt.columns:get() * 0.6),
 })
 
+-- }}}
+
+-- lua_deol {{{
+local deol_opt = { silent = true, noremap = true, buffer = true }
+require("user.utils").keymaps_set({
+    {
+        mode = { "n" },
+        lhs = [[<C-n>]],
+        rhs = [[<Plug>(deol_next_prompt)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[<C-p>]],
+        rhs = [[<Plug>(deol_previous_prompt)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[q]],
+        rhs = [[<Plug>(deol_quit)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[i]],
+        rhs = [[<Plug>(deol_start_insert)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[I]],
+        rhs = [[<Plug>(deol_start_insert_first)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[a]],
+        rhs = [[<Plug>(deol_start_append)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[A]],
+        rhs = [[<Plug>(deol_start_append_last)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[p]],
+        rhs = [[<Plug>(deol_paste_prompt)]],
+        opts = deol_opt,
+    },
+    {
+        mode = { "n" },
+        lhs = [[<CR>]],
+        rhs = [[<Plug>(deol_execute_line)]],
+        opts = deol_opt,
+    },
+})
 -- }}}
