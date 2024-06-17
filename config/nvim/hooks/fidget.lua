4f0909e5cd418559148029cb7a915a95a0e675c4
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-06-17 22:51:40 +0900

    update: vim.notifyをfidgetでオーバーライドする設定を無効化する

--- config/nvim/hooks/fidget.lua
+++ config/nvim/hooks/fidget.lua
@@ -59,7 +59,7 @@ fidget.setup({
         poll_rate = 10, -- How frequently to update and render notifications
         filter = vim.log.levels.INFO, -- Minimum notifications level
         history_size = 128, -- Number of removed messages to retain in history
-        override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+        override_vim_notify = false, -- Automatically override vim.notify() with Fidget
         -- How to configure notification groups when instantiated
         configs = { default = require("fidget.notification").default_config },
         -- Conditionally redirect notifications to another backend
