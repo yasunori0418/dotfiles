f7fab8f9d010ea5934e351de80258cd33347221c
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-06-20 22:46:39 +0900

    update: gitsignsのsignsの部分

--- config/nvim/hooks/gitsigns.lua
+++ config/nvim/hooks/gitsigns.lua
@@ -31,12 +31,20 @@ local gitsigns = require("gitsigns")
 
 gitsigns.setup({
     signs = {
-        add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "▁" },
+        topdelete = { text = "▔" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+    },
+    signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "▁" },
+        topdelete = { text = "▔" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
     },
 
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
