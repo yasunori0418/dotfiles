1e1c428ac3151687cf71abac1ba9c1c514f06a28
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-06-21 23:35:00 +0900

    update: gitsigns keymaps

--- config/nvim/lua/user/plugins/gitsigns.lua
+++ config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,15 +1,162 @@
 local M = {}
 
+local gitsigns = require("gitsigns")
+
 ---current_line_blame_formatter
 ---@param name string
 ---@param blame_info Gitsigns.BlameInfoPublic
 ---@return {[1]:string,[2]:"GitSignsCurrentLineBlame"}[]
 function M.blame_line_formatter(name, blame_info)
     if name == blame_info.author then
-        return {{[1] = "", [2] = "GitSignsCurrentLineBlame"}}
+        return { { [1] = "", [2] = "GitSignsCurrentLineBlame" } }
     end
     local text = blame_info.author .. ":" .. blame_info.abbrev_sha .. " - " .. blame_info.summary
-    return {{[1] = text, [2] = "GitSignsCurrentLineBlame"}}
+    return { { [1] = text, [2] = "GitSignsCurrentLineBlame" } }
+end
+
+---set keymaps by on_attach
+---@param bufnr integer
+function M.keymaps(bufnr)
+    ---@type vim.keymap.set.Opts
+    local opts = { nnoremap = true, buffer = bufnr }
+
+    ---@type Gitsigns.NavOpts
+    local nav_opts = {
+        wrap = true,
+        navigation_message = true,
+        foldopen = true,
+        preview = true,
+        greedy = true,
+        target = "all",
+        count = vim.v.count1,
+    }
+
+    require("user.utils").keymaps_set({
+        {
+            mode = { "n", "x" },
+            lhs = [[gs]],
+            rhs = [[<Plug>(gitsigns)]],
+            opts = { noremap = false, buffer = bufnr },
+        },
+        {
+            mode = { "n" },
+            lhs = "]c",
+            rhs = function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "]c", bang = true })
+                else
+                    gitsigns.nav_hunk("next", nav_opts)
+                end
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = "[c",
+            rhs = function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "[c", bang = true })
+                else
+                    gitsigns.nav_hunk("prev", nav_opts)
+                end
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)s]],
+            rhs = function()
+                gitsigns.stage_hunk()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)r]],
+            rhs = function()
+                gitsigns.reset_hunk()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "x" },
+            lhs = [[<Plug>(gitsigns)s]],
+            rhs = function()
+                gitsigns.stage_hunk({
+                    vim.fn.line("."),
+                    vim.fn.line("v"),
+                })
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "x" },
+            lhs = [[<Plug>(gitsigns)r]],
+            rhs = function()
+                gitsigns.reset_hunk({
+                    vim.fn.line("."),
+                    vim.fn.line("v"),
+                })
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)S]],
+            rhs = function()
+                gitsigns.stage_buffer()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)R]],
+            rhs = function()
+                gitsigns.reset_buffer()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)u]],
+            rhs = function()
+                gitsigns.undo_stage_hunk()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)p]],
+            rhs = function()
+                gitsigns.preview_hunk()
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)b]],
+            rhs = function()
+                gitsigns.blame_line({ full = true })
+            end,
+            opts = opts,
+        },
+        {
+            mode = { "n" },
+            lhs = [[<Plug>(gitsigns)d]],
+            rhs = function()
+                gitsigns.diffthis()
+            end,
+            opts = opts,
+        },
+        {
+            mode = {"o", "x"},
+            lhs = [[is]],
+            rhs = function()
+                gitsigns.select_hunk()
+            end,
+            opts = opts,
+        }
+    })
 end
 
 return M
