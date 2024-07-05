6d7b03f0e219162caace0d2e06b2ff8e54cbfd35
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-06 00:43:51 +0900

    chore: symlinkのverboseが汚なかったのでチョイ直し

--- scripts/expand_symlink.sh
+++ scripts/expand_symlink.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-ln -snvf ~/dotfiles/home/.??* ~/
-ln -snvf ~/dotfiles/home/package.json ~/
-ln -snvf ~/dotfiles/home/bun.lockb ~/
-ln -snvf ~/dotfiles/bin ~/
-ln -snvf ~/dotfiles/config/* ~/.config/
+ln -snvf ~/dotfiles/home/.??* ~
+ln -snvf ~/dotfiles/home/package.json ~
+ln -snvf ~/dotfiles/home/bun.lockb ~
+ln -snvf ~/dotfiles/bin ~
+ln -snvf ~/dotfiles/config/* ~/.config
 
 if [[ -d /nix && $(command -v devbox) ]]; then
   ln -svf ~/dotfiles/local/share/devbox/global/default/* ~/.local/share/devbox/global/default/
