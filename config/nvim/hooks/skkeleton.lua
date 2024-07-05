4f67c326d4c323351fdb440928a44a3974b51f05
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-05 23:58:08 +0900

    update: luaのannotationも使いこなしていきたい

--- config/nvim/hooks/skkeleton.lua
+++ config/nvim/hooks/skkeleton.lua
@@ -6,7 +6,7 @@ vim.keymap.set({ "i", "c" }, [[<C-j>]], [[<Plug>(skkeleton-toggle)]], { noremap 
 local skkeleton_autocmds = vim.api.nvim_create_augroup("skkeleton_autocmds", { clear = true })
 local skkeleton = require("user.plugins.skkeleton")
 
-local skkeleton_dir = tostring(vim.fn.expand("~/.skk"))
+local skkeleton_dir = vim.fn.expand("~/.skk") --[[@as string]]
 if vim.fn.isdirectory(skkeleton_dir) == 0 then
     vim.fn.mkdir(skkeleton_dir)
 end
