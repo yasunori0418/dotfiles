914c3ba974f7c336a9f959acc2f6efe9903b0819
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-07 22:34:47 +0900

    fix:

--- config/karabiner/utils.ts
+++ config/karabiner/utils.ts
@@ -3,7 +3,7 @@ import { $ } from "@david/dax";
 /** Get the bundle identifier of the application by name.
  * Refer: https://github.com/ryoppippi/dotfiles/blob/main/karabiner/utils.ts#L11-L27
  */
-export async function extractIdentifer(appName: string) {
+export async function extractIdentifer(appName: string): Promise<string | undefined> {
   const appPath = $.path(`/Applications/${appName}.app`);
 
   if (!await appPath.exists()) {
@@ -11,11 +11,11 @@ export async function extractIdentifer(appName: string) {
   }
 
   /* output is like `kMDItemCFBundleIdentifier = "com.apple.Safari"` */
-  const output = await $`mdls -name kMDItemCFBundleIdentifier ${appPath}`.text()
-    .then((l) => l.trim());
+  const output = await $`mdls -name kMDItemCFBundleIdentifier ${appPath}`
+    .text();
 
   /** output is like `com.apple.Safari` */
-  const identifer = output.split("=")[1].replace(/"/g, "").trim();
+  const identifer = output.match(/"(.*)"/)?.[1];
 
   return identifer;
 }
