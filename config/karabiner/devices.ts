283619f3fad35474b5943dce8de666333003e029
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-07 13:27:18 +0900

    Add: device list for use karabiner.ts

---
+++ config/karabiner/devices.ts
@@ -0,0 +1,32 @@
+import { type DeviceIdentifier } from "karabiner_ts";
+
+export const AppleInternalKeyboard: DeviceIdentifier = {
+  vendor_id: 1452,
+  product_id: 832,
+  is_keyboard: true,
+  is_built_in_keyboard: true,
+};
+
+export const TouchBarUserDevice: DeviceIdentifier = {
+  vendor_id: 1452,
+  product_id: 34304,
+  is_touch_bar: true,
+}
+
+export const KeychronK8: DeviceIdentifier = {
+  vendor_id: 1452,
+  product_id: 591,
+  is_keyboard: true,
+};
+
+export const HHKBProfessionalHybridTypeS: DeviceIdentifier = {
+  vendor_id: 1278,
+  product_id: 33,
+  is_keyboard: true,
+};
+
+export const KensingtonSlimBladePro: DeviceIdentifier = {
+  vendor_id: 1149,
+  product_id: 32982,
+  is_pointing_device: true,
+};
