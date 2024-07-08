c377a984254d84cadf811a0a0c7b5ba4e503b7c3
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-08 23:31:13 +0900

    update: UIで簡単に設定できるものは、karabiner.tsでは設定しない

--- config/karabiner/karabiner.ts
+++ config/karabiner/karabiner.ts
@@ -8,34 +8,8 @@ import {
 } from "./devices.ts";
 
 k.writeToProfile("Default", [
-  disableBuiltInKeyboard(),
-  swapCapsLockToCtrl(),
   startupWezterm(),
 ]);
-
-function swapCapsLockToCtrl() {
-  return k.rule("swapping caps_lock and left_control")
-    .condition(k.ifDevice([AppleBuiltInKeyboard, KeychronK8]))
-    .manipulators([
-      k.map("caps_lock").to("left_control"),
-      k.map("left_control").to("caps_lock"),
-      k.map({ key_code: "caps_lock", modifiers: { mandatory: ["left_shift"] } })
-        .toNone(),
-    ]);
-}
-
-function disableBuiltInKeyboard() {
-  const disableMappingRules = appleBuiltInKeyboardKeyCodes.map((keyCode) => {
-    return k.map(keyCode).toNone();
-  });
-  return k.rule(
-    "disable built in apple keyboard when connection another keyboards.",
-  )
-    .condition(
-      k.ifDevice(AppleBuiltInKeyboard),
-      k.ifDeviceExists([KeychronK8, HHKBProfessionalHybridTypeS]),
-    ).manipulators(disableMappingRules);
-}
 
 function startupWezterm() {
   const hideOrStartWezterm = k.withMapper(
