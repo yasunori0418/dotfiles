7b131a3b7987f6fd89e41e7436ba59bd1b0ac918
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-18 00:01:36 +0900

    add: vscode setup tool

---
+++ scripts/vscode-setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+timestamp=$(date +%Y%m%d%H%M%S)
+readonly timestamp
+declare -r VSCODE_CONF="${HOME}/dotfiles/vscode"
+declare -r EXTENSIONS="${VSCODE_CONF}/extensions.txt"
+declare -r SETTINGS_JSON="${VSCODE_CONF}/settings.json"
+declare -r OUT_LOG="${VSCODE_CONF}/logs/install_extensions-${timestamp}.out.log"
+declare -r ERR_LOG="${VSCODE_CONF}/logs/install_extensions-${timestamp}.err.log"
+
+declare -r LINUX="${XDG_CONFIG_HOME}/Code/User"
+declare -r MAC="${HOME}/Library/Application Support/Code/User"
+
+[[ ! $(command -v code) ]] && echo "Not installed Visual Studio Code!" && exit 1
+
+echo "${timestamp} Execute : code --install-extension {extension_name} --force"
+while IFS= read -r line; do
+  echo "extension: ${line}"
+  code --install-extension "${line}" --force \
+    1>> "${OUT_LOG}" \
+    2>> "${ERR_LOG}"
+done < "${EXTENSIONS}"
+
+echo "================================"
+echo "Done VSCode install extensions."
+echo "Exported install log files."
+ls -l "${VSCODE_CONF}/logs"
+echo "================================"
+
+if [[ $(uname -s) = 'Linux' ]]; then
+  ln -svf "${SETTINGS_JSON}" "${LINUX}"
+  exit 0
+fi
+
+if [[ $(uname -s) = 'Darwin' ]]; then
+  ln -svf "${SETTINGS_JSON}" "${MAC}"
+  exit 0
+fi
