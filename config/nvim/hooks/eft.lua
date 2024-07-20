761c5ad1df5ab12c00b4e0d6afe8f867148e50bf
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-20 10:37:29 +0900

    update: eftのt-motionを使うようにした

--- config/nvim/hooks/eft.lua
+++ config/nvim/hooks/eft.lua
@@ -3,6 +3,8 @@ local opt = { silent = true, noremap = false }
 require("user.utils").keymaps_set({
     { mode = { "n", "x", "o" }, lhs = [[f]], rhs = [[<Plug>(eft-f-repeatable)]], opts = opt },
     { mode = { "n", "x", "o" }, lhs = [[F]], rhs = [[<Plug>(eft-F-repeatable)]], opts = opt },
+    { mode = { "n", "x", "o" }, lhs = [[t]], rhs = [[<Plug>(eft-t-repeatable)]], opts = opt },
+    { mode = { "n", "x", "o" }, lhs = [[T]], rhs = [[<Plug>(eft-T-repeatable)]], opts = opt },
     { mode = { "n", "x", "o" }, lhs = [[;]], rhs = [[<Plug>(eft-repeat)]], opts = opt },
 })
 -- }}}
