25241d73a4defadc187fead7aa54e61cb75bc380
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-07-28 19:25:48 +0900

    add: neovim側で使っているdppの設定を流用

---
+++ config/vim/dpp/config.ts
@@ -0,0 +1,102 @@
+import {
+  BaseConfig,
+  ConfigReturn,
+  Denops,
+  join,
+  Plugin,
+  vars,
+} from "./deps.ts";
+import {
+  type ConfigArguments,
+  gatherCheckFiles,
+  gatherTomls,
+  gatherVimrcs,
+  type LazyMakeStateResult,
+  type Toml,
+  type VimrcSkipRule,
+} from "./helper.ts";
+
+export class Config extends BaseConfig {
+  override async config(args: ConfigArguments): Promise<ConfigReturn> {
+    const denops: Denops = args.denops;
+
+    const vimrcSkipRules: VimrcSkipRule[] = [];
+
+    const inlineVimrcs: string[] = gatherVimrcs(
+      await vars.g.get(denops, "rc_dir"),
+      vimrcSkipRules,
+    );
+
+    args.contextBuilder.setGlobal({
+      inlineVimrcs: inlineVimrcs,
+      protocols: ["git"],
+      protocolParams: {
+        git: {
+          enablePartialClone: true,
+        },
+      },
+      extParams: {
+        installer: {
+          checkDiff: false,
+          logFilePath: join(
+            await vars.g.get(denops, "dpp_cache"),
+            // installer_{YYYYMMDD}.log
+            `installer_${
+              new Date().toLocaleDateString("ja-JP", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              }).replaceAll("/", "")
+            }.log`,
+          ),
+        },
+      },
+    });
+
+    const tomls = await gatherTomls(
+      await vars.g.get(denops, "toml_dir"),
+      ["dpp.toml", "no_lazy.toml"],
+      args,
+    ) as Toml[];
+
+    const recordPlugins: Record<string, Plugin> = {};
+    const hooksFiles: string[] = [];
+
+    for (const toml of tomls) {
+      if (toml.plugins) {
+        for (const plugin of toml.plugins) {
+          recordPlugins[plugin.name] = plugin;
+        }
+      }
+
+      if (toml.hooks_file) {
+        hooksFiles.push(toml.hooks_file);
+      }
+    }
+
+    const [context, options] = await args.contextBuilder.get(denops);
+
+    const lazyResult = await args.dpp.extAction(
+      denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult | undefined;
+
+    const checkFiles = gatherCheckFiles(
+      await vars.g.get(denops, "base_dir"),
+      "**/*.(ts|toml|vim)",
+    );
+
+    return {
+      checkFiles: checkFiles,
+      hooksFiles,
+      plugins: lazyResult?.plugins ?? [],
+      stateLines: lazyResult?.stateLines ?? [],
+    };
+  }
+}
---
+++ config/vim/dpp/helper.ts
@@ -0,0 +1,112 @@
+import { ContextBuilder, Denops, Dpp, expandGlobSync, Plugin } from "./deps.ts";
+
+export type Toml = {
+  hooks_file?: string;
+  ftplugins?: Record<string, string>;
+  plugins?: Plugin[];
+};
+
+export type ConfigArguments = {
+  denops: Denops;
+  contextBuilder: ContextBuilder;
+  basePath: string;
+  dpp: Dpp;
+};
+
+export type LazyMakeStateResult = {
+  plugins: Plugin[];
+  stateLines: string[];
+};
+
+export type VimrcSkipRule = {
+  name: string; // file name
+  condition: boolean; // skip target by condition
+};
+
+export function gatherVimrcs(
+  path: string,
+  vimrcSkipRules: VimrcSkipRule[],
+): string[] {
+  const inlineVimrcs: string[] = [];
+  if (path === null) throw `failure read directory in ${path}`;
+  for (const dirEntry of Deno.readDirSync(path)) {
+    if (typeof dirEntry == "undefined") continue;
+
+    const vimrcSkipRule = vimrcSkipRules.find((skipRule) => {
+      return dirEntry.name == skipRule.name;
+    }) as VimrcSkipRule | undefined;
+
+    if (vimrcSkipRule?.condition) continue;
+
+    inlineVimrcs.push(`${path}/${dirEntry.name}`);
+  }
+  return inlineVimrcs;
+}
+
+export async function gatherTomls(
+  path: string,
+  noLazyTomlNames: string[],
+  args: ConfigArguments,
+): Promise<Toml[]> {
+  const tomls: Toml[] = [];
+  const [context, options] = await args.contextBuilder.get(args.denops);
+
+  for (const tomlFile of Deno.readDirSync(path)) {
+    if (typeof tomlFile.name === "undefined") continue;
+    const isLazy = !noLazyTomlNames.includes(tomlFile.name);
+    tomls.push(
+      await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: `${path}/${tomlFile.name}`,
+          options: {
+            lazy: isLazy,
+          },
+        },
+      ) as Toml,
+    );
+  }
+  return tomls;
+}
+
+export async function gatherGhqPlugins(
+  args: ConfigArguments,
+): Promise<Plugin[]> {
+  const [context, options] = await args.contextBuilder.get(args.denops);
+  return await args.dpp.extAction(
+    args.denops,
+    context,
+    options,
+    "ghq",
+    "ghq",
+    {
+      ghq_root: "~/src",
+      repos: ["yasunori0418/ddu-gh_project"],
+      hostname: "github.com",
+      options: {
+        lazy: true,
+        merged: false,
+        on_source: "ddu.vim",
+        hook_add:
+          "let g:ddu_gh_project_gh_cmd = '/home/yasunori/src/github.com/yasunori0418/cli/bin/gh'",
+      },
+    },
+  ) as Plugin[];
+}
+
+export function gatherCheckFiles(path: string, glob: string): string[] {
+  const checkFiles: string[] = [];
+  for (
+    const file of expandGlobSync(glob, {
+      root: path,
+      globstar: true,
+    })
+  ) {
+    checkFiles.push(file.path);
+  }
+  return checkFiles;
+}
