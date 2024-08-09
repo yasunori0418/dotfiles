cdbcf66d90dff2d56b30213679aef1d518c640e0
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-10 02:54:40 +0900

    update: nixでMoralerspaceが無いので、HackGenに回避

--- config/wezterm/configs/font.lua
+++ config/wezterm/configs/font.lua
@@ -3,9 +3,9 @@ local wezterm = require("wezterm")
 
 function M.setup(config)
     -- HackGen好き
-    -- config.font = wezterm.font("HackGen35 Console NF")
+    config.font = wezterm.font("HackGen35 Console NF")
     -- config.font = wezterm.font("Moralerspace Neon NF")
-    config.font = wezterm.font("Moralerspace Argon NF")
+    -- config.font = wezterm.font("Moralerspace Argon NF")
     -- config.font = wezterm.font("Moralerspace Xenon NF")
     -- config.font = wezterm.font("Moralerspace Radon NF")
     -- config.font = wezterm.font("Moralerspace Krypton NF")
