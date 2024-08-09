53bc43599ae42e4afda131fd61d5426e64775615
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-10 01:36:27 +0900

    update: Makefile

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,7 +5,7 @@
 
   outputs = inputs: {
     nixosConfigurations = {
-      yasunoriLaptop = inputs.nixpkgs.lib.nixosSystem {
+      laptop = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./laptop/configuration.nix
