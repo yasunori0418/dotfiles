a6e2b3b2706837f091795424d8932035911def83
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-11 00:58:28 +0900

    update: nixのhardware設定をインポート

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,15 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    xremap.url = "github:xremap/nix-flake";
   };
 
-  outputs = inputs: {
+  outputs = {self, nixpkgs, nixos-hardware, xremap }: {
     nixosConfigurations = {
-      laptop = inputs.nixpkgs.lib.nixosSystem {
+      laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./laptop/configuration.nix
+          nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
         ];
+        specialArgs = {
+          inherit nixpkgs;
+          inherit nixos-hardware;
+          inherit xremap;
+        };
       };
     };
   };
