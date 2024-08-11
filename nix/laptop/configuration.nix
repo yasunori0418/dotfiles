5bdf81d06982b072d1d9df84ef4984f3b8e0f3a4
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-12 01:32:33 +0900

    chore: 余分なコメント行を削除

--- nix/laptop/configuration.nix
+++ nix/laptop/configuration.nix
@@ -102,9 +102,6 @@
     jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.yasunori = {
     isNormalUser = true;
@@ -224,25 +221,9 @@
     };
   };
 
-  # 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
