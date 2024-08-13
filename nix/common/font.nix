94365e400be43bfb895602021f52d5001f89d3e8
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-13 19:17:04 +0900

    update: font設定を分割

---
+++ nix/common/font.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      nerdfonts
+      hackgen-nf-font
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
+        sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+        monospace = ["HackGen35 Console NF" "JetBrainsMono Nerd Font" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
+
+}
--- nix/common/xserver.nix
+++ nix/common/xserver.nix
@@ -4,6 +4,7 @@
     ./sound.nix
     ./xremap.nix
     ./polkit.nix
+    ./font.nix
   ];
 
   services.xserver = {
@@ -58,26 +59,6 @@
   qt = {
     enable = true;
     platformTheme = "qt5ct";
-  };
-
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-serif
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-      nerdfonts
-      hackgen-nf-font
-    ];
-    fontDir.enable = true;
-    fontconfig = {
-      defaultFonts = {
-        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
-        sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
-        monospace = ["HackGen35 Console NF" "JetBrainsMono Nerd Font" "Noto Color Emoji"];
-        emoji = ["Noto Color Emoji"];
-      };
-    };
   };
 
   i18n.inputMethod = {
