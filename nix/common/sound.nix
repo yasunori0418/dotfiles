126f36e4991543f5cbd12ae48c2cb450c7a9fb67
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-13 17:10:38 +0900

    update: 音声用の設定を分割

---
+++ nix/common/sound.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+  };
+  programs.noisetorch.enable = true;
+}
--- nix/common/xserver.nix
+++ nix/common/xserver.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
 
   imports = [
+    ./sound.nix
     ./xremap.nix
   ];
 
@@ -105,18 +106,5 @@
       fcitx5-nord
     ];
   };
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-  };
-  programs.noisetorch.enable = true;
 
 }
