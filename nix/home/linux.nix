9051b34156b626dd78b75a29b241b44d8940c2fd
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-14 23:27:12 +0900

    chore: sort

--- nix/home/linux.nix
+++ nix/home/linux.nix
@@ -1,8 +1,8 @@
 { ... }: {
+  programs.home-manager.enable = true;
   home = rec {
     username = "yasunori";
     homeDirectory = "/home/${username}";
     stateVersion = "22.11";
   };
-  programs.home-manager.enable = true;
 }
