3da8f11b685ea544c67d00ba062c7fb6c96eda3a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-14 12:35:06 +0900

    update: ventoyメンテ用にexfatがあると:be::nri:

--- nix/common/system.nix
+++ nix/common/system.nix
@@ -52,6 +52,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    exfat
   ];
 
   programs.nix-ld = {
