4dcd856d02b5a6e59562ea41f571b2536889d757
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-15 17:03:38 +0900

    update: うおおおbumblebee-status動いたぞおおお

--- nix/common/xserver.nix
+++ nix/common/xserver.nix
@@ -21,7 +21,20 @@
       enable = true;
       extraPackages = with pkgs; [
         i3status
-        # bumblebee-status
+        i3status-rust
+        (bumblebee-status.override{
+          plugins = p:[
+            p.title
+            p.cpu2
+            p.memory
+            p.nic
+            p.datetime
+            p.battery
+            p.dunstctl
+            p.indicator
+            p.error
+          ];
+        })
       ];
     };
   };
