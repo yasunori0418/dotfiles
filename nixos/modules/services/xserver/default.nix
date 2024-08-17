1977a63cb6647bb646462399c900d0c5a47f6ba2
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-18 00:57:50 +0900

    update: xserver/default.nixで各種xserverの設定を統合

--- nixos/modules/services/xserver/default.nix
+++ nixos/modules/services/xserver/default.nix
@@ -1,7 +1,20 @@
+let
+  lightdm = import ./displayManager/lightdm.nix;
+  i3 = import ./windowManager/i3wm.nix;
+in
 {
   # Enable the X11 windowing system.
   enable = true;
 
   # Configure keymap in X11
   xkb.layout = "us";
+
+  displayManager = {
+    inherit lightdm;
+  };
+
+  windowManager = {
+    inherit i3;
+  };
+
 }
