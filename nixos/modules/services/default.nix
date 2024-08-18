398bbb3cd5931cbbc379007d6d36d1bff393a6bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-18 17:44:08 +0900

    update: tlpの設定を追加

--- nixos/modules/services/default.nix
+++ nixos/modules/services/default.nix
@@ -2,9 +2,10 @@
 let
   openssh = import ./openssh.nix;
   xserver = import ./xserver { inherit pkgs; };
+  tlp = import ./tlp.nix;
 in
 {
-  inherit openssh xserver;
+  inherit openssh xserver tlp;
 
   # Enable CUPS to print documents.
   printing.enable = true;
