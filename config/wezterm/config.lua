8e78586354a18b06ef4a57613d0bfae3d415b086
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-24 11:33:36 +0900

    update: nixosで抽象化されるweztermを回避する素敵な設定

--- config/wezterm/config.lua
+++ config/wezterm/config.lua
@@ -22,5 +22,6 @@ config.scrollback_lines = 5000
 config.disable_default_key_bindings = true
 config.keys = keybinds.keys
 config.key_tables = keybinds.key_tables
+config.front_end = [[WebGpu]]
 
 return config
