c711fa68281fd26cf48423b7a2212bb8bc630638
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-31 00:46:33 +0900

    update: home-managerでaquaで管理していたツールを移行

--- home-manager/pkgs.nix
+++ home-manager/pkgs.nix
@@ -6,12 +6,30 @@
       kitty
       alacritty
       lemonade
-      deno
       gcc
       ncurses
       unzip
       rustup
       luajitPackages.luarocks
+
+      # aqua config.yaml
+      sheldon
+      bat
+      gh
+      jq
+      ripgrep
+      ghq
+      delta
+      deno
+      fd
+      fzf
+      eza
+      go
+      bun
+      usql
+      glow
+      volta
+      awscli2
     ]
     ++ [ wezterm-flake.packages.${pkgs.system}.default ];
 }
