09b8d4f119d11e028a4c601f2ce9ee7975489f61
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-31 12:54:43 +0900

    update: やったーsheldon 0.8.0落ちてきたーーー

--- flake.nix
+++ flake.nix
@@ -56,19 +56,28 @@
           pkgs = import nixpkgs {
             system = "x86_64-linux";
             config.allowUnfree = true;
-            # overlays = [
-            #   (final: prev: {
-            #     sheldon = prev.sheldon.overrideAttrs (oldAttrs: {
-            #       version = "0.8.0";
-            #       src = prev.fetchFromGitHub {
-            #         owner = "rossmacarthur";
-            #         repo = "sheldon";
-            #         rev = "9836ef98ca2b44f781deafb409028d4dda7fef17";
-            #         hash = "sha256-eyfIPO1yXvb+0SeAx+F6/z5iDUA2GfWOiElfjn6abJM=";
-            #       };
-            #     });
-            #   })
-            # ];
+            overlays = [
+              (final: prev: {
+                sheldon =
+                  let
+                    sheldon-src = prev.fetchFromGitHub {
+                      owner = "rossmacarthur";
+                      repo = "sheldon";
+                      rev = "9836ef98ca2b44f781deafb409028d4dda7fef17";
+                      hash = "sha256-eyfIPO1yXvb+0SeAx+F6/z5iDUA2GfWOiElfjn6abJM=";
+                    };
+                  in
+                  prev.sheldon.overrideAttrs (oldAttrs: {
+                    version = "0.8.0";
+                    src = sheldon-src;
+                    cargoDeps = oldAttrs.cargoDeps.overrideAttrs {
+                      src = sheldon-src;
+                      name = "sheldon-0.8.0-vendor.tar.gz";
+                      outputHash = "sha256-+yTX1wUfVVjsM42X0QliL+0xbzTPheADZibPh/5Czh8=";
+                    };
+                  });
+              })
+            ];
           };
           extraSpecialArgs = {
             inherit wezterm-flake;
