04f7e40036358d6ee07f383e58d3f4c3187ecc92
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-08-31 12:52:58 +0900

    update: miseの自動起動とは:byebye-nya:

--- home/.zsh/sync/pkg_manager.zsh
+++
@@ -1,8 +0,0 @@
-# mise
-mise_cache=${XDG_CACHE_HOME}/mise.zsh
-if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
-  mise activate zsh > ${mise_cache}
-fi
-source ${mise_cache}
-
-unset mise_cache
