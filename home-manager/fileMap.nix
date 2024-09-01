479befec07b16b8ab757045d87279d486d0304ea
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-02 00:12:35 +0900

    refactor: formatted

--- home-manager/fileMap.nix
+++ home-manager/fileMap.nix
@@ -6,189 +6,193 @@
   ...
 }:
 {
-  home.file = let
-    symlink = config.lib.file.mkOutOfStoreSymlink;
-  in {
-    ".docker" = {
-      source = (symlink /${homeDir}/.docker);
-      recursive = true;
-    };
-    ".icons" = {
-      source = (symlink /${homeDir}/.icons);
-      recursive = true;
-    };
-    ".Xresources.d" = {
-      source = (symlink /${homeDir}/.Xresources.d);
-      recursive = true;
-    };
-    ".zsh" = {
-      source = (symlink /${homeDir}/.zsh);
-      recursive = true;
-    };
-    "bin" = {
-      source = (symlink /${dotfiles}/bin);
-      recursive = true;
-    };
-    ".Xresources".source = (symlink /${homeDir}/.Xresources);
-    ".zshenv".source = (symlink /${homeDir}/.zshenv);
-    ".zshrc".source = (symlink /${homeDir}/.zshrc);
-    ".zprofile".source = (symlink /${homeDir}/.zprofile);
-    ".p10k.zsh".source = (symlink /${homeDir}/.p10k.zsh);
-    ".xprofile".source = (symlink /${homeDir}/.xprofile);
-    ".xinitrc".source = (symlink /${homeDir}/.xinitrc);
-    ".xserverrc".source = (symlink /${homeDir}/.xserverrc);
-    ".textlintrc.yaml".source = (symlink /${homeDir}/.textlintrc.yaml);
-    ".pam_environment".source = (symlink /${homeDir}/.pam_environment);
-    ".gtkrc-2.0".source = (symlink /${homeDir}/.gtkrc-2.0);
-    ".face".source = (symlink /${homeDir}/.face);
-    ".dir_colors".source = (symlink /${homeDir}/.dir_colors);
-    ".bashrc".source = (symlink /${homeDir}/.bashrc);
-    ".bash_profile".source = (symlink /${homeDir}/.bash_profile);
-    ".bash_logout".source = (symlink /${homeDir}/.bash_logout);
-    "bun.lockb".source = (symlink /${homeDir}/bun.lockb);
-    "package.json".source = (symlink /${homeDir}/package.json);
+  home.file =
+    let
+      symlink = config.lib.file.mkOutOfStoreSymlink;
+    in
+    {
+      # $HOME
+      ".docker" = {
+        source = (symlink /${homeDir}/.docker);
+        recursive = true;
+      };
+      ".icons" = {
+        source = (symlink /${homeDir}/.icons);
+        recursive = true;
+      };
+      ".Xresources.d" = {
+        source = (symlink /${homeDir}/.Xresources.d);
+        recursive = true;
+      };
+      ".zsh" = {
+        source = (symlink /${homeDir}/.zsh);
+        recursive = true;
+      };
+      "bin" = {
+        source = (symlink /${dotfiles}/bin);
+        recursive = true;
+      };
+      ".Xresources".source = (symlink /${homeDir}/.Xresources);
+      ".zshenv".source = (symlink /${homeDir}/.zshenv);
+      ".zshrc".source = (symlink /${homeDir}/.zshrc);
+      ".zprofile".source = (symlink /${homeDir}/.zprofile);
+      ".p10k.zsh".source = (symlink /${homeDir}/.p10k.zsh);
+      ".xprofile".source = (symlink /${homeDir}/.xprofile);
+      ".xinitrc".source = (symlink /${homeDir}/.xinitrc);
+      ".xserverrc".source = (symlink /${homeDir}/.xserverrc);
+      ".textlintrc.yaml".source = (symlink /${homeDir}/.textlintrc.yaml);
+      ".pam_environment".source = (symlink /${homeDir}/.pam_environment);
+      ".gtkrc-2.0".source = (symlink /${homeDir}/.gtkrc-2.0);
+      ".face".source = (symlink /${homeDir}/.face);
+      ".dir_colors".source = (symlink /${homeDir}/.dir_colors);
+      ".bashrc".source = (symlink /${homeDir}/.bashrc);
+      ".bash_profile".source = (symlink /${homeDir}/.bash_profile);
+      ".bash_logout".source = (symlink /${homeDir}/.bash_logout);
+      "bun.lockb".source = (symlink /${homeDir}/bun.lockb);
+      "package.json".source = (symlink /${homeDir}/package.json);
 
-    ".config/aerospace" = {
-      source = (symlink /${xdgConfigHome}/aerospace);
-      recursive = true;
+      # XDG_CONFIG_HOME
+      ".config/aerospace" = {
+        source = (symlink /${xdgConfigHome}/aerospace);
+        recursive = true;
+      };
+      ".config/aqua" = {
+        source = (symlink /${xdgConfigHome}/aqua);
+        recursive = true;
+      };
+      ".config/bumblebee-status" = {
+        source = (symlink /${xdgConfigHome}/bumblebee-status);
+        recursive = true;
+      };
+      ".config/cantata" = {
+        source = (symlink /${xdgConfigHome}/cantata);
+        recursive = true;
+      };
+      ".config/dunst" = {
+        source = (symlink /${xdgConfigHome}/dunst);
+        recursive = true;
+      };
+      ".config/fastfetch" = {
+        source = (symlink /${xdgConfigHome}/fastfetch);
+        recursive = true;
+      };
+      ".config/fcitx5" = {
+        source = (symlink /${xdgConfigHome}/fcitx5);
+        recursive = true;
+      };
+      ".config/fd" = {
+        source = (symlink /${xdgConfigHome}/fd);
+        recursive = true;
+      };
+      ".config/git" = {
+        source = (symlink /${xdgConfigHome}/git);
+        recursive = true;
+      };
+      ".config/gtk-2.0" = {
+        source = (symlink /${xdgConfigHome}/gtk-2.0);
+        recursive = true;
+      };
+      ".config/gtk-3.0" = {
+        source = (symlink /${xdgConfigHome}/gtk-3.0);
+        recursive = true;
+      };
+      ".config/i3" = {
+        source = (symlink /${xdgConfigHome}/i3);
+        recursive = true;
+      };
+      ".config/jj" = {
+        source = (symlink /${xdgConfigHome}/jj);
+        recursive = true;
+      };
+      ".config/karabiner" = {
+        source = (symlink /${xdgConfigHome}/karabiner);
+        recursive = true;
+      };
+      ".config/keynav" = {
+        source = (symlink /${xdgConfigHome}/keynav);
+        recursive = true;
+      };
+      ".config/kitty" = {
+        source = (symlink /${xdgConfigHome}/kitty);
+        recursive = true;
+      };
+      ".config/libskk" = {
+        source = (symlink /${xdgConfigHome}/libskk);
+        recursive = true;
+      };
+      ".config/luacheck" = {
+        source = (symlink /${xdgConfigHome}/luacheck);
+        recursive = true;
+      };
+      ".config/mise" = {
+        source = (symlink /${xdgConfigHome}/mise);
+        recursive = true;
+      };
+      ".config/mpd" = {
+        source = (symlink /${xdgConfigHome}/mpd);
+        recursive = true;
+      };
+      ".config/neofetch" = {
+        source = (symlink /${xdgConfigHome}/neofetch);
+        recursive = true;
+      };
+      ".config/nvim" = {
+        source = (symlink /${xdgConfigHome}/nvim);
+        recursive = true;
+      };
+      ".config/paru" = {
+        source = (symlink /${xdgConfigHome}/paru);
+        recursive = true;
+      };
+      ".config/picom" = {
+        source = (symlink /${xdgConfigHome}/picom);
+        recursive = true;
+      };
+      ".config/rofi" = {
+        source = (symlink /${xdgConfigHome}/rofi);
+        recursive = true;
+      };
+      ".config/sheldon" = {
+        source = (symlink /${xdgConfigHome}/sheldon);
+        recursive = true;
+      };
+      ".config/sketchybar" = {
+        source = (symlink /${xdgConfigHome}/sketchybar);
+        recursive = true;
+      };
+      ".config/skhd" = {
+        source = (symlink /${xdgConfigHome}/skhd);
+        recursive = true;
+      };
+      ".config/systemd" = {
+        source = (symlink /${xdgConfigHome}/systemd);
+        recursive = true;
+      };
+      ".config/vim" = {
+        source = (symlink /${xdgConfigHome}/vim);
+        recursive = true;
+      };
+      ".config/wezterm" = {
+        source = (symlink /${xdgConfigHome}/wezterm);
+        recursive = true;
+      };
+      ".config/xremap" = {
+        source = (symlink /${xdgConfigHome}/xremap);
+        recursive = true;
+      };
+      ".config/yabai" = {
+        source = (symlink /${xdgConfigHome}/yabai);
+        recursive = true;
+      };
+      ".config/yamllint" = {
+        source = (symlink /${xdgConfigHome}/yamllint);
+        recursive = true;
+      };
+      ".config/zeno" = {
+        source = (symlink /${xdgConfigHome}/zeno);
+        recursive = true;
+      };
+      ".config/ncpamixer.conf".source = (symlink /${xdgConfigHome}/ncpamixer.conf);
+      ".config/screenkey.json".source = (symlink /${xdgConfigHome}/screenkey.json);
     };
-    ".config/aqua" = {
-      source = (symlink /${xdgConfigHome}/aqua);
-      recursive = true;
-    };
-    ".config/bumblebee-status" = {
-      source = (symlink /${xdgConfigHome}/bumblebee-status);
-      recursive = true;
-    };
-    ".config/cantata" = {
-      source = (symlink /${xdgConfigHome}/cantata);
-      recursive = true;
-    };
-    ".config/dunst" = {
-      source = (symlink /${xdgConfigHome}/dunst);
-      recursive = true;
-    };
-    ".config/fastfetch" = {
-      source = (symlink /${xdgConfigHome}/fastfetch);
-      recursive = true;
-    };
-    ".config/fcitx5" = {
-      source = (symlink /${xdgConfigHome}/fcitx5);
-      recursive = true;
-    };
-    ".config/fd" = {
-      source = (symlink /${xdgConfigHome}/fd);
-      recursive = true;
-    };
-    ".config/git" = {
-      source = (symlink /${xdgConfigHome}/git);
-      recursive = true;
-    };
-    ".config/gtk-2.0" = {
-      source = (symlink /${xdgConfigHome}/gtk-2.0);
-      recursive = true;
-    };
-    ".config/gtk-3.0" = {
-      source = (symlink /${xdgConfigHome}/gtk-3.0);
-      recursive = true;
-    };
-    ".config/i3" = {
-      source = (symlink /${xdgConfigHome}/i3);
-      recursive = true;
-    };
-    ".config/jj" = {
-      source = (symlink /${xdgConfigHome}/jj);
-      recursive = true;
-    };
-    ".config/karabiner" = {
-      source = (symlink /${xdgConfigHome}/karabiner);
-      recursive = true;
-    };
-    ".config/keynav" = {
-      source = (symlink /${xdgConfigHome}/keynav);
-      recursive = true;
-    };
-    ".config/kitty" = {
-      source = (symlink /${xdgConfigHome}/kitty);
-      recursive = true;
-    };
-    ".config/libskk" = {
-      source = (symlink /${xdgConfigHome}/libskk);
-      recursive = true;
-    };
-    ".config/luacheck" = {
-      source = (symlink /${xdgConfigHome}/luacheck);
-      recursive = true;
-    };
-    ".config/mise" = {
-      source = (symlink /${xdgConfigHome}/mise);
-      recursive = true;
-    };
-    ".config/mpd" = {
-      source = (symlink /${xdgConfigHome}/mpd);
-      recursive = true;
-    };
-    ".config/neofetch" = {
-      source = (symlink /${xdgConfigHome}/neofetch);
-      recursive = true;
-    };
-    ".config/nvim" = {
-      source = (symlink /${xdgConfigHome}/nvim);
-      recursive = true;
-    };
-    ".config/paru" = {
-      source = (symlink /${xdgConfigHome}/paru);
-      recursive = true;
-    };
-    ".config/picom" = {
-      source = (symlink /${xdgConfigHome}/picom);
-      recursive = true;
-    };
-    ".config/rofi" = {
-      source = (symlink /${xdgConfigHome}/rofi);
-      recursive = true;
-    };
-    ".config/sheldon" = {
-      source = (symlink /${xdgConfigHome}/sheldon);
-      recursive = true;
-    };
-    ".config/sketchybar" = {
-      source = (symlink /${xdgConfigHome}/sketchybar);
-      recursive = true;
-    };
-    ".config/skhd" = {
-      source = (symlink /${xdgConfigHome}/skhd);
-      recursive = true;
-    };
-    ".config/systemd" = {
-      source = (symlink /${xdgConfigHome}/systemd);
-      recursive = true;
-    };
-    ".config/vim" = {
-      source = (symlink /${xdgConfigHome}/vim);
-      recursive = true;
-    };
-    ".config/wezterm" = {
-      source = (symlink /${xdgConfigHome}/wezterm);
-      recursive = true;
-    };
-    ".config/xremap" = {
-      source = (symlink /${xdgConfigHome}/xremap);
-      recursive = true;
-    };
-    ".config/yabai" = {
-      source = (symlink /${xdgConfigHome}/yabai);
-      recursive = true;
-    };
-    ".config/yamllint" = {
-      source = (symlink /${xdgConfigHome}/yamllint);
-      recursive = true;
-    };
-    ".config/zeno" = {
-      source = (symlink /${xdgConfigHome}/zeno);
-      recursive = true;
-    };
-    ".config/ncpamixer.conf".source = (symlink /${xdgConfigHome}/ncpamixer.conf);
-    ".config/screenkey.json".source = (symlink /${xdgConfigHome}/screenkey.json);
-  };
 }
