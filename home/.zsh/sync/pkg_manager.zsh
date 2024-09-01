7bda3dcc7ac4ca4c04497baf5beed705f3c0d198
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-01 12:38:58 +0900

    Revert "update: miseの自動起動とは:byebye-nya:"
    This reverts commit 04f7e40036358d6ee07f383e58d3f4c3187ecc92.

---
+++ home/.zsh/sync/pkg_manager.zsh
@@ -0,0 +1,8 @@
+# mise
+mise_cache=${XDG_CACHE_HOME}/mise.zsh
+if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
+  mise activate zsh > ${mise_cache}
+fi
+source ${mise_cache}
+
+unset mise_cache
