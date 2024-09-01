0372fe0f8bc0fa9aa131b262a5167afe713c7bbb
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-01 22:51:20 +0900

    update: home directoryへのファイル配置をhome-managerに移行

--- home-manager/fileMap.nix
+++ home-manager/fileMap.nix
@@ -1,15 +1,32 @@
 {
-  config,
-  # flakeRoot,
+  flakeRoot,
   homeDir,
   # xdgConfigHome,
   ...
 }:
 {
   home.file = {
-    ".docker" = {
-      source = /${homeDir}/.docker;
-      recursive = false;
-    };
+    ".docker".source = /${homeDir}/.docker;
+    ".icons".source = /${homeDir}/.icons;
+    ".Xresources".source = /${homeDir}/.Xresources;
+    ".Xresources.d".source = /${homeDir}/.Xresources.d;
+    ".zsh".source = /${homeDir}/.zsh;
+    ".zshenv".source = /${homeDir}/.zshenv;
+    ".zshrc".source = /${homeDir}/.zshrc;
+    ".zprofile".source = /${homeDir}/.zprofile;
+    ".p10k.zsh".source = /${homeDir}/.p10k.zsh;
+    ".xprofile".source = /${homeDir}/.xprofile;
+    ".xinitrc".source = /${homeDir}/.xinitrc;
+    ".textlintrc.yaml".source = /${homeDir}/.textlintrc.yaml;
+    ".pam_environment".source = /${homeDir}/.pam_environment;
+    ".gtkrc-2.0".source = /${homeDir}/.gtkrc-2.0;
+    ".face".source = /${homeDir}/.face;
+    ".dir_colors".source = /${homeDir}/.dir_colors;
+    ".bashrc".source = /${homeDir}/.bashrc;
+    ".bash_profile".source = /${homeDir}/.bash_profile;
+    ".bash_logout".source = /${homeDir}/.bash_logout;
+    "bun.lockb".source = /${homeDir}/bun.lockb;
+    "package.json".source = /${homeDir}/package.json;
+    "bin".source = /${flakeRoot}/bin;
   };
 }
---
+++ migrate.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S bash
+
+cd "${HOME}"
+
+unlink bin
+unlink .icons
+unlink .Xresources.d
+unlink .zsh
+unlink .bash_logout
+unlink .bash_profile
+unlink .bashrc
+unlink .dir_colors
+unlink .face
+unlink .gtkrc-2.0
+unlink .p10k.zsh
+unlink .pam_environment
+unlink .textlintrc.yaml
+unlink .xinitrc
+unlink .xprofile
+unlink .Xresources
+unlink .xserverrc
+unlink .zprofile
+unlink .zshenv
+unlink .zshrc
+unlink bun.lockb
+unlink package.json
