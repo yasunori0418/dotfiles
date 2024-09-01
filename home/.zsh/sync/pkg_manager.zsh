75e743fad0b6a864917c5f38abebed9e2dac1528
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-01 12:41:02 +0900

    update: miseの自動起動をmacのとき限定にしておく

--- home/.zsh/sync/pkg_manager.zsh
+++ home/.zsh/sync/pkg_manager.zsh
@@ -1,8 +1,11 @@
 # mise
 mise_cache=${XDG_CACHE_HOME}/mise.zsh
-if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
-  mise activate zsh > ${mise_cache}
+
+if [[ $(uname -s) = 'Darwin' ]]; then
+    if [[ ! -r ${mise_cache} || ${MISE_CONFIG_FILE} -nt ${mise_cache} ]]; then
+        mise activate zsh > ${mise_cache}
+    fi
+    source ${mise_cache}
 fi
-source ${mise_cache}
 
 unset mise_cache
