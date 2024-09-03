bf078bf3e589c1589b984ff83edaf3e6d87708a1
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-03 23:42:20 +0900

    update: zeno起動速度向上とキーマップ反映のトリガーを更新

--- home/.zsh/defer/zeno.zsh
+++ home/.zsh/defer/zeno.zsh
@@ -12,8 +12,10 @@ export ZENO_GIT_TREE="exa --tree"
 # if disable builtin completion
 # export ZENO_DISABLE_BUILTIN_COMPLETION=1
 
+export ZENO_ENABLE_SOCK=1
+
 # zeno keybinds
-if [[ $(command -v deno) ]]; then
+if [[ $(command -v deno) && ${ZENO_LOADED} ]]; then
   bindkey " "  zeno-auto-snippet
   bindkey "^I" zeno-completion
   bindkey "^R" zeno-history-selection
