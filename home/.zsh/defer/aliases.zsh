6c6366a92f36ca7fc392e78b4261f3820b242264
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-08 01:32:13 +0900

    update: nvim-nightlyがserver.pipe共有不可なので、良い感じに切り替えるalias

--- home/.zsh/defer/aliases.zsh
+++ home/.zsh/defer/aliases.zsh
@@ -1,16 +1,16 @@
 # ls command aliases.
 if [[ $(command -v eza) ]]; then
-  alias eza='eza --icons -F'
-  alias ls='eza'
-  alias ll='eza -l --git'
-  alias la='ll -a'
-  alias lt='eza -T -L 3 -a -I ".git|.atom|.cache" --color=always | less'
-  alias lal='la --color=always | less'
+    alias eza='eza --icons -F'
+    alias ls='eza'
+    alias ll='eza -l --git'
+    alias la='ll -a'
+    alias lt='eza -T -L 3 -a -I ".git|.atom|.cache" --color=always | less'
+    alias lal='la --color=always | less'
 else
-  alias ls='ls --color=always'
-  alias la='ls -laA'
-  alias ll='ls -lA'
-  alias la='ls -laA | less'
+    alias ls='ls --color=always'
+    alias la='ls -laA'
+    alias ll='ls -lA'
+    alias la='ls -laA | less'
 fi
 
 # clear command aliases.
@@ -22,11 +22,7 @@ alias cd=custom_cd # ./commands.zsh
 alias dotfiles='cd ~/dotfiles'
 
 # Neovim alias
-if [[ $(command -v nvim) ]]; then
-  alias nv='nvim --listen ~/.cache/nvim/server.pipe'
-  alias nvr='nvim --server ~/.cache/nvim/server.pipe --remote'
-  alias nvrt='nvim --server ~/.cache/nvim/server.pipe --remote-tab'
-fi
+[[ $(command -v nvim) ]] && alias nv=nvim_server_pipe
 
 [[ $(command -v neovide) ]] && alias nvg='neovide --multigrid'
 
--- home/.zsh/defer/commands.zsh
+++ home/.zsh/defer/commands.zsh
@@ -1,10 +1,23 @@
 # Custom cd command
 custom_cd ()
 {
-  \cd $@ ; cc la
+    \cd $@ ; cc la
 }
 
 git_root ()
 {
-  cd $(git root)
+    cd $(git root)
+}
+
+nvim_server_pipe ()
+{
+    local server_pipe="${XDG_CACHE_HOME}/nvim/server.pipe"
+    local args=()
+    if [[ -S "${server_pipe}" ]]; then
+        args+=("--server")
+    else
+        args+=("--listen")
+    fi
+    args+=("${server_pipe}")
+    nvim "${args[@]} $@"
 }
