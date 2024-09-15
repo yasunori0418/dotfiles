8f4f481831a6bcf8dd198380381e5250146a3285
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-15 13:52:44 +0900

    refactor: i3wmで使用するツールの記述方法を変更

--- nixos/settings/xserver/windowManager/i3wm.nix
+++ nixos/settings/xserver/windowManager/i3wm.nix
@@ -1,33 +1,33 @@
 { pkgs }:
-let
-  applicationList = import ../applications.nix { inherit pkgs; };
-in
 {
   services.xserver.windowManager.i3 = {
     enable = true;
     extraPackages =
+      let
+        applications = import ../applications.nix { inherit pkgs; };
+        extraPackages =
+          with applications;
+          [ ] ++ systemThemeTools ++ nordThemePkgs ++ desktopTools ++ xfceTools ++ otherTools;
+      in
       with pkgs;
       [
         i3status
         i3status-rust
         (bumblebee-status.override {
-          plugins = p: [
-            p.title
-            p.cpu2
-            p.memory
-            p.nic
-            p.datetime
-            p.battery
-            p.dunstctl
-            p.indicator
-            p.error
-          ];
+          plugins =
+            p: with p; [
+              title
+              cpu2
+              memory
+              nic
+              datetime
+              battery
+              dunstctl
+              indicator
+              error
+            ];
         })
       ]
-      ++ applicationList.systemThemeTools
-      ++ applicationList.nordThemePkgs
-      ++ applicationList.desktopTools
-      ++ applicationList.xfceTools
-      ++ applicationList.otherTools;
+      ++ extraPackages;
   };
 }
