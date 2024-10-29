1ed55043a1ace30bb8947204279689a337ce272e
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-10-29 22:21:12 +0900

    update: kdlのindentサイズは4らしい

--- home/.config/nvim/hooks/dppft.lua
+++ home/.config/nvim/hooks/dppft.lua
@@ -85,6 +85,12 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 -- }}}
 
+-- lua_kdl {{{
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+-- }}}
+
 -- lua_help {{{
 vim.opt_local.conceallevel = 0
 vim.opt_local.foldmethod = "marker"
