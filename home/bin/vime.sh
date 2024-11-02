6ce7dadba19fbdc863a2367dd8d23109d1b57234
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-03 00:30:34 +0900

    update: vimeをalacrittyに切り替え

--- home/bin/vime.sh
+++ home/bin/vime.sh
@@ -4,20 +4,18 @@
 # https://gist.github.com/kuuote/4b2feda31d80fc8266fc562fc7f89c7f
 #
 declare -r clip_file="/tmp/clip.txt"
-declare -r nvim="${HOME}/.local/dotfiles/nvim/bin/nvim"
-eval "$(mise hook-env -s zsh)"
 
-[[ -f ${clip_file} ]] && rm ${clip_file}
-touch ${clip_file}
+echo -n > "${clip_file}"
 
-wezterm \
-  --config initial_rows=30 \
-  --config initial_cols=120 \
-  --config enable_tab_bar=false \
-  start --class FloatingVim \
-  ${nvim} --listen ~/.cache/nvim/server.pipe ${clip_file} \
+alacritty \
+  --option "window.dimensions.columns=120" \
+  --option "window.dimensions.lines=30" \
+  --option "window.decorations='FULL'" \
+  --option "terminal.shell=$(which zsh)" \
+  --class FloatingVim \
+  --command nvim "${clip_file}" \
   || exit 1
 
-head -c -1 ${clip_file} | xsel -bi
+head -c -1 "${clip_file}" | xsel -bi
 
 dunstify --appname=NVIME "copied in buffer text."
