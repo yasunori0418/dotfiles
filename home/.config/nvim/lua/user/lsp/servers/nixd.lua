76fd9f7ceca1176af9a893934a35b2e726be0e8d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-13 23:48:38 +0900

    update: nixdにフォーマットの設定を追加する

--- home/.config/nvim/lua/user/lsp/servers/nixd.lua
+++ home/.config/nvim/lua/user/lsp/servers/nixd.lua
@@ -1,3 +1,10 @@
 require("lspconfig").nixd.setup({
     capabilities = require("user.lsp.utils").capabilities,
+    settings = {
+        nixd = {
+            formatting = {
+                command = { "nixfmt" },
+            },
+        },
+    },
 })
