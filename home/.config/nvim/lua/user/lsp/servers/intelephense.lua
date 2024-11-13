a0194b12e0924281744e665a1a8244c25d363ab4
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-13 23:53:42 +0900

    update: intelephenseからのフォーマットを無効化

--- home/.config/nvim/lua/user/lsp/servers/intelephense.lua
+++ home/.config/nvim/lua/user/lsp/servers/intelephense.lua
@@ -1,3 +1,10 @@
 require("lspconfig").intelephense.setup({
     capabilities = require("user.lsp.utils").capabilities,
+    settings = {
+        intelephense = {
+            format = {
+                enable = false,
+            },
+        },
+    },
 })
