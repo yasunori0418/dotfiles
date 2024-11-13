d94c83d83738353437afd1a24c95878f242ec79e
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-13 22:54:49 +0900

    update: yamllsの設定が充実した

--- home/.config/nvim/lua/user/lsp/servers/yamlls.lua
+++ home/.config/nvim/lua/user/lsp/servers/yamlls.lua
@@ -1,3 +1,43 @@
+local yaml = {
+    format = {
+        enable = true,
+        singleQuote = false,
+        bracketSpacing = true,
+        proseWrap = "Never",
+    },
+    validate = true,
+    hover = true,
+    completion = true,
+    schemaStore = {
+        enable = true,
+    },
+    customTags = {
+        "!fn",
+        "!And",
+        "!If",
+        "!Not",
+        "!Equals",
+        "!Or",
+        "!FindInMap sequence",
+        "!Base64",
+        "!Cidr",
+        "!Ref",
+        "!Ref Scalar",
+        "!Sub",
+        "!GetAtt",
+        "!GetAZs",
+        "!ImportValue",
+        "!Select",
+        "!Split",
+        "!Join sequence",
+    },
+    schemas = {
+        ["https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json"] = "/*",
+    },
+}
 require("lspconfig").yamlls.setup({
     capabilities = require("user.lsp.utils").capabilities,
+    settings = {
+        yaml = yaml,
+    },
 })
