6c7e79df6e0c984a132d6478680280c18a3cfe36
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-16 14:11:39 +0900

    update:

--- nixos/ThinkPadE14Gen2/hardware-configuration.nix
+++ nixos/ThinkPadE14Gen2/hardware-configuration.nix
@@ -35,7 +35,9 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/swapfile"; size = 4096; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
