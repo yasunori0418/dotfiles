be68e13fa1697cf19b5159cf3a8453eb99afc258
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-19 00:50:53 +0900

    update: vimeをmac/linux両対応しておく

--- home/bin/vime.sh
+++ home/bin/vime.sh
@@ -7,14 +7,19 @@ declare -r clip_file="/tmp/clip.txt"
 
 echo -n > "${clip_file}"
 
+if [[ $(uname -s) = 'Linux' ]]; then
+    copy_cmd="xsel -bi"
+elif [[ $(uname -s) = 'Darwin' ]]; then
+    copy_cmd="pbcopy"
+fi
+
 alacritty \
-  --option "window.dimensions.columns=120" \
-  --option "window.dimensions.lines=30" \
-  --option "window.decorations='FULL'" \
-  --class FloatingVim \
-  --command nvim "${clip_file}" \
-  || exit 1
+    --option "window.dimensions.columns=120" \
+    --option "window.dimensions.lines=30" \
+    --option "window.decorations='FULL'" \
+    --class FloatingVim \
+    --title FloatingVim \
+    --command nvim "${clip_file}" \
+    || exit 1
 
-head -c -1 "${clip_file}" | xsel -bi
-
-dunstify --appname=NVIME "copied in buffer text."
+eval "${copy_cmd}" - < "${clip_file}" 
