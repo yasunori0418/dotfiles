0721625990926fe343ca7971cc929cd80f7e28c0
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-22 00:02:40 +0900

    update: nixos側にoverlaysの設定を増やす

--- nixos/settings/nixpkgs.nix
+++ nixos/settings/nixpkgs.nix
@@ -1,7 +1,21 @@
+{ inputs, ... }:
+let
+  inherit (inputs)
+    neovim-nightly-overlay
+    vim-overlay
+    ;
+in
 {
   nixpkgs = {
     config.allowUnfree = true;
     overlays = [
+      neovim-nightly-overlay.overlays.default
+      (vim-overlay.overlays.features {
+        lua = true;
+        python3 = true;
+        ruby = true;
+        sodium = true;
+      })
     ];
   };
 }
