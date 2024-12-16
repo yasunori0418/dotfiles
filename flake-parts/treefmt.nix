3bb011d7f6ab0e851b1c8a5dfd767723b2c1f851
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-12-17 00:47:40 +0900

    update: treefmtの設定を分割

---
+++ flake-parts/treefmt.nix
@@ -0,0 +1,43 @@
+# The importApply argument. Use this to reference things defined locally,
+# as opposed to the flake where this is imported.
+localFlake:
+
+# Regular module arguments; self, inputs, etc all reference the final user flake,
+# where this module was imported.
+{ ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt.programs = {
+        beautysh = {
+          enable = true;
+          indent_size = 4;
+        };
+        nixfmt = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
+        };
+        stylua = {
+          enable = true;
+          settings = {
+            call_parentheses = "Always";
+            collapse_simple_statement = "Never";
+            column_width = 120;
+            indent_type = "Spaces";
+            indent_width = 4;
+            line_endings = "Unix";
+            quote_style = "AutoPreferDouble";
+            sort_requires.enabled = false;
+          };
+        };
+        deno = {
+          enable = true;
+          includes = [
+            "home/.config/nvim"
+            "home/.config/vim"
+          ];
+        };
+      };
+    };
+}
--- flake.nix
+++ flake.nix
@@ -31,113 +31,96 @@
       flake-parts,
       ...
     }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-        inputs.treefmt-nix.flakeModule
-      ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem =
-        {
-          # config,
-          # self',
-          # inputs',
-          # pkgs,
-          # system,
-          ...
-        }:
-        {
-          treefmt = {
-            programs = {
-              beautysh = {
-                enable = true;
-                indent_size = 4;
-              };
-              nixfmt-rfc-style.enable = true;
-              stylua = {
-                enable = true;
-                settings = {
-                  call_parentheses = "Always";
-                  collapse_simple_statement = "Never";
-                  column_width = 120;
-                  indent_type = "Spaces";
-                  indent_width = 4;
-                  line_endings = "Unix";
-                  quote_style = "AutoPreferDouble";
-                  sort_requires.enabled = false;
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, flake-parts-lib, ... }:
+      let
+        # ref: https://flake.parts/dogfood-a-reusable-module.html?highlight=imports#example-with-importapply
+        inherit (flake-parts-lib) importApply;
+      in
+      {
+        imports =
+          let
+            treefmt.default = importApply ./flake-parts/treefmt.nix { inherit withSystem; };
+          in
+          [
+            # To import a flake module
+            # 1. Add foo to inputs
+            # 2. Add foo as a parameter to the outputs function
+            # 3. Add here: foo.flakeModule
+            inputs.treefmt-nix.flakeModule
+            treefmt.default
+          ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
+        perSystem =
+          {
+            # config,
+            # self',
+            # inputs',
+            # pkgs,
+            # system,
+            ...
+          }:
+          {
+            # Per-system attributes can be defined here. The self' and inputs'
+            # module parameters provide easy access to attributes of the same
+            # system.
+          };
+        flake = {
+          # The usual flake attributes can be defined here, including system-
+          # agnostic ones like nixosModule and system-enumerating ones, although
+          # those are more easily expressed in perSystem.
+
+          nixosConfigurations =
+            let
+              nixosSystemArgs =
+                { profileName, system }:
+                import ./nixos {
+                  inherit
+                    inputs
+                    profileName
+                    system
+                    ;
                 };
-              };
-              deno = {
-                enable = true;
-                includes = [
-                  "home/.config/nvim"
-                  "home/.config/vim"
-                ];
-              };
+              inherit (inputs.nixpkgs.lib) nixosSystem;
+            in
+            {
+              laptop = nixosSystem (nixosSystemArgs {
+                profileName = "ThinkPadE14Gen2";
+                system = "x86_64-linux";
+              });
+              desktop = nixosSystem (nixosSystemArgs {
+                profileName = "Desktop";
+                system = "x86_64-linux";
+              });
+              macx64OrbStack = nixosSystem (nixosSystemArgs {
+                profileName = "MacX64_OrbStack";
+                system = "x86_64-linux";
+              });
             };
-          };
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
+
+          homeConfigurations =
+            let
+              inherit (inputs.home-manager.lib) homeManagerConfiguration;
+              inherit (import ./home-manager/args.nix) args;
+            in
+            {
+              linux = homeManagerConfiguration (args {
+                inherit inputs;
+                profileName = "linux";
+                system = "x86_64-linux";
+              });
+              macx64 = homeManagerConfiguration (args {
+                inherit inputs;
+                profileName = "macx64";
+                system = "x86_64-darwin";
+              });
+            };
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-        nixosConfigurations =
-          let
-            nixosSystemArgs =
-              { profileName, system }:
-              import ./nixos {
-                inherit
-                  inputs
-                  profileName
-                  system
-                  ;
-              };
-            inherit (inputs.nixpkgs.lib) nixosSystem;
-          in
-          {
-            laptop = nixosSystem (nixosSystemArgs {
-              profileName = "ThinkPadE14Gen2";
-              system = "x86_64-linux";
-            });
-            desktop = nixosSystem (nixosSystemArgs {
-              profileName = "Desktop";
-              system = "x86_64-linux";
-            });
-            macx64OrbStack = nixosSystem (nixosSystemArgs {
-              profileName = "MacX64_OrbStack";
-              system = "x86_64-linux";
-            });
-          };
-
-        homeConfigurations =
-          let
-            inherit (inputs.home-manager.lib) homeManagerConfiguration;
-            inherit (import ./home-manager/args.nix) args;
-          in
-          {
-            linux = homeManagerConfiguration (args {
-              inherit inputs;
-              profileName = "linux";
-              system = "x86_64-linux";
-            });
-            macx64 = homeManagerConfiguration (args {
-              inherit inputs;
-              profileName = "macx64";
-              system = "x86_64-darwin";
-            });
-          };
-      };
-    };
+      }
+    );
 }
