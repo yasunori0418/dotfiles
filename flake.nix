2e3032a412e2e018633da960d57de4258e0a27ec
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-12-16 23:59:23 +0900

    update: flake-partsで包んでおく

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     xremap-flake = {
       url = "github:xremap/nix-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,56 +27,87 @@
   outputs =
     {
       # self,
-      nixpkgs,
+      flake-parts,
       ...
     }@inputs:
-    {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-      formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.nixfmt-rfc-style;
-      nixosConfigurations =
-        let
-          nixosSystemArgs =
-            { profileName, system }:
-            import ./nixos {
-              inherit
-                inputs
-                profileName
-                system
-                ;
-            };
-          inherit (inputs.nixpkgs.lib) nixosSystem;
-        in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
         {
-          laptop = nixosSystem (nixosSystemArgs {
-            profileName = "ThinkPadE14Gen2";
-            system = "x86_64-linux";
-          });
-          desktop = nixosSystem (nixosSystemArgs {
-            profileName = "Desktop";
-            system = "x86_64-linux";
-          });
-          macx64OrbStack = nixosSystem (nixosSystemArgs {
-            profileName = "MacX64_OrbStack";
-            system = "x86_64-linux";
-          });
+          # config,
+          # self',
+          # inputs',
+          pkgs,
+          # system,
+          ...
+        }:
+        {
+          formatter.default = pkgs.nixfmt-rfc-style;
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
         };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
 
-      homeConfigurations =
-        let
-          inherit (inputs.home-manager.lib) homeManagerConfiguration;
-          inherit (import ./home-manager/args.nix) args;
-        in
-        {
-          linux = homeManagerConfiguration (args {
-            inherit inputs;
-            profileName = "linux";
-            system = "x86_64-linux";
-          });
-          macx64 = homeManagerConfiguration (args {
-            inherit inputs;
-            profileName = "macx64";
-            system = "x86_64-darwin";
-          });
-        };
+        nixosConfigurations =
+          let
+            nixosSystemArgs =
+              { profileName, system }:
+              import ./nixos {
+                inherit
+                  inputs
+                  profileName
+                  system
+                  ;
+              };
+            inherit (inputs.nixpkgs.lib) nixosSystem;
+          in
+          {
+            laptop = nixosSystem (nixosSystemArgs {
+              profileName = "ThinkPadE14Gen2";
+              system = "x86_64-linux";
+            });
+            desktop = nixosSystem (nixosSystemArgs {
+              profileName = "Desktop";
+              system = "x86_64-linux";
+            });
+            macx64OrbStack = nixosSystem (nixosSystemArgs {
+              profileName = "MacX64_OrbStack";
+              system = "x86_64-linux";
+            });
+          };
+
+        homeConfigurations =
+          let
+            inherit (inputs.home-manager.lib) homeManagerConfiguration;
+            inherit (import ./home-manager/args.nix) args;
+          in
+          {
+            linux = homeManagerConfiguration (args {
+              inherit inputs;
+              profileName = "linux";
+              system = "x86_64-linux";
+            });
+            macx64 = homeManagerConfiguration (args {
+              inherit inputs;
+              profileName = "macx64";
+              system = "x86_64-darwin";
+            });
+          };
+      };
     };
 }
