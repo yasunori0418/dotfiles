c59fe9883dfadc7c6b597b10803d7d8be2540598
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-12-27 01:01:33 +0900

    update: skkeleton_state_popup

--- home/.config/nvim/hooks/skkeleton_state_popup.lua
+++ home/.config/nvim/hooks/skkeleton_state_popup.lua
@@ -1,9 +1,10 @@
 -- lua_source {{{
 vim.fn["skkeleton_state_popup#config"]({
     labels = {
-        ["input:okurinasi"] = { hira = "▽", kata = "▽", hankata = "▽", abbrev = "▽" },
+        input = { hira = "あ", kata = "ア", hankata = "ｶﾅ", zenkaku = "Ａ" },
+        ["input:okurinasi"] = { hira = "▽", kata = "▽", hankata = "▽", abbrev = "ab" },
         ["input:okuriari"] = { hira = "▽", kata = "▽", hankata = "▽" },
-        henkan = { hira = "▼", kata = "▼", hankata = "▼", abbrev = "▼" },
+        henkan = { hira = "▼", kata = "▼", hankata = "▼", abbrev = "ab" },
     },
     opts = { relative = "cursor", col = 5, row = 1, zindex = 5000, anchor = "NW", style = "minimal" },
 })
