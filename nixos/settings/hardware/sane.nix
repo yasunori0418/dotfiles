875ed5f758332fdf6d9760411412d49b4ba26170
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-12-27 21:16:21 +0900

    update: flake.lock

--- nixos/settings/hardware/sane.nix
+++ nixos/settings/hardware/sane.nix
@@ -2,11 +2,9 @@
 {
   hardware.sane = {
     enable = true;
-    # TODO(2024-12-27): 修正されているけどnixpkgs-unstableに振ってこない
-    # refer: https://nixpk.gs/pr-tracker.html?pr=365687
-    # extraBackends = with pkgs; [
-    #   epsonscan2
-    # ];
+    extraBackends = with pkgs; [
+      epsonscan2
+    ];
     openFirewall = true;
   };
   environment.systemPackages = with pkgs; [
