a76db55ae4cd0899e177b2c31dc6519ad7b8214f
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-01-03 02:31:37 +0900

    update: pipewire.nix

--- nixos/settings/applications/pipewire.nix
+++ nixos/settings/applications/pipewire.nix
@@ -1,13 +1,29 @@
+{ pkgs, ... }:
 {
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      jack.enable = true;
+      wireplumber = {
+        extraConfig.bluetoothEnhancements = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = [
+              "hsp_hs"
+              "hsp_ag"
+              "hfp_hf"
+              "hfp_ag"
+            ];
+          };
+        };
+      };
+    };
   };
   programs.noisetorch.enable = true;
 }
