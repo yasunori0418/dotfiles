8242e73e95458bf19dd7d27a340a9ea57c202338
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-01-05 02:55:01 +0900

    refactor: 止まらなくなっちまったんだ、しょうがないだろ
    やりすぎっていう自覚はあるんだ
    でも、止まらなくな(ry

--- home-manager/fileMap.nix
+++ home-manager/fileMap.nix
@@ -24,107 +24,130 @@ let
       "${pkgs.nixos-artwork.wallpapers.${name}}/share/backgrounds/nixos/nix-wallpaper-${name}.png"
     else
       (symlink /${homeDir}/.background-image);
+
+  /*
+    ファイル/ディレクトリ配置をいい感じにマップする
+    ```
+    ".config/hoge/conf" = {
+      source = symlink /${xdgConfigHome}/conf;
+      recursive = true;
+    };
+    ```
+    ↑こういう記述が連続するのを減らせる
+    配置元と配置先の命名が一致しているときだけ使える
+
+    dist: 配置先
+    src: dotfiles上のファイルパス
+    is_recursive: ディレクトリを配置するときはtrue、ファイルを配置するときはfalse
+    return: ファイル名のリストが引数となる関数
+  */
+  fileMap =
+    {
+      dist,
+      src,
+      is_recursive,
+    }:
+    let
+      f =
+        files:
+        builtins.foldl' (acc: elem: acc // elem) { } (
+          map (
+            file:
+            let
+              distFile = if dist == "" then "${file}" else "${dist}/${file}";
+            in
+            {
+              ${distFile} = {
+                source = symlink /${src}/${file};
+                recursive = is_recursive;
+              };
+            }
+          ) files
+        );
+    in
+    f;
+
+  homeFileMap = fileMap {
+    dist = "";
+    src = homeDir;
+    is_recursive = false;
+  };
+  homeDirMap = fileMap {
+    dist = "";
+    src = homeDir;
+    is_recursive = true;
+  };
+  xdgConfigFileMap = fileMap {
+    dist = ".config";
+    src = xdgConfigHome;
+    is_recursive = false;
+  };
+  xdgConfigDirMap = fileMap {
+    dist = ".config";
+    src = xdgConfigHome;
+    is_recursive = true;
+  };
 in
 {
-  homeDirectory = {
-    ".zsh" = {
-      source = symlink /${homeDir}/.zsh;
-      recursive = true;
-    };
-    "bin" = {
-      source = symlink /${homeDir}/bin;
-      recursive = true;
-    };
-    ".zshenv".source = symlink /${homeDir}/.zshenv;
-    ".zshrc".source = symlink /${homeDir}/.zshrc;
-    ".p10k.zsh".source = symlink /${homeDir}/.p10k.zsh;
-    ".textlintrc.yaml".source = symlink /${homeDir}/.textlintrc.yaml;
-    ".dir_colors".source = symlink /${homeDir}/.dir_colors;
-    ".bashrc".source = symlink /${homeDir}/.bashrc;
-    ".bash_profile".source = symlink /${homeDir}/.bash_profile;
-    ".bash_logout".source = symlink /${homeDir}/.bash_logout;
-    "package.json".source = symlink /${homeDir}/package.json;
-    ".screenrc".source = symlink /${homeDir}/.screenrc;
-    ".background-image".source = selectWallpaper {
-      type = "default";
-      name = "";
-      # type = "nixos-artwork";
-      # name = "nineish-solarized-dark";
-    };
-  };
+  homeDirectory =
+    {
+      ".background-image".source = selectWallpaper {
+        type = "default";
+        name = "";
+        # type = "nixos-artwork";
+        # name = "nineish-solarized-dark";
+      };
+    }
+    // homeDirMap [
+      ".zsh"
+      "bin"
+    ]
+    // homeFileMap [
+      ".zshenv"
+      ".zshrc"
+      ".p10k.zsh"
+      ".textlintrc.yaml"
+      ".dir_colors"
+      ".bashrc"
+      ".bash_profile"
+      ".bash_logout"
+      "package.json"
+      ".screenrc"
+    ];
 
-  dotConfig = {
-    ".config/alacritty/alacritty.toml".source = symlink /${xdgConfigHome}/alacritty/alacritty.toml;
-    ".config/alacritty/nord.toml".source = symlink /${xdgConfigHome}/alacritty/nord.toml;
-    ".config/alacritty/keybinds" = {
-      source = symlink /${xdgConfigHome}/alacritty/keybinds;
-      recursive = true;
-    };
-    ".config/tmux" = {
-      source = symlink /${xdgConfigHome}/tmux;
-      recursive = true;
-    };
-    ".config/fastfetch" = {
-      source = symlink /${xdgConfigHome}/fastfetch;
-      recursive = true;
-    };
-    ".config/fd" = {
-      source = symlink /${xdgConfigHome}/fd;
-      recursive = true;
-    };
-    ".config/git" = {
-      source = symlink /${xdgConfigHome}/git;
-      recursive = true;
-    };
-    ".config/jj" = {
-      source = symlink /${xdgConfigHome}/jj;
-      recursive = true;
-    };
-    ".config/kitty" = {
-      source = symlink /${xdgConfigHome}/kitty;
-      recursive = true;
-    };
-    ".config/ghostty" = {
-      source = symlink /${xdgConfigHome}/ghostty;
-      recursive = true;
-    };
-    ".config/luacheck" = {
-      source = symlink /${xdgConfigHome}/luacheck;
-      recursive = true;
-    };
-    ".config/neofetch" = {
-      source = symlink /${xdgConfigHome}/neofetch;
-      recursive = true;
-    };
-    ".config/nvim" = {
-      source = symlink /${xdgConfigHome}/nvim;
-      recursive = true;
-    };
-    ".config/vim" = {
-      source = symlink /${xdgConfigHome}/vim;
-      recursive = true;
-    };
-    ".config/sheldon" = {
-      source = symlink /${xdgConfigHome}/sheldon;
-      recursive = true;
-    };
-    ".config/yamllint" = {
-      source = symlink /${xdgConfigHome}/yamllint;
-      recursive = true;
-    };
-    ".config/zeno" = {
-      source = symlink /${xdgConfigHome}/zeno;
-      recursive = true;
-    };
-    ".config/wezterm" = {
-      source = symlink /${xdgConfigHome}/wezterm;
-      recursive = true;
-    };
-    ".config/direnv" = {
-      source = symlink /${xdgConfigHome}/direnv;
-      recursive = true;
-    };
-  };
+  dotConfig =
+    xdgConfigDirMap [
+      "alacritty/keybinds"
+      "tmux"
+      "fastfetch"
+      "fd"
+      "git"
+      "jj"
+      "kitty"
+      "luacheck"
+      "neofetch"
+      "nvim"
+      "vim"
+      "sheldon"
+      "yamllint"
+      "zeno"
+      "wezterm"
+      "direnv"
+    ]
+    // xdgConfigFileMap [
+      "alacritty/alacritty.toml"
+      "alacritty/nord.toml"
+      "ghostty/core.conf"
+      "ghostty/clipboard.conf"
+      "ghostty/command.conf"
+      "ghostty/font.conf"
+      "ghostty/keybinds.conf"
+      "ghostty/mouse.conf"
+      "ghostty/quick.conf"
+      "ghostty/resize.conf"
+      "ghostty/theme.conf"
+      "ghostty/window.conf"
+    ];
 
   MacOS = {
     homeDirectory = {
@@ -142,119 +165,65 @@ in
         recursive = true;
       };
     };
-    dotConfig = {
-      ".config/alacritty/os.toml".source = symlink /${xdgConfigHome}/alacritty/mac.toml;
-      ".config/aerospace" = {
-        source = symlink /${xdgConfigHome}/aerospace;
-        recursive = true;
-      };
-      ".config/karabiner" = {
-        source = symlink /${xdgConfigHome}/karabiner;
-        recursive = true;
-      };
-      ".config/sketchybar" = {
-        source = symlink /${xdgConfigHome}/sketchybar;
-        recursive = true;
-      };
-      ".config/skhd" = {
-        source = symlink /${xdgConfigHome}/skhd;
-        recursive = true;
-      };
-      ".config/yabai" = {
-        source = symlink /${xdgConfigHome}/yabai;
-        recursive = true;
-      };
-    };
+    dotConfig =
+      {
+        ".config/alacritty/os.toml".source = symlink /${xdgConfigHome}/alacritty/mac.toml;
+      }
+      // xdgConfigDirMap [
+        "ghostty/macos.conf"
+      ]
+      // xdgConfigDirMap [
+        "aerospace"
+        "karabiner"
+        "sketchybar"
+        "skhd"
+        "yabai"
+      ];
   };
 
   Linux = {
-    homeDirectory = {
-      ".docker/config.json".source = symlink /${homeDir}/.docker/linux_config.json;
-      ".icons" = {
-        source = symlink /${homeDir}/.icons;
-        recursive = true;
-      };
-      # ".Xresources.d" = {
-      #   source = symlink /${homeDir}/.Xresources.d;
-      #   recursive = true;
-      # };
-      # ".Xresources".source = symlink /${homeDir}/.Xresources;
-      ".xprofile".source = symlink /${homeDir}/.xprofile;
-      ".xinitrc".source = symlink /${homeDir}/.xinitrc;
-      ".xserverrc".source = symlink /${homeDir}/.xserverrc;
-      ".pam_environment".source = symlink /${homeDir}/.pam_environment;
-      ".gtkrc-2.0".source = symlink /${homeDir}/.gtkrc-2.0;
-      ".face".source = symlink /${homeDir}/.face;
-    };
-    dotConfig = {
-      ".config/alacritty/os.toml".source = symlink /${xdgConfigHome}/alacritty/linux.toml;
-      ".config/bumblebee-status" = {
-        source = symlink /${xdgConfigHome}/bumblebee-status;
-        recursive = true;
-      };
-      ".config/cantata" = {
-        source = symlink /${xdgConfigHome}/cantata;
-        recursive = true;
-      };
-      ".config/dunst" = {
-        source = symlink /${xdgConfigHome}/dunst;
-        recursive = true;
-      };
-      ".config/fcitx5" = {
-        source = symlink /${xdgConfigHome}/fcitx5;
-        recursive = true;
-      };
-      ".config/gtk-2.0" = {
-        source = symlink /${xdgConfigHome}/gtk-2.0;
-        recursive = true;
-      };
-      ".config/gtk-3.0" = {
-        source = symlink /${xdgConfigHome}/gtk-3.0;
-        recursive = true;
-      };
-      ".config/i3" = {
-        source = symlink /${xdgConfigHome}/i3;
-        recursive = true;
-      };
-      ".config/keynav" = {
-        source = symlink /${xdgConfigHome}/keynav;
-        recursive = true;
-      };
-      ".config/libskk" = {
-        source = symlink /${xdgConfigHome}/libskk;
-        recursive = true;
-      };
-      ".config/mpd" = {
-        source = symlink /${xdgConfigHome}/mpd;
-        recursive = true;
-      };
-      ".config/paru" = {
-        source = symlink /${xdgConfigHome}/paru;
-        recursive = true;
-      };
-      ".config/picom" = {
-        source = symlink /${xdgConfigHome}/picom;
-        recursive = true;
-      };
-      ".config/rofi" = {
-        source = symlink /${xdgConfigHome}/rofi;
-        recursive = true;
-      };
-      ".config/systemd" = {
-        source = symlink /${xdgConfigHome}/systemd;
-        recursive = true;
-      };
-      ".config/xremap" = {
-        source = symlink /${xdgConfigHome}/xremap;
-        recursive = true;
-      };
-      ".config/ncpamixer.conf".source = symlink /${xdgConfigHome}/ncpamixer.conf;
-      ".config/screenkey.json".source = symlink /${xdgConfigHome}/screenkey.json;
-      ".config/Code/User/settings.json".source = symlink /${xdgConfigHome}/Code/User/settings.json;
-      ".config/zellij" = {
-        source = symlink /${xdgConfigHome}/zellij;
-        recursive = true;
-      };
-    };
+    homeDirectory =
+      homeDirMap [
+        ".icons"
+        # ".Xresources.d"
+      ]
+      // homeFileMap [
+        ".docker/config.json"
+        ".xprofile"
+        ".xinitrc"
+        ".xserverrc"
+        ".pam_environment"
+        ".gtkrc-2.0"
+        ".face"
+        # ".Xresources"
+      ];
+    dotConfig =
+      {
+        ".config/alacritty/os.toml".source = symlink /${xdgConfigHome}/alacritty/linux.toml;
+      }
+      // xdgConfigDirMap [
+        "bumblebee-status"
+        "cantata"
+        "dunst"
+        "fcitx5"
+        "gtk-2.0"
+        "gtk-3.0"
+        "i3"
+        "keynav"
+        "libskk"
+        "mpd"
+        "paru"
+        "picom"
+        "rofi"
+        "systemd"
+        "xremap"
+        "zellij"
+      ]
+      // xdgConfigFileMap [
+        "ghostty/linux.conf"
+        "ncpamixer.conf"
+        "screenkey.json"
+        "Code/User/settings.json"
+      ];
   };
 }
