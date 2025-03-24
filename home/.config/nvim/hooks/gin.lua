aea7bd1e3125fec7f850a49abc1d44ddc2ed0642
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-03-24 13:55:01 +0900

    update: GinLogをハデにしておく

--- home/.config/nvim/hooks/gin.lua
+++ home/.config/nvim/hooks/gin.lua
@@ -4,6 +4,14 @@
 
 -- lua_source {{{
 vim.g.gin_proxy_apply_without_confirm = 1
+vim.g.gin_log_default_args = {
+    "++emojify",
+    "++opener=tabedit",
+    "--graph",
+    "--all",
+    "--abbrev-commit",
+    "--pretty=format:%C(bold yellow)%h%Creset -> %s%C(auto)%d%Creset %C(dim green)(%cr)%Creset%C(dim blue)<%an>%Creset",
+}
 -- }}}
 
 -- lua_gin-log {{{
