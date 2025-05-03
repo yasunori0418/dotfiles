c206d3ee1b7ddb15f037764a5236c953307aa83e
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-05-03 22:09:19 +0900

    update: サンプルのhello denopsを追加しておく

--- home/.config/nvim/denops/user/main.ts
+++ home/.config/nvim/denops/user/main.ts
@@ -1,5 +1,13 @@
 import type { Denops, Entrypoint } from "jsr:@denops/core@^7.0.1";
+import { is, ensure } from "jsr:@core/unknownutil@^4.3.0";
 
 export const main: Entrypoint = (denops: Denops) => {
-  denops.dispatcher = {};
+  denops.dispatcher = {
+    async hello(mes: unknown) {
+      const messages = ensure(mes, is.String);
+      await denops.cmd('echo "Hello Denops! mes:" messages', {
+        messages,
+      });
+    },
+  };
 };
