ec8c4429a8d12113e7290fd26a3431c73405a58f
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-05-16 15:22:47 +0900

    refactor: file-map用の関数を分割

--- home-manager/fileMap.nix
+++ home-manager/fileMap.nix
@@ -7,64 +7,10 @@
 }:
 let
   symlink = config.lib.file.mkOutOfStoreSymlink;
-  selectWallpaper =
-    {
-      ## type = (nixos-artwork | (default | null))
-      type,
-
-      ## type is `nixos-artwork` then select from nixos-artwork.
-      ## mean name: nix-wallpaper-${name}.png
-      ## refer: https://github.com/NixOS/nixos-artwork/tree/master/wallpapers
-      #
-      ## type is default or null then must be set any value.
-      ## e.g. name = "";
-      name,
-    }:
-    if type == "nixos-artwork" then
-      "${pkgs.nixos-artwork.wallpapers.${name}}/share/backgrounds/nixos/nix-wallpaper-${name}.png"
-    else
-      (symlink /${homeDir}/.background-image);
-
-  /*
-    ファイル/ディレクトリ配置をいい感じにマップする
-    ```
-    ".config/hoge/conf" = {
-      source = symlink /${xdgConfigHome}/conf;
-      recursive = true;
-    };
-    ```
-    ↑こういう記述が連続するのを減らせる
-    配置元と配置先の命名が一致しているときだけ使える
-
-    dist: 配置先
-    src: dotfiles上のファイルパス
-    is_recursive: ディレクトリを配置するときはtrue、ファイルを配置するときはfalse
-    return: ファイル名のリストが引数となる関数
-  */
-  fileMap =
-    {
-      dist,
-      src,
-      is_recursive,
-    }:
-    let
-      f =
-        files:
-        builtins.foldl' (
-          acc: file:
-          let
-            distFile = if dist == "" then "${file}" else "${dist}/${file}";
-          in
-          acc
-          // {
-            ${distFile} = {
-              source = symlink /${src}/${file};
-              recursive = is_recursive;
-            };
-          }
-        ) { } files;
-    in
-    f;
+  inherit (import ./lib/file-map.nix { inherit pkgs symlink homeDir; })
+    selectWallpaper
+    fileMap
+    ;
 
   homeFileMap = fileMap {
     dist = "";
