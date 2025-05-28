e005f5b5637bd06325cffcb828c37430afd4ff81
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-05-29 00:45:03 +0900

    update: xremapでemacsキーバインドにしてみる

--- nixos/settings/applications/xremap.nix
+++ nixos/settings/applications/xremap.nix
@@ -26,16 +26,169 @@
       ];
       keymap = [
         {
-          name = "Slightly emacs-like keymap.";
+          name = "emacs-like keymap.";
+          # https://github.com/xremap/xremap/blob/master/example/emacs.yml
           remap = {
-            C-h = "Backspace";
+            C-h = "backspace";
+
+            # Cursor
+            C-f = {
+              with_mark = "right";
+            };
+            C-b = {
+              with_mark = "left";
+            };
+            C-p = {
+              with_mark = "up";
+            };
+            C-n = {
+              with_mark = "down";
+            };
+
+            # Forward/Backward word
+            M-f = {
+              with_mark = "C-right";
+            };
+            M-b = {
+              with_mark = "C-left";
+            };
+
+            # Beginning/End of line
+            C-a = {
+              with_mark = "home";
+            };
+            C-e = {
+              with_mark = "end";
+            };
+
+            # Page up/down
+            M-v = {
+              with_mark = "pageup";
+            };
+            C-v = {
+              with_mark = "pagedown";
+            };
+
+            # Beginning/End of file
+            M-Shift-comma = {
+              with_mark = "C-home";
+            };
+            M-Shift-dot = {
+              with_mark = "C-end";
+            };
+
+            # newline
             C-m = "enter";
+
+            # Copy
+            C-w = [
+              "C-x"
+              { set_mark = false; }
+            ];
+            M-w = [
+              "C-c"
+              { set_mark = false; }
+            ];
+            C-y = [
+              "C-v"
+              { set_mark = false; }
+            ];
+
+            # Delete
+            C-d = [
+              "delete"
+              { set_mark = false; }
+            ];
+            M-d = [
+              "C-delete"
+              { set_mark = false; }
+            ];
+
+            # Kill line
+            C-k = [
+              "Shift-end"
+              "C-x"
+              { set_mark = false; }
+            ];
+            C-u = [
+              "Shift-home"
+              "C-x"
+              { set_mark = false; }
+            ];
+
+            # Kill word backward
+            Alt-backspace = [
+              "C-backspace"
+              { set_mark = false; }
+            ];
+
+            # set mark next word continuously.
+            C-M-space = [
+              "C-Shift-right"
+              { set_mark = true; }
+            ];
+
+            # Undo
+            C-slash = [
+              "C-z"
+              { set_mark = false; }
+            ];
+            C-Shift-ro = "C-z";
+
+            # Mark
+            C-space = {
+              set_mark = true;
+            };
+
+            # Search
+            C-s = "C-f";
+            C-r = "Shift-F3";
+            M-Shift-5 = "C-h";
+
+            # Cancel
+            C-g = [
+              "esc"
+              { set_mark = false; }
+            ];
+
+            # C-x YYY
+            C-x = {
+              remap = {
+                # C-x h (select all)
+                C-h = [
+                  "C-home"
+                  "C-a"
+                  { set_mark = true; }
+                ];
+
+                # C-x C-f (open)
+                C-f = "C-o";
+
+                # C-x C-s (save)
+                C-s = "C-s";
+
+                # C-x k (kill tab)
+                C-k = "C-F4";
+
+                # C-x C-c (exit)
+                C-c = "C-q";
+
+                # C-x u (undo)
+                C-u = [
+                  "C-z"
+                  { set_mark = false; }
+                ];
+
+              };
+            };
           };
           application = {
             not = [
               "/wezterm/"
               "/kitty/"
               "/Alacritty/"
+              "/ghostty/"
+              "/Emacs/"
             ];
           };
           device = {
