d2aa51eed334b4071bbe9ac7cc972ac6c92600d3
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-06-21 16:23:06 +0900

    update: claude-codeの1.0.31を強制しておく

--- home-manager/applications.nix
+++ home-manager/applications.nix
@@ -156,11 +156,22 @@
     biome
   ];
 
-  aiTools = with pkgs; [
-    codex
-    goose-cli
-    claude-code
-  ];
+  aiTools =
+    with pkgs;
+    let
+      claude-code-latest = claude-code.overrideAttrs (prev: rec {
+        version = "1.0.31"; # Update this to match your claude-code CLI version
+        src = fetchzip {
+          url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+          hash = "sha256-DrjR1LHnLXk2cW3zXO1jxc3octITMq6t28E2KJHBxZE=";
+        };
+      });
+    in
+    [
+      codex
+      goose-cli
+      claude-code-latest
+    ];
 
   libraries = with pkgs; [
     gcc
