5b3848c40bf667b96ec59b6ea5cb688a8c5f6064
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-06-22 02:12:07 +0900

    update: これでimeの切り替えとかspotlightの無効がnixでも実現できた

--- nix-darwin/settings/system.nix
+++ nix-darwin/settings/system.nix
@@ -3,6 +3,11 @@
   system = {
     stateVersion = 6;
     primaryUser = name;
+    activationScripts.postActivation.text = ''
+      # Following line should allow us to avoid a logout/login cycle when changing settings
+      # https://github.com/nix-darwin/nix-darwin/issues/518#issuecomment-2906740167
+      sudo -u ${name} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
     defaults = {
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
@@ -19,6 +24,55 @@
         autohide = true;
         show-recents = false;
         orientation = "left";
+      };
+      # カスタムキーボードショートカット設定
+      # @ = Command (⌘), $ = Shift (⇧), ~ = Option (⌥), ^ = Control (⌃)
+      CustomUserPreferences = {
+        # NSGlobalDomain = {
+        #   NSUserKeyEquivalents = {
+        #     # 全アプリケーション共通のショートカット例
+        #     "Paste and Match Style" = "@$v"; # Cmd+Shift+V
+        #     # "Minimize" = "@m";               # Cmd+M
+        #     # "Close" = "@w";                  # Cmd+W
+        #   };
+        # };
+        # 特定アプリケーション用のショートカット例
+        # "com.apple.Safari" = {
+        #   NSUserKeyEquivalents = {
+        #     "Show All History" = "@$h";     # Cmd+Shift+H
+        #   };
+        # };
+
+        # Symbolic Hotkeys設定
+        # Spotlightを無効化し、入力ソース切り替えをCmd+Spaceに設定
+        "com.apple.symbolichotkeys" = {
+          AppleSymbolicHotKeys = {
+            # Spotlight検索を無効化
+            "64" = {
+              enabled = false;
+            };
+            # Finder検索を無効化
+            "65" = {
+              enabled = false;
+            };
+            # 入力ソース切り替えをCmd+Spaceに設定
+            "60" = {
+              enabled = true;
+              value = {
+                type = "standard";
+                parameters = [
+                  32
+                  49
+                  1048576
+                ];
+              };
+            };
+            # 次の入力ソースを無効化
+            "61" = {
+              enabled = false;
+            };
+          };
+        };
       };
     };
   };
