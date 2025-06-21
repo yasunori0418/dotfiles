f0f79a9927499d936c11c985fddec1e72bf9a3b5
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-06-22 00:51:53 +0900

    update: うおおおおおこれでターミナルの中がぬるぬるにカーソル動くううううう

--- nix-darwin/settings/system.nix
+++ nix-darwin/settings/system.nix
@@ -8,6 +8,8 @@
         AppleShowAllExtensions = true;
         "com.apple.keyboard.fnState" = false;
         "com.apple.sound.beep.volume" = 0.0;
+        KeyRepeat = 1;
+        InitialKeyRepeat = 10;
       };
       finder = {
         AppleShowAllFiles = true;
