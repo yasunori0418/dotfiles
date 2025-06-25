8b250cd1a7ada21d0fbfa8079a23517363668a51
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-06-25 16:45:13 +0900

    update: kotlin_lsのroot_markersを追加

--- home/.config/nvim/after/lsp/kotlin_ls.lua
+++ home/.config/nvim/after/lsp/kotlin_ls.lua
@@ -1,6 +1,16 @@
+local root_markers = {
+    ".git",
+    "build.gradle",
+    "build.gradle.kts",
+    "pom.xml",
+    "settings.gradle",
+    "settings.gradle.kts",
+}
+
+---@type vim.lsp.Config
 return {
     cmd = { "kotlin-ls", "--stdio" },
     single_file_support = true,
     filetypes = { "kotlin" },
-    root_markers = { "build.gradle", "build.gradle.kts", "pom.xml" },
+    root_markers = root_markers,
 }
