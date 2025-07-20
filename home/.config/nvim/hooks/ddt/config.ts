cdd329440c7a54c797fab1584922147bd4ffadaa
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-07-20 22:57:54 +0900

    update: リファレンス実装見たらexpandしてなかった

--- home/.config/nvim/hooks/ddt/config.ts
+++ home/.config/nvim/hooks/ddt/config.ts
@@ -1,10 +1,9 @@
 import { BaseConfig, ConfigArguments } from "./deps.ts";
-import { expandHome } from "../ddu/helper.ts";
 
 export class Config extends BaseConfig {
   override config({ contextBuilder }: ConfigArguments): Promise<void> {
     contextBuilder.patchGlobal({
-      nvimServer: expandHome("~/.cache/nvim/server.pipe"),
+      nvimServer: "~/.cache/nvim/server.pipe",
       uiParams: {
         shell: {
           prompt: "%",
