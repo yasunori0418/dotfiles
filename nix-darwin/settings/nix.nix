16bfd1647ca5409aa1eaf96d5cf58a244d3d96a2
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-08-18 00:31:07 +0900

    update: nix-darwinの方キャッシュの設定入ってないわ

--- nix-darwin/settings/nix.nix
+++ nix-darwin/settings/nix.nix
@@ -8,6 +8,14 @@
       experimental-features = "nix-command flakes pipe-operators";
       max-jobs = 8;
       sandbox = "relaxed";
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://yasunori0418.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "yasunori0418.cachix.org-1:mC1j+M5A6063OHaOB5bH2nS0BiCW/BJsSRiOWjLeV9o="
+      ];
     };
   };
 }
