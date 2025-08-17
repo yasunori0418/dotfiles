28c7fc3be6d45657ff843d9e4eed45b3ad3b0bb7
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-08-17 12:27:50 +0900

    docs: コメントを手厚くしてみる

--- home-manager/lib/concat-target-attrs-value.nix
+++ home-manager/lib/concat-target-attrs-value.nix
@@ -10,4 +10,34 @@ let
   concat = list: foldl' (item: acc: acc ++ item) [ ] list;
   f = targetNames: attrSet: attrSet |> getAttrs targetNames |> attrValues |> concat;
 in
+/**
+  # Example
+
+  ```nix
+  let
+    attr = {
+      abc = ["a" "b" "c"]
+      def = ["d" "e" "f"]
+      ghi = ["g" "h" "i"]
+      jkl = ["j" "k" "l"]
+    };
+  in
+  concatTargetAttrsValue ["abc" "ghi"] attr
+  => ["a" "b" "c" "g" "h" "i"]
+  ```
+
+  # Type
+
+  ```
+  concatTargetAttrsValue :: [String] -> AttrSet [a] -> [a]
+  ```
+
+  # Arguments
+
+  targetNames :: [String]
+  : List of names of AttrSets to be retrieved
+
+  attrSet :: AttrSet [a]
+  : The original AttrSet
+*/
 f
