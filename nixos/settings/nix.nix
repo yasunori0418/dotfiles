85b2e066e4bc438d3138410e08b0b64324a1efa8
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-08-17 23:51:54 +0900

    update: 自分のcachixを定義してみる

--- nixos/settings/nix.nix
+++ nixos/settings/nix.nix
@@ -17,10 +17,12 @@
       trusted-substituters = [
         "https://hydra.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://yasunori0418.cachix.org"
       ];
       trusted-public-keys = [
         "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "yasunori0418.cachix.org-1:mC1j+M5A6063OHaOB5bH2nS0BiCW/BJsSRiOWjLeV9o="
       ];
       trusted-users = [
         "root"
