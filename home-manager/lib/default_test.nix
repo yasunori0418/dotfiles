57b3bec3c27efe4601973f95e4b86c2e4d992390
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-08-24 01:57:16 +0900

    test: targetAttrsValue

--- home-manager/lib/default_test.nix
+++ home-manager/lib/default_test.nix
@@ -57,4 +57,92 @@ in
       f = "f";
     };
   }
+  {
+    name = "targetAttrsValue. attr in list";
+    actual =
+      let
+        testData = {
+          abc = [
+            "a"
+            "b"
+            "c"
+          ];
+          def = [
+            "d"
+            "e"
+            "f"
+          ];
+          ghi = [
+            "g"
+            "h"
+            "i"
+          ];
+          jkl = [
+            "j"
+            "k"
+            "l"
+          ];
+        };
+      in
+      lib.targetAttrsValue [
+        "abc"
+        "ghi"
+      ] testData;
+    expected = [
+      [
+        "a"
+        "b"
+        "c"
+      ]
+      [
+        "g"
+        "h"
+        "i"
+      ]
+    ];
+  }
+  {
+    name = "targetAttrsValue. attr in attrs";
+    actual =
+      let
+        testData = {
+          abc = {
+            a = "a";
+            b = "b";
+            c = "c";
+          };
+          def = {
+            d = "d";
+            e = "e";
+            f = "f";
+          };
+          ghi = {
+            g = "g";
+            h = "h";
+            i = "i";
+          };
+          jkl = {
+            j = "j";
+            k = "k";
+            l = "l";
+          };
+        };
+      in
+      lib.targetAttrsValue [
+        "abc"
+        "ghi"
+      ] testData;
+    expected = [
+      {
+        a = "a";
+        b = "b";
+        c = "c";
+      }
+      {
+        g = "g";
+        h = "h";
+        i = "i";
+      }
+    ];
+  }
 ]
