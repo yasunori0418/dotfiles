8e121e2e58ee26df9f724b7214b90569146ed086
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-09-05 14:25:11 +0900

    update(typos_lsp): ライブラリ名とかでエラーがウザいので一旦makrdownだけにしておく

--- home/.config/nvim/after/lsp/typos_lsp.lua
+++ home/.config/nvim/after/lsp/typos_lsp.lua
@@ -1,20 +1,7 @@
 ---@type vim.lsp.Config
 return {
     filetypes = {
-        "bash",
-        -- "kotlin",
-        "lua",
-        "make",
         "markdown",
-        "nix",
-        "sh",
-        "toml",
-        "typescript",
-        "vim",
-        "zsh",
-        "html",
-        "css",
-        "awk",
     },
     init_options = {
         config = "~/.config/typos/config.toml",
