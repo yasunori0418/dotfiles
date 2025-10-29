67166a52c19688a5da5baa58215ff557c201d3bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-10-29 23:12:29 +0900

    update: もうちょいリッチにしておく

--- home-manager/initial-dotfiles-clone-after-on-files-change.nix
+++ home-manager/initial-dotfiles-clone-after-on-files-change.nix
@@ -10,6 +10,10 @@
       dotfiles = "${config.home.homeDirectory}/dotfiles";
     in
     lib.hm.dag.entryAfter [ "onFilesChange" ] ''
-      [[ ! -d ${dotfiles} ]] && ${pkgs.git}/bin/git clone https://github.com/yasunori0418/dotfiles.git ${dotfiles}
+      [[ -d ${dotfiles} ]] && exit 0
+      ${pkgs.git}/bin/git clone https://github.com/yasunori0418/dotfiles.git ${dotfiles}
+      cd ${dotfiles}
+      cp example.env .env
+      cp example.envrc .envrc
     '';
 }
