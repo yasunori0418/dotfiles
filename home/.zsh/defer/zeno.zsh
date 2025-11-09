38d52e890686ca77283e9af124d78ae812b8400f
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-11-09 18:34:42 +0900

    update: zenoのkeybindを見直す

--- home/.zsh/defer/zeno.zsh
+++ home/.zsh/defer/zeno.zsh
@@ -16,9 +16,21 @@ export ZENO_ENABLE_SOCK=1
 
 # zeno keybinds
 if [[ $(command -v deno) && ${ZENO_LOADED} ]]; then
+    # abbrev snippet
     bindkey " "  zeno-auto-snippet
+    bindkey "^M" zeno-auto-snippet-and-accept-line
+
+    # fallback bindkey
+    bindkey "^X^M" accept-line
+    bindkey "^XM" accept-line
+    bindkey "^X^ " zeno-insert-space
+    bindkey "^X " zeno-insert-space
+
+    # completion
     bindkey "^I" zeno-completion
+
+    # ZLE wigets
     bindkey "^R" zeno-history-selection
-    bindkey '^X' zeno-insert-snippet
-    bindkey '^G' zeno-ghq-cd
+    bindkey '^X^S' zeno-insert-snippet
+    bindkey '^X^F' zeno-ghq-cd
 fi
