3213b1f765d8d5bf0ab153e6f530201197010460
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-12-18 00:41:10 +0900

    feat: swayprop

--- home/.zsh/defer/commands.zsh
+++ home/.zsh/defer/commands.zsh
@@ -23,3 +23,45 @@ nvim_server_pipe ()
     args+=("${server_pipe}")
     nvim "${args[@]}" "${@}"
 }
+
+swayprop ()
+{
+    logger_swayprop () {
+        logger -t swayprop "$@"
+    }
+    # https://github.com/alterNERDtive/swayprop/blob/release/swayprop
+    # Needs to be declared before it’s assigned or I cannot grab the exit code
+    # later. Fun race condition <.<
+    local selection
+    local window
+    # Find all visible windows and get their geometry, then
+    # format slurp-friendly, then
+    # run slurp!
+    selection=$(swaymsg -t get_tree | jq  '.. | select(.visible?==true) | .rect | [ .x,.y,.width,.height ]' \
+            | jq -r '"\(.[0]),\(.[1]) \(.[2])x\(.[3])"' \
+        | slurp -r)
+
+    local ret=$?
+    if [[ $ret -ne 0 ]]; then
+        logger_swayprop "selection failure. exit code: ${ret}"
+        return $ret
+    fi
+    logger_swayprop "selection: ${selection}"
+
+    local x=$(echo "$selection" | awk -F'[, x]' '{print $1}')
+    local y=$(echo "$selection" | awk -F'[, x]' '{print $2}')
+    local w=$(echo "$selection" | awk -F'[, x]' '{print $3}')
+    local h=$(echo "$selection" | awk -F'[, x]' '{print $4}')
+
+    # Find the window matching the selection.
+    window=$(swaymsg -t get_tree | \
+        jq ".. | select((.rect?.x==$x) and (.rect?.y==$y) and (.rect?.width==$w) and (.rect?.height=$h) and (.visible?==true))")
+
+    if [[ -z "$window" ]]; then
+        logger_swayprop "not found window."
+        return 1
+    fi
+
+    # Pretty print, since capturing the output of `swaymsg` makes it uglify.
+    echo $window | jq
+}
