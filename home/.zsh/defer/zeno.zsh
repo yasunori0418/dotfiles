d4766fe9355d6043d1d8aa57b0e098c9bb8567ac
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-12-29 17:51:21 +0900

    update: zenoのfzf-tmuxを有効にしてみる

--- home/.zsh/defer/zeno.zsh
+++ home/.zsh/defer/zeno.zsh
@@ -12,6 +12,12 @@ export ZENO_GIT_TREE="exa --tree"
 # if disable builtin completion
 # export ZENO_DISABLE_BUILTIN_COMPLETION=1
 
+# if enable fzf-tmux
+export ZENO_ENABLE_FZF_TMUX=1
+
+# if setting fzf-tmux options
+export ZENO_FZF_TMUX_OPTIONS="-p"
+
 # zeno keybinds
 if [[ $(command -v deno) && ${ZENO_LOADED} ]]; then
     # abbrev snippet
