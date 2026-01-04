cd0a7b8654645b35e81457c5449e42e7ce36e3d4
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-01-04 21:28:31 +0900

    update: session毎に設定分けておく

--- home-manager/options/xdg.nix
+++ home-manager/options/xdg.nix
@@ -10,11 +10,17 @@
       enable = true;
       xdgOpenUsePortal = true;
       config = {
-        common = {
+        sway = {
           default = "gtk";
           "org.freedesktop.impl.portal.Inhibit" = "none";
           "org.freedesktop.impl.portal.ScreenCast" = "wlr";
           "org.freedesktop.impl.portal.Screenshot" = "wlr";
+        };
+        niri = {
+          default = "gnome;gtk";
+          "org.freedesktop.impl.portal.Access" = "gtk";
+          "org.freedesktop.impl.portal.Notification" = "gtk";
+          "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
         };
       };
       extraPortals = with pkgs; [
