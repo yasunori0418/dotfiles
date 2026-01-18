3119f13fdbd17aa1e59e66b3b2f83dbb93be3fe2
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-01-18 22:43:56 +0900

    fix: 適当にhome-manager側でsubstitutersの設定入れたら変になっちゃった。
    なんか末端のライブラリから全部ビルドしようとしてきて、時間泥棒になる設定になっちゃった。
    nix分からん

--- home-manager/options/nix.nix
+++ home-manager/options/nix.nix
@@ -5,29 +5,29 @@
     nixPath = [
       "nixpkgs=flake:nixpkgs"
     ];
-    settings = {
-      experimental-features = "nix-command flakes pipe-operators";
-      max-jobs = 8;
-      # sandbox = "relaxed";
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://yasunori0418.cachix.org"
-        "https://devenv.cachix.org"
-        "https://cache.numtide.com"
-      ];
-      trusted-substituters = [
-        "https://nix-community.cachix.org"
-        "https://yasunori0418.cachix.org"
-        "https://devenv.cachix.org"
-        "https://cache.numtide.com"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "yasunori0418.cachix.org-1:mC1j+M5A6063OHaOB5bH2nS0BiCW/BJsSRiOWjLeV9o="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      ];
-    };
+    # settings = {
+    #   experimental-features = "nix-command flakes pipe-operators";
+    #   max-jobs = 8;
+    #   # sandbox = "relaxed";
+    #   substituters = [
+    #     "https://nix-community.cachix.org"
+    #     "https://yasunori0418.cachix.org"
+    #     "https://devenv.cachix.org"
+    #     "https://cache.numtide.com"
+    #   ];
+    #   trusted-substituters = [
+    #     "https://nix-community.cachix.org"
+    #     "https://yasunori0418.cachix.org"
+    #     "https://devenv.cachix.org"
+    #     "https://cache.numtide.com"
+    #   ];
+    #   trusted-public-keys = [
+    #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    #     "yasunori0418.cachix.org-1:mC1j+M5A6063OHaOB5bH2nS0BiCW/BJsSRiOWjLeV9o="
+    #     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    #     "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    #   ];
+    # };
     registry = {
       nixpkgs = {
         from = {
