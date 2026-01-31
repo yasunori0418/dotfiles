4ea11aec50af72c40866c7b2f548286592ca3f63
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-01-31 21:58:13 +0900

    update: flake.lock

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "My dotfiles, all my wisdom, my castle.";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/70801e06d9730c4f1704fbd3bbf5b8e11c03a2a7";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # TODO: CHECK NIXPKGS TRACKER!!!!!!
+    # see https://nixpk.gs/pr-tracker.html?pr=485174
+    # nixpkgs.url = "github:NixOS/nixpkgs/70801e06d9730c4f1704fbd3bbf5b8e11c03a2a7";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     flake-parts.url = "github:hercules-ci/flake-parts";
