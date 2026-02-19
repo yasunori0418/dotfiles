ceb9900c7fe8985369988eb1d8cdd172ac26a24f
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-02-19 23:17:41 +0900

    update:

--- home-manager/applications.nix
+++ home-manager/applications.nix
@@ -29,46 +29,46 @@ in
     # keep-sorted end
   ];
 
-  utilityTools = with pkgs; [
-    # keep-sorted start
-    act
-    asciinema
-    asciinema-agg
-    awscli2
-    clipcat
-    colorized-logs
-    cryoflow
-    deno
-    direnv
-    flock
-    gh
-    git
-    git-credential-manager
-    git-lfs
-    glow
-    gnumake
-    jnv
-    kanata-with-cmd
-    lemonade
-    lnav
-    myNurPkgs.pict
-    myNurPkgs.safe-chain
-    pqrs
-    pueue
-    python313Packages.datadog
-    ssm-session-manager-plugin
-    tmux
-    tree-sitter
-    typos
-    unar
-    unzip
-    usql
-    zellij
-    zip
-    # keep-sorted end
-  ]
-  # ++ (optionalIsLinux [ inputs.zmx.packages.${pkgs.stdenv.hostPlatform.system}.default ])
-  ;
+  utilityTools =
+    with pkgs;
+    [
+      # keep-sorted start
+      act
+      asciinema
+      asciinema-agg
+      awscli2
+      colorized-logs
+      cryoflow
+      deno
+      direnv
+      flock
+      gh
+      git
+      git-credential-manager
+      git-lfs
+      glow
+      gnumake
+      jnv
+      kanata-with-cmd
+      lemonade
+      lnav
+      myNurPkgs.pict
+      myNurPkgs.safe-chain
+      pqrs
+      pueue
+      python313Packages.datadog
+      ssm-session-manager-plugin
+      tmux
+      tree-sitter
+      typos
+      unar
+      unzip
+      usql
+      zellij
+      zip
+      # keep-sorted end
+    ]
+    ++ (optionalIsLinux [ clipcat ]);
 
   textEditors =
     let
