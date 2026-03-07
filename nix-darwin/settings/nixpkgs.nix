bad7d308390d6125a082edb2ddd07fe3608e6a21
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-03-07 22:36:13 +0900

    update: flake.lock

--- nix-darwin/settings/nixpkgs.nix
+++ nix-darwin/settings/nixpkgs.nix
@@ -4,7 +4,8 @@
     config.allowUnfree = true;
     overlays = [
       inputs.claude-code-overlay.overlays.default
-      (inputs.vim-overlay.overlays.features {
+      (inputs.vim-overlay.lib.features {
+        cscope = true;
         lua = true;
         python3 = true;
         ruby = true;
