63d55f1aa9e38fdfc803e73a06ee2ea7d26c8056
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-03-16 10:39:28 +0900

    update: vim-overlayはnurでビルドする用にしたから、overlayから外す

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -20,12 +20,6 @@ in
     config.allowUnfree = true;
     overlays = [
       inputs.claude-code-overlay.overlays.default
-      (inputs.vim-overlay.overlays.features {
-        lua = true;
-        python3 = true;
-        ruby = true;
-        sodium = true;
-      })
     ];
   };
   extraSpecialArgs = {
--- nixos/options/nixpkgs.nix
+++ nixos/options/nixpkgs.nix
@@ -4,13 +4,6 @@
     config.allowUnfree = true;
     overlays = [
       inputs.claude-code-overlay.overlays.default
-      (inputs.vim-overlay.lib.features {
-        cscope = true;
-        lua = true;
-        python3 = true;
-        ruby = true;
-        sodium = true;
-      })
     ];
   };
 }
