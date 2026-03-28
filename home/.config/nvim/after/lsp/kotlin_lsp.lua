f9af4ba8e738b2f7cb9d2c24cb3477c66b27e289
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-03-28 23:14:54 +0900

    change: kotlin-lspのinitialize_resultは一旦見れなくて良いや

--- home/.config/nvim/after/lsp/kotlin_lsp.lua
+++ home/.config/nvim/after/lsp/kotlin_lsp.lua
@@ -101,8 +101,8 @@ return {
 
     ---@param client vim.lsp.Client
     ---@param initialize_result lsp.InitializeResult
-    on_init = function(client, initialize_result)
-        vim.print(initialize_result)
+    on_init = function(client, _--[[, initialize_result]])
+        -- vim.print(initialize_result)
         -- Add workspace folders from includeBuild in settings.gradle
         local settings_gradle_path = vim.fn.glob(vim.env.PWD .. "/settings.gradle*", false, true)[1]
         if settings_gradle_path then
