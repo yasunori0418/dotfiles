302352477100cf7578d6cdcde114196ea6d5ee74
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-05-02 18:13:48 +0900

    style: nix fmt

--- nixos/options/hardware/bluetooth.nix
+++ nixos/options/hardware/bluetooth.nix
@@ -12,5 +12,8 @@
   };
 
   services.blueman.enable = true;
-  environment.systemPackages = with pkgs; [ bluez bluez-tools ];
+  environment.systemPackages = with pkgs; [
+    bluez
+    bluez-tools
+  ];
 }
