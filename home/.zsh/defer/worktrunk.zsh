423b7957a72098fecf9aa462851e17d1fbb54e91
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-05-05 14:22:25 +0900

    update: wtで出てくるshell統合のrmをパッチする

--- home/.zsh/defer/worktrunk.zsh
+++ home/.zsh/defer/worktrunk.zsh
@@ -2,7 +2,9 @@
 wt_cache="${XDG_CACHE_HOME}/wt.zsh"
 dotfiles_flake_lock="${DOTFILES}/flake.lock"
 if [[ ! -f ${wt_cache} || ${dotfiles_flake_lock} -nt ${wt_cache} ]]; then
-    wt config shell init zsh > ${wt_cache}
+    # Patch rm -f -> command rm -f before writing, so alias expansion at source
+    # time doesn't intercept the temp file cleanup inside wt().
+    wt config shell init zsh | sed 's/\brm -f\b/command rm -f/g' > ${wt_cache}
 fi
 source ${wt_cache}
 unset wt_cache dotfiles_flake_lock
