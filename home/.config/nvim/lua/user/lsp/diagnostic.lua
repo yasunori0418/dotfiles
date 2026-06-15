0e4868ef6fa693958502d7cea47c98b629125b47
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-06-15 20:02:55 +0900

    update: diagnosticのjump周りの定義を最新の物に更新

--- home/.config/nvim/lua/user/lsp/diagnostic.lua
+++ home/.config/nvim/lua/user/lsp/diagnostic.lua
@@ -1,6 +1,5 @@
-local diagnostic = vim.diagnostic
-
-diagnostic.config({
+---@type vim.diagnostic.Opts
+local config = {
     underline = true,
     virtual_text = false,
     virtual_lines = {
@@ -8,10 +7,10 @@ diagnostic.config({
     },
     signs = {
         text = {
-            [diagnostic.severity.ERROR] = "",
-            [diagnostic.severity.WARN] = "",
-            [diagnostic.severity.INFO] = "",
-            [diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
         },
     },
     float = {
@@ -20,6 +19,14 @@ diagnostic.config({
     },
     severity_sort = true,
     jump = {
-        float = true,
+        on_jump = function(_, bufnr)
+            vim.diagnostic.open_float({
+                bufnr = bufnr,
+                scope = "cursor",
+                focus = false,
+            })
+        end,
     },
-})
+}
+
+vim.diagnostic.config(config)
